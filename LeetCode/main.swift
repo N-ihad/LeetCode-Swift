670043420ae2558b63cfcf4c04000f8589167670
Nihad Samedov <the_nihad@icloud.com>
2021-11-17 18:39:43 +0300

    O^2 algorithm

--- LeetCode/main.swift
+++ LeetCode/main.swift
@@ -7,5 +7,14 @@
 
 import Foundation
 
-print("Hello, World!")
-
+// MARK: - O^2
+func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+    for i in 0..<nums.count {
+        for j in i+1..<nums.count {
+            if nums[i] + nums[j] == target {
+                return [i, j]
+            }
+        }
+    }
+    return []
+}
