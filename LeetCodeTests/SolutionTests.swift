eb51a7cc1eacb41eeca9b6323d376eeaa343ca3e
Nihad Samedov <the_nihad@icloud.com>
2021-11-20 21:29:58 +0300

    Update unit testing bundle

--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,10 +22,14 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testSolution_() {
+        
     }
+
+//    func testPerformanceExample() throws {
+//        // This is an example of a performance test case.
+//        measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
 }
