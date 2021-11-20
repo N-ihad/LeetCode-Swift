107affc84fc2705e196bddbc4bc12629a47a3f01
Nihad Samedov <the_nihad@icloud.com>
2021-11-20 16:09:02 +0300

    Initial setup

--- LeetCode/main.swift
+++ LeetCode/main.swift
@@ -7,5 +7,25 @@
 
 import Foundation
 
-print("Hello, World!")
+func intToRoman(_ num: Int) -> String {
+    return ""
+}
 
+
+// MARK: - Testing
+// Input: num = 3
+// Output: "III"
+
+// Input: num = 4
+// Output: "IV"
+
+// Input: num = 9
+// Output: "IX"
+
+// Input: num = 58
+// Output: "LVIII"
+// Explanation: L = 50, V = 5, III = 3.
+
+// Input: num = 1994
+// Output: "MCMXCIV"
+// Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
