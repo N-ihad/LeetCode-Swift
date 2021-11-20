25b48ce5766b47bc6a61b5e359be7be014a6981f
Nihad Samedov <the_nihad@icloud.com>
2021-11-20 21:28:54 +0300

    Solution C++ and Swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,80 @@
 
 import Foundation
 
-class Solution {
-    
+extension String {
+    subscript(_ index: Int) -> Self {
+        return String(self[self.index(self.startIndex, offsetBy: index)])
+    }
 }
+
+struct Stack<T> {
+    private var elements: [T] = []
+
+    var peek: T? {
+        elements.last
+    }
+
+    var isEmpty: Bool {
+        elements.isEmpty
+    }
+
+    mutating func push(_ newElement: T) {
+        elements.append(newElement)
+    }
+
+    mutating func pop() {
+        elements.removeLast()
+    }
+}
+
+class Solution {
+    func isValid(_ s: String) -> Bool {
+        var stack = Stack<String>()
+
+        for i in 0..<s.count {
+            if stack.isEmpty {
+                stack.push(s[i])
+                continue
+            }
+
+            let last = stack.peek!
+
+            switch s[i] {
+            case ")" where last == "(", "]" where last == "[", "}" where last ==  "{":
+                stack.pop()
+            case ")", "]", "}" where last == ")" || last == "]" || last == "}":
+                return false
+            default:
+                stack.push(s[i])
+            }
+        }
+
+        return stack.isEmpty ? true : false
+    }
+}
+
+// MARK: - Solution C++ (since Swift sucks ass: retrieving character from a string by index takes fucking O(n))
+/*
+bool isValid(string s) {
+    stack<char> stack;
+
+    for(int i=0; i<s.length(); ++i) {
+        if (stack.empty()) {
+            stack.push(s[i]);
+            continue;
+        }
+
+        if ((s[i] == ')' && stack.top() == '(') ||
+            (s[i] == ']' && stack.top() == '[') ||
+            (s[i] == '}' && stack.top() == '{')) {
+            stack.pop();
+        } else if (s [i] == '(' || s[i] == '[' || s[i] == '{') {
+            stack.push(s[i]);
+        } else {
+            return false;
+        }
+    }
+
+    return stack.empty() ? true : false;
+}
+*/
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,10 +22,38 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testSolution_isValidParentheses() {
+        // given
+        let parenthesis1 = "()"
+        let parenthesis2 = "()[]{}"
+        let parenthesis3 = "(]"
+        let parenthesis4 = "([)]"
+        let parenthesis5 = "{[]}"
+        let parenthesis6 = "(("
+        let parenthesis7 = "("
+
+        // when
+        let result1 = sut.isValid(parenthesis1)
+        let result2 = sut.isValid(parenthesis2)
+        let result3 = sut.isValid(parenthesis3)
+        let result4 = sut.isValid(parenthesis4)
+        let result5 = sut.isValid(parenthesis5)
+        let result6 = sut.isValid(parenthesis6)
+        let result7 = sut.isValid(parenthesis7)
+
+        // then
+        XCTAssertEqual(result1, true)
+        XCTAssertEqual(result2, true)
+        XCTAssertEqual(result3, false)
+        XCTAssertEqual(result4, false)
+        XCTAssertEqual(result5, true)
+        XCTAssertEqual(result6, false)
+        XCTAssertEqual(result7, false)
     }
+
+//    func testPerformanceExample() throws {
+//        measure {
+//            sut.isValid("{{{{{{{{{{{{{{{{")
+//        }
+//    }
 }
