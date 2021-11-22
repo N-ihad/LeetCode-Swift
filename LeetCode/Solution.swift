19f0b0f1023b46f55a7ac9c54b35ca4c751d2888
Nihad Samedov <the_nihad@icloud.com>
2021-11-23 02:34:20 +0300

    Solution

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,45 @@
 
 import Foundation
 
+class Node {
+    var val: Int
+    var next: Node?
+    var random: Node?
+
+    init(_ val: Int) {
+        self.val = val
+        self.next = nil
+        self.random = nil
+    }
+}
+
 class Solution {
-    
+    func copyRandomList(_ head: Node?) -> Node? {
+        var node: Node? = head
+        while node != nil {
+            let newNode = Node(node!.val)
+            newNode.next = node!.next
+            newNode.random = node
+            node!.next = newNode
+            node = newNode.next
+        }
+
+        node = head
+        while node != nil {
+            node!.next?.random = node!.next!.random?.random?.next
+            node = node?.next?.next
+        }
+
+        node = head
+        let origin: Node? = head?.next
+        var newHead: Node? = origin
+        while node != nil {
+            node?.next = node?.next?.next
+            newHead?.next = node?.next?.next
+            newHead = newHead?.next
+            node = node?.next
+        }
+
+        return origin
+    }
 }
--- LeetCode/main.swift
+++ LeetCode/main.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+let s = Solution()
+
+let node0 = Node(7)
+let node1 = Node(13)
+let node2 = Node(11)
+let node3 = Node(10)
+let node4 = Node(1)
+node0.next = node1
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node1.random = node0
+node2.random = node4
+node3.random = node2
+node4.random = node0
+
+// MARK: - Printing all nodes -
+//var h: Node? = node0
+//while h != nil {
+//    print("\(h!.val), \(h!.random?.val)")
+//    h = h!.next
+//}
+
+let _ = s.copyRandomList(node0)
