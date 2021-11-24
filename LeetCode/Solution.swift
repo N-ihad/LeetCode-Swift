19542d47b7fae087fc9ae44f301d3ae3d35dec12
Nihad Samedov <the_nihad@icloud.com>
2021-11-24 23:10:12 +0300

    Solution

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,20 @@
 import Foundation
 
 class Solution {
-    
+    func merge(_ intervals: [[Int]]) -> [[Int]] {
+        let intervals = intervals.sorted(by: { $0[0] < $1[0] })
+        var output: [[Int]] = [intervals[0]]
+
+        var i = 1
+        while i != intervals.count {
+            if output[output.count-1][1] >= intervals[i][0] {
+                output[output.count-1][1] = max(output[output.count-1][1], intervals[i][1])
+            } else {
+                output.append(intervals[i])
+            }
+            i += 1
+        }
+
+        return output
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,8 +22,33 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_mergeIntervals() {
+        // given
+        let intervals1 = [[1, 3], [2, 6], [8, 10], [15, 18]]
+        let intervals2 = [[1, 4], [4, 5]]
+        let intervals3 = [[1, 4], [0, 4]]
+        let intervals4 = [[1, 4], [0, 0]]
+        let intervals5 = [[1, 4], [0, 2], [3, 5]]
+
+        // when
+        let result1 = sut.merge(intervals1)
+        let result2 = sut.merge(intervals2)
+        let result3 = sut.merge(intervals3)
+        let result4 = sut.merge(intervals4)
+        let result5 = sut.merge(intervals5)
+
+        // then
+        let expected1 = [[1, 6], [8, 10], [15, 18]]
+        let expected2 = [[1, 5]]
+        let expected3 = [[0, 4]]
+        let expected4 = [[0, 0], [1, 4]]
+        let expected5 = [[0, 5]]
+
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
+        XCTAssertEqual(result3, expected3)
+        XCTAssertEqual(result4, expected4)
+        XCTAssertEqual(result5, expected5)
     }
 
 //    func testPerformanceExample() throws {
