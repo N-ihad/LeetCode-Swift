dd708b37dab820a503c217a574855b4f4cd584d3
Nihad Samedov <the_nihad@icloud.com>
2021-11-25 01:40:35 +0300

    Solution

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,56 @@
 
 import Foundation
 
+class ListNode {
+    var val: Int
+    var next: ListNode?
+
+    init() {
+        val = 0
+        next = nil
+    }
+
+    init(_ val: Int) {
+        self.val = val
+        next = nil
+    }
+
+    init(_ val: Int, _ next: ListNode?) {
+        self.val = val
+        self.next = next
+    }
+}
+
 class Solution {
-    
+    func rotateRight(_ head: ListNode?, _ k: Int) -> ListNode? {
+        var n = 0
+        var node: ListNode? = head
+        while node != nil {
+            n += 1
+            node = node!.next
+        }
+
+        if n == k || k == 0 || head == nil || n == 1 {
+            return head
+        }
+
+        let k = k < n ? n - k : n - (k % n == 0 ? n : k % n)
+
+        node = head
+        for i in 0..<k {
+            if i == k - 1 {
+                let newHead = node!.next
+                node!.next = nil
+                node = newHead
+                while node?.next != nil {
+                    node = node?.next
+                }
+                node?.next = head
+                return newHead
+            }
+            node = node?.next
+        }
+
+        return node
+    }
 }
--- LeetCode/main.swift
+++ LeetCode/main.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+// MARK: - Testing
+let sut = Solution()
+
+let node1 = ListNode(1)
+let node2 = ListNode(2)
+let node3 = ListNode(3)
+let node4 = ListNode(4)
+let node5 = ListNode(5)
+let k = 2
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+
+//let node1 = ListNode(0)
+//let node2 = ListNode(1)
+//let k = 3
+
+node1.next = node2
+
+var node: ListNode? = sut.rotateRight(node1, k)
+while node != nil {
+    print(node!.val)
+    node = node!.next
+}
