47bd35cf599b5bc6790d7d1978cfa62bb343d9a2
Nihad Samedov <the_nihad@icloud.com>
2021-11-25 16:21:17 +0300

    Remove unused code

--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -25,11 +25,4 @@ class SolutionTests: XCTestCase {
     func testSolution_() {
         
     }
-
-//    func testPerformanceExample() throws {
-//        // This is an example of a performance test case.
-//        measure {
-//            // Put the code you want to measure the time of here.
-//        }
-//    }
 }
