0ddac9e0074746270867faee9e104b83cc99de42
Nihad Samedov <the_nihad@icloud.com>
2021-12-26 18:49:54 +0300

    Solution

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,25 @@
 import Foundation
 
 class Solution {
-    
+    func lengthOfLIS(_ nums: [Int]) -> Int {
+        var hashMap: [Int: Int] = [:]
+
+        for i in 0..<nums.count {
+            hashMap[i] = 1
+        }
+
+        var i = nums.count - 2
+        while i >= 0 {
+            var R = i + 1
+            while R < nums.count {
+                if nums[i] < nums[R] {
+                    hashMap[i] = max(hashMap[i]!, hashMap[R]! + 1)
+                }
+                R += 1
+            }
+            i -= 1
+        }
+
+        return hashMap.values.max()!
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,24 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_longestIncreasingSubsequence() {
+        // given
+        let nums1 = [10, 9, 2, 5, 3, 7, 101, 18]
+        let nums2 = [0, 1, 0, 3, 2, 3]
+        let nums3 = [7, 7, 7, 7, 7, 7, 7]
+
+        // when
+        let result1 = sut.lengthOfLIS(nums1)
+        let result2 = sut.lengthOfLIS(nums2)
+        let result3 = sut.lengthOfLIS(nums3)
+
+        // then
+        let expected1 = 4
+        let expected2 = 4
+        let expected3 = 1
+
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
+        XCTAssertEqual(result3, expected3)
     }
 }
