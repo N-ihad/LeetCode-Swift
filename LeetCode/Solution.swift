42651a5a61250c6d9e3fa8a62c48a6d70d957ce9
Nihad Samedov <the_nihad@icloud.com>
2021-12-26 17:45:15 +0300

    Yet to be solved

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,89 @@
 
 import Foundation
 
-class Solution {
-    
+class Node {
+    var next: Node?
+    var value: Triplet
+
+    init(value: Triplet) {
+        self.value = value
+    }
+}
+
+class LinkedList {
+    var head: Node
+    var tail: Node
+
+    init(head: Node) {
+        self.head = head
+        tail = head
+    }
+
+    func insert(_ node: Node) {
+        tail.next = node
+        tail = node
+    }
+}
+
+class Triplet {
+    let key: Int
+    var value: Int
+    var usedCount: Int
+
+    init() {
+        key = -2
+        value = -2
+        usedCount = 0
+    }
+
+    init(key: Int, value: Int) {
+        self.key = key
+        self.value = value
+        usedCount = 0
+    }
+}
+
+class LRUCache {
+
+    let capacity: Int
+    var elements: [Triplet?]
+    var minUsedKey: Int
+    var minUsedTimes: Int
+
+    init(_ capacity: Int) {
+        self.capacity = capacity
+        elements = Array<Triplet?>(repeating: nil, count: capacity)
+        minUsedKey = -2
+        minUsedTimes = -2
+    }
+
+    func get(_ key: Int) -> Int {
+        let i = index(hash(key))
+
+        if let element = elements[i] {
+            element.usedCount += 1
+            
+            return element.value
+        } else {
+            return -1
+        }
+    }
+
+    func put(_ key: Int, _ value: Int) {
+        let i = index(hash(key))
+
+        if let element = elements[i] {
+            element.value = value
+        } else {
+            elements[i] = Triplet(key: key, value: value)
+        }
+    }
+
+    func hash(_ key: Int) -> Int {
+        return key
+    }
+
+    func index(_ hash: Int) -> Int {
+        return hash % capacity
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -8,13 +8,19 @@
 import XCTest
 @testable import LeetCode
 
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * let obj = LRUCache(capacity)
+ * let ret_1: Int = obj.get(key)
+ * obj.put(key, value)
+ */
+
 class SolutionTests: XCTestCase {
 
-    var sut: Solution!
+    var sut: LRUCache!
 
     override func setUp() {
         super.setUp()
-        sut = Solution()
     }
 
     override func tearDown() {
@@ -22,7 +28,30 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_LRUCache() {
+        // given
+        sut = LRUCache(2)
+        sut.put(1, 1)
+        sut.put(2, 2)
+        let result1 = sut.get(1)
+        sut.put(3, 3)
+        let result2 = sut.get(2)
+        sut.put(4, 4)
+        let result3 = sut.get(1)
+        let result4 = sut.get(3)
+        let result5 = sut.get(4)
+
+        // then
+        let expected1 = 1
+        let expected2 = -1
+        let expected3 = -1
+        let expected4 = 3
+        let expected5 = 4
+
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
+        XCTAssertEqual(result3, expected3)
+        XCTAssertEqual(result4, expected4)
+        XCTAssertEqual(result5, expected5)
     }
 }
