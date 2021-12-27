11dca282c67cea230539e9f1cd7ff6c92bb6e465
Nihad Samedov <the_nihad@icloud.com>
2021-12-27 18:02:58 +0300

    Solution

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,20 @@
 import Foundation
 
 class Solution {
-    
+    func maxSlidingWindow(_ nums: [Int], _ k: Int) -> [Int] {
+        let n = nums.count - k + 1
+        var result: [Int] = []
+
+        for i in 0..<n {
+            var max = nums[i]
+            for j in i+1..<i+k {
+                if nums[j] > max {
+                    max = nums[j]
+                }
+            }
+            result.append(max)
+        }
+
+        return result
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,23 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_maxSlidingWindow() {
+        // given
+        let nums1 = [1, 3, -1, -3, 5, 3, 6, 7]
+        let k1 = 3
+
+        let nums2 = [1]
+        let k2 = 1
+
+        // when
+        let result1 = sut.maxSlidingWindow(nums1, k1)
+        let result2 = sut.maxSlidingWindow(nums2, k2)
+
+        // then
+        let expected1 = [3, 3, 5, 5, 6, 7]
+        let expected2 = [1]
+
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
     }
 }
