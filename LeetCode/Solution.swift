4e1735a68793fff14d5262fefd063d08320125b6
Nihad Samedov <the_nihad@icloud.com>
2021-12-29 19:46:39 +0300

    Yet to be solved: need to come up with a solution to check EVERY node in a tree instead of only root node

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,40 @@
 
 import Foundation
 
+class TreeNode {
+    var val: Int
+    var left: TreeNode?
+    var right: TreeNode?
+
+    init() {
+        val = 0
+        left = nil
+        right = nil
+    }
+
+    init(_ val: Int) {
+        self.val = val
+        left = nil
+        right = nil
+    }
+
+    init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+// [4,-7,-3,null,null,-9,-3,9,-7,-4,null,6,null,-6,-6,null,null,0,6,5,null,9,null,null,-1,-4,null,null,null,-2]
 class Solution {
-    
+    func dfs(_ node: TreeNode?) -> Int {
+        if node == nil {
+            return 0
+        }
+
+        return max(1 + dfs(node?.left), 1 + dfs(node?.right))
+    }
+
+    func diameterOfBinaryTree(_ root: TreeNode?) -> Int {
+        return dfs(root?.right) + dfs(root?.left)
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,20 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_diameterOfBinaryTree() {
+        // given
+        let root1 = TreeNode(1, TreeNode(2, TreeNode(4), TreeNode(5)), TreeNode(3))
+        let root2 = TreeNode(1, TreeNode(2), nil)
+
+        // when
+        let result1 = sut.diameterOfBinaryTree(root1)
+        let result2 = sut.diameterOfBinaryTree(root2)
+
+        // then
+        let expected1 = 3
+        let expected2 = 1
+
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
     }
 }
