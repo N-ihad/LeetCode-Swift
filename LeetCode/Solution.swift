36f71550c2dd447d51409dbcedadd35d598d47eb
Nihad Samedov <the_nihad@icloud.com>
2021-12-31 01:20:15 +0300

    Yet to be solved: coz Im lazy

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,39 @@
 
 import Foundation
 
+class ListNode {
+    var val: Int
+    var next: ListNode?
+
+    init() {
+        val = 0
+        next = nil
+    }
+
+    init(_ val: Int) {
+        self.val = val
+        next = nil
+    }
+
+    init(_ val: Int, _ next: ListNode?) {
+        self.val = val
+        self.next = next
+    }
+}
+
 class Solution {
-    
+    func reverseList(_ head: ListNode?) -> ListNode? {
+        var head = head
+        var newHead: ListNode? = head
+        while head != nil {
+            if head!.next == nil {
+                newHead = head
+            }
+            let curHead = head
+            head = head?.next?.next
+            curHead?.next = nil
+        }
+
+        return newHead
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,34 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_reverseLinkedList() {
+        // given
+        let head1 = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5, nil)))))
+        let head2 = ListNode(1, ListNode(2, nil))
+        let head3: ListNode? = nil
+
+        // when
+        let result1 = sut.reverseList(head1)
+        let result2 = sut.reverseList(head2)
+        let result3 = sut.reverseList(head3)
+
+//        // then
+//        let expected1 = ListNode(5, ListNode(4, ListNode(3, ListNode(2, ListNode(1, nil)))))
+//        let expected2 = ListNode(2, ListNode(1, nil))
+//        let expected3: ListNode? = nil
+
+        printList(result1, label: "result1, should be 5 4 3 2 1")
+        printList(result2, label: "result2, should be 2 1")
+        printList(result3, label: "result3, should be empty")
+    }
+
+    func printList(_ head: ListNode?, label: String) {
+        var head = head
+        print("***** \(label) *****")
+        while head != nil {
+            print(head!.val, terminator: " ")
+            head = head!.next
+        }
+        print("***** \(label) *****")
     }
 }
