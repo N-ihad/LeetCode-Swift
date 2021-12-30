3eb2fdc92c7e83e92767f12e3af2aa8356f8eb68
Nihad Samedov <the_nihad@icloud.com>
2021-12-31 01:47:37 +0300

    Solution

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,19 @@
 import Foundation
 
 class Solution {
-    
+    func generate(_ numRows: Int) -> [[Int]] {
+        var result: [[Int]] = []
+        for i in 0..<numRows {
+            if i > 1 {
+                var localResult: [Int] = [1, 1]
+                for j in 1..<i {
+                    localResult.insert(result[i-1][j-1] + result[i-1][j], at: j)
+                }
+                result.append(localResult)
+            } else {
+                result.append(i == 0 ? [1] : [1, 1])
+            }
+        }
+        return result
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,20 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_pascalsTriangle() {
+        // given
+        let numRows1 = 5
+        let numRows2 = 1
+
+        // when
+        let result1 = sut.generate(numRows1)
+        let result2 = sut.generate(numRows2)
+
+        // then
+        let expected1 = [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+        let expected2 = [[1]]
+
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
     }
 }
