174f1bfd8250fa535517450a79896a2137e4f2c1
Nihad Samedov <the_nihad@icloud.com>
2021-12-31 18:14:42 +0300

    Solution (via XOR)

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,7 @@
 import Foundation
 
 class Solution {
-    
+    func singleNumber(_ nums: [Int]) -> Int {
+        return nums.reduce(0) { $0 ^ $1 }
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,24 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_singleNumber() {
+        // given
+        let nums1 = [2,2,1]
+        let nums2 = [4,1,2,1,2]
+        let nums3 = [1]
+
+        // when
+        let result1 = sut.singleNumber(nums1)
+        let result2 = sut.singleNumber(nums2)
+        let result3 = sut.singleNumber(nums3)
+
+        // then
+        let expected1 = 1
+        let expected2 = 4
+        let expected3 = 1
+
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
+        XCTAssertEqual(result3, expected3)
     }
 }
