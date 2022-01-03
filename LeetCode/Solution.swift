98bac42a97866b8e3231326b338b892074434e88
Nihad Samedov <the_nihad@icloud.com>
2022-01-03 04:27:33 +0300

    Solution: Floyd's Tortoise and Hare algorithm

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,29 @@
 
 import Foundation
 
+class ListNode {
+    var val: Int
+    var next: ListNode?
+
+    init(_ val: Int, next: ListNode? = nil) {
+        self.val = val
+        self.next = next
+    }
+}
+
 class Solution {
-    
+    func hasCycle(_ head: ListNode?) -> Bool {
+        var slow: ListNode? = head
+        var fast: ListNode? = head?.next
+
+        while slow != nil {
+            if slow === fast {
+                return true
+            }
+            slow = slow?.next
+            fast = fast?.next?.next
+        }
+
+        return false
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,36 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_hasCycle() {
+        // given
+        let node11: ListNode? = ListNode(3)
+        let node12: ListNode? = ListNode(2)
+        let node13: ListNode? = ListNode(0)
+        let node14: ListNode? = ListNode(-4)
+        node11?.next = node12
+        node12?.next = node13
+        node13?.next = node14
+        node14?.next = node12
+
+        let node21: ListNode? = ListNode(1)
+        let node22: ListNode? = ListNode(2)
+        node21?.next = node22
+        node22?.next = node21
+
+        let node31: ListNode? = ListNode(1)
+
+        // when
+        let result1 = sut.hasCycle(node11)
+        let result2 = sut.hasCycle(node21)
+        let result3 = sut.hasCycle(node31)
+
+        // then
+        let expected1 = true
+        let expected2 = true
+        let expected3 = false
+
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
+        XCTAssertEqual(result3, expected3)
     }
 }
