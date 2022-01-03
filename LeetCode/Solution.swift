f63bbef4263f10ebdd41db5b85df8a17031ad373
Nihad Samedov <the_nihad@icloud.com>
2022-01-03 04:50:52 +0300

    Solution

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,18 @@
 import Foundation
 
 class Solution {
-    
+    func maxProfit(_ prices: [Int]) -> Int {
+        var minPrice = Int.max
+        var maxProfit = 0
+
+        for i in 0..<prices.count {
+            if prices[i] < minPrice {
+                minPrice = prices[i]
+            } else if prices[i] - minPrice > maxProfit {
+                maxProfit = prices[i] - minPrice
+            }
+        }
+
+        return maxProfit
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,20 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_maxProfit() {
+        // given
+        let prices1 = [7, 1, 5, 3, 6, 4]
+        let prices2 = [7, 6, 4, 3, 1]
+
+        // when
+        let result1 = sut.maxProfit(prices1)
+        let result2 = sut.maxProfit(prices2)
+
+        // then
+        let expected1 = 5
+        let expected2 = 0
+
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
     }
 }
