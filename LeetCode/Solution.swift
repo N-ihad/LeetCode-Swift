b770f32de77339d2d813a7c150938854ce3b92ae
Nihad Samedov <the_nihad@icloud.com>
2022-01-03 21:50:32 +0300

    Solution: 2 pointers (left L and right R)

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,18 @@
 import Foundation
 
 class Solution {
-    
+    func moveZeroes(_ nums: inout [Int]) {
+        var L = 0
+        var R = 0
+
+        while R != nums.count {
+            if nums[R] != 0 {
+                let temp = nums[L]
+                nums[L] = nums[R]
+                nums[R] = temp
+                L += 1
+            }
+            R += 1
+        }
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,20 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_moveZeroes() {
+        // given
+        var nums1 = [0, 1, 0, 3, 12]
+        var nums2 = [0]
+
+        // when
+        sut.moveZeroes(&nums1)
+        sut.moveZeroes(&nums2)
+
+        // then
+        let expected1 = [1, 3, 12, 0, 0]
+        let expected2 = [0]
+
+        XCTAssertEqual(nums1, expected1)
+        XCTAssertEqual(nums2, expected2)
     }
 }
