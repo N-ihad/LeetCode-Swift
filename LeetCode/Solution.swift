4f40850d9edda184bf04d8c28bd569f6ea6ac23e
Nihad Samedov <the_nihad@icloud.com>
2022-01-04 19:52:52 +0300

    Solution

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,52 @@
 
 import Foundation
 
+class ListNode {
+    var val: Int
+    var next: ListNode?
+
+    init(_ val: Int, _ next: ListNode? = nil) {
+        self.val = val
+        self.next = next
+    }
+}
+
+func printList(_ root: ListNode?) {
+    var head = root
+    if head == nil {
+        print("[]")
+        return
+    } else {
+        print("[ ", terminator: "")
+    }
+    while head != nil {
+        print(head!.val, terminator: " ")
+        head = head?.next
+    }
+    print("]")
+}
+
 class Solution {
-    
+    func removeNthFromEnd(_ head: ListNode?, _ n: Int) -> ListNode? {
+        let originalHead = head
+        var counter = 0
+        var hashMap: [Int: ListNode?] = [:]
+
+        var head = head
+        while head != nil {
+            counter += 1
+            hashMap[counter] = head
+            head = head?.next
+        }
+
+        let elementToRemove = counter + 1 - n
+
+        if n == counter {
+            return hashMap[elementToRemove]??.next
+        }
+
+        hashMap[elementToRemove-1]??.next = hashMap[elementToRemove+1] as? ListNode
+
+        return originalHead
+    }
 }
--- LeetCode/main.swift
+++ LeetCode/main.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+let head1: ListNode? = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
+let n1 = 2
+
+let head2: ListNode? = ListNode(1)
+let n2 = 1
+
+let head3: ListNode? = ListNode(1, ListNode(2))
+let n3 = 1
+
+let s = Solution()
+printList(s.removeNthFromEnd(head1, n1))
+printList(s.removeNthFromEnd(head2, n2))
+printList(s.removeNthFromEnd(head3, n3))
