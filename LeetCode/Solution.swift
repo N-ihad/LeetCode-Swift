65cdbfeb63d94a6d3849ff82a4c20eb8478f71b5
Nihad Samedov <the_nihad@icloud.com>
2022-01-04 18:57:38 +0300

    Solution

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,29 @@
 
 import Foundation
 
+class TreeNode {
+    var val: Int
+    var left: TreeNode?
+    var right: TreeNode?
+
+    init(_ val: Int = 0, _ left: TreeNode? = nil, _ right: TreeNode? = nil) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
 class Solution {
-    
+    func isSymmetric(_ root: TreeNode?) -> Bool {
+        func isMirror(left: TreeNode?, right: TreeNode?) -> Bool {
+            if left == nil && right == nil { return true }
+            guard let left = left,
+                  let right = right else {
+                return false
+            }
+
+            return left.val == right.val && isMirror(left: left.left, right: right.right) && isMirror(left: left.right, right: right.left)
+        }
+        return isMirror(left: root, right: root)
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -23,6 +23,19 @@ class SolutionTests: XCTestCase {
     }
 
     func testSolution_() {
-        
+        // given
+        let root1 = TreeNode(1, TreeNode(2, TreeNode(3), TreeNode(4)), TreeNode(2, TreeNode(4), TreeNode(3)))
+        let root2 = TreeNode(1, TreeNode(2, nil, TreeNode(3)), TreeNode(2, nil, TreeNode(3)))
+
+        // when
+        let result1 = sut.isSymmetric(root1)
+        let result2 = sut.isSymmetric(root2)
+
+        // then
+        let expected1 = true
+        let expected2 = false
+
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
     }
 }
