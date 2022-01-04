42a53a9c5a0ce7fbaef796d3c577f3b070d0b810
Nihad Samedov <the_nihad@icloud.com>
2022-01-04 18:21:56 +0300

    Yet to be solved

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,40 @@
 
 import Foundation
 
+class Vertex: Hashable {
+
+    var id: Int
+    var adjacentVertices: [Vertex]
+
+    init(_ id: Int, pointsToVertex vertex: Vertex? = nil) {
+        self.id = id
+        adjacentVertices = []
+    }
+
+    static func == (lhs: Vertex, rhs: Vertex) -> Bool {
+        return lhs.id == rhs.id
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+}
+
 class Solution {
-    
+    func canFinish(_ numCourses: Int, _ prerequisites: [[Int]]) -> Bool {
+        var hashMap: [Vertex: Bool] = [:]
+        let vertices = (0..<numCourses).map { vertex -> Vertex in
+            let vertex = Vertex(vertex)
+            hashMap[vertex] = false
+            return vertex
+        }
+
+        for i in 0..<prerequisites.count {
+            vertices[prerequisites[i][0]].adjacentVertices.append(vertices[prerequisites[i][1]])
+        }
+
+
+
+        return true
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -23,6 +23,22 @@ class SolutionTests: XCTestCase {
     }
 
     func testSolution_() {
-        
+        // given
+        let numCourses1 = 2
+        let prerequisites1 = [[1, 0]]
+
+        let numCourses2 = 2
+        let prerequisites2 = [[1, 0], [0, 1]]
+
+        // when
+        let result1 = sut.canFinish(numCourses1, prerequisites1)
+        let result2 = sut.canFinish(numCourses2, prerequisites2)
+
+        // then
+        let expected1 = true
+        let expected2 = false
+
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
     }
 }
