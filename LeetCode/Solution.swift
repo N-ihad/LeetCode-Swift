df0e355aaaebed666fcb2951edc5e0dec194ee46
Nihad Samedov <the_nihad@icloud.com>
2022-01-06 18:25:30 +0300

    Solution

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,22 @@
 import Foundation
 
 class Solution {
-    
+    func reverseWords(_ s: String) -> String {
+        let s = Array(s)
+        var result = ""
+
+        var i = 0
+        while i < s.count {
+            while i < s.count && s[i] == " " { i += 1 }
+            if i == s.count { break }
+            var j = i + 1
+            while j < s.count && s[j] != " " { j += 1 }
+
+            let word = String(s[i..<j])
+            result = word + (result.isEmpty ? "" : " " + result)
+            i = j + 1
+        }
+
+        return result
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,24 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_reverseString() {
+        // given
+        let str1 = "the sky is blue"
+        let str2 = "  hello world  "
+        let str3 = "a good   example"
+
+        // when
+        let result1 = sut.reverseWords(str1)
+        let result2 = sut.reverseWords(str2)
+        let result3 = sut.reverseWords(str3)
+
+        // then
+        let expected1 = "blue is sky the"
+        let expected2 = "world hello"
+        let expected3 = "example good a"
+
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
+        XCTAssertEqual(result3, expected3)
     }
 }
