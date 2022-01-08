cb75f4fdcd1ff04e33df29e86d4965589dd1673b
Nihad Samedov <the_nihad@icloud.com>
2022-01-09 00:30:50 +0300

    Add 1 more test case

--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -26,16 +26,20 @@ class SolutionTests: XCTestCase {
         // given
         let n1 = 3
         let n2 = 1
+        let n3 = 2
 
         // when
         let result1 = sut.generateParenthesis(n1)
         let result2 = sut.generateParenthesis(n2)
+        let result3 = sut.generateParenthesis(n3)
 
         // then
         let expected1 = ["((()))","(()())","(())()","()(())","()()()"]
         let expected2 = ["()"]
+        let expected3 = ["(())", "()()"]
 
         XCTAssertEqual(result1, expected1)
         XCTAssertEqual(result2, expected2)
+        XCTAssertEqual(result3, expected3)
     }
 }
