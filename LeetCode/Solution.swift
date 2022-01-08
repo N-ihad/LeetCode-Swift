64b92da0b389c06e63eea87b64bcb478c10d04b9
Nihad Samedov <the_nihad@icloud.com>
2022-01-08 18:53:34 +0300

    Solution

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -26,4 +26,31 @@ class Solution {
 
         return result
     }
+
+    // MARK: - Attempt to do without sorting
+//    func groupAnagrams2(_ strs: [String]) -> [[String]] {
+//        var hashMap: [String: Int] = [:]
+//        var result: [[String]] = []
+//        var bucketIndex = 0
+//
+//        for str in strs {
+//            var _key: Double = 0
+//            for c in str {
+//                _key += Int(c.asciiValue!) + Double(c.asciiValue!)/(Double(c.asciiValue!)/10)%10 + Int(c.asciiValue!)%10
+//            }
+//            let key = String(_key)
+////            let key = String(str.reduce(0) {
+////                $0 + Int($1.asciiValue!)/100 + (Int($1.asciiValue!)/10)%10 + Int($1.asciiValue!)%10
+////            })
+//            if hashMap[key] == nil {
+//                hashMap[key] = bucketIndex
+//                result.append([str])
+//                bucketIndex += 1
+//            } else {
+//                result[hashMap[key]!].append(str)
+//            }
+//        }
+//
+//        return result
+//    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -27,19 +27,27 @@ class SolutionTests: XCTestCase {
         let strs1 = ["eat","tea","tan","ate","nat","bat"]
         let strs2 = [""]
         let strs3 = ["a"]
+        let strs4 = ["bdddddddddd","bbbbbbbbbbc"]
+        let strs5 = ["tao","pit","cam","aid","pro","dog"]
 
         // when
         let result1 = sut.groupAnagrams(strs1)
         let result2 = sut.groupAnagrams(strs2)
         let result3 = sut.groupAnagrams(strs3)
+        let result4 = sut.groupAnagrams(strs4)
+        let result5 = sut.groupAnagrams(strs5)
 
         // then
         let expected1 = [["eat","tea","ate"],["tan","nat"],["bat"]]
         let expected2 = [[""]]
         let expected3 = [["a"]]
+        let expected4 = [["bdddddddddd"],["bbbbbbbbbbc"]]
+        let expected5 = [["dog"],["pro"],["aid"],["cam"],["pit"],["tao"]]
 
         XCTAssertEqual(result1, expected1)
         XCTAssertEqual(result2, expected2)
         XCTAssertEqual(result3, expected3)
+        XCTAssertEqual(result4, expected4)
+        XCTAssertEqual(result5, expected5)
     }
 }
