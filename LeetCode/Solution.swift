b276204044d0f2c9d35b6a8875fcb045354b0502
Nihad Samedov <the_nihad@icloud.com>
2022-01-09 17:36:23 +0300

    Solution

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,16 @@
 import Foundation
 
 class Solution {
-    
+    func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
+        if nums.isEmpty { return 0 }
+
+        var L = 0
+        for i in 0..<nums.count {
+            if nums[i] == val { continue }
+            nums[L] = nums[i]
+            L += 1
+        }
+
+        return L
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,28 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_removeElement() {
+        // given
+        var nums1 = [3,2,2,3]
+        let val1 = 3
+
+        var nums2 = [0,1,2,2,3,0,4,2]
+        let val2 = 2
+
+        // when
+        let k1 = sut.removeElement(&nums1, val1)
+        let k2 = sut.removeElement(&nums2, val2)
+
+        // then
+        let expected1 = [2,2,0,0]
+        let kk1 = 2
+
+        let expected2 = [0,1,3,0,4,0,0,0]
+        let kk2 = 5
+
+        XCTAssertEqual(k1, kk1)
+        XCTAssertEqual(k2, kk2)
+        XCTAssertEqual(nums1[0..<kk1], expected1[0..<k1])
+        XCTAssertEqual(nums2[0..<kk2], expected2[0..<k2])
     }
 }
