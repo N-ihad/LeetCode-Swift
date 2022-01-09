b2de3cc3e0287510a97fa48c87a1d25773fd6d35
Nihad Samedov <the_nihad@icloud.com>
2022-01-09 17:21:31 +0300

    Solution

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,19 @@
 import Foundation
 
 class Solution {
-    
+    func removeDuplicates(_ nums: inout [Int]) -> Int {
+        if nums.isEmpty { return 0 }
+
+        var L = 0
+        var seen = nums[0]
+
+        for i in 0..<nums.count {
+            if nums[i] == seen { continue }
+            L += 1
+            seen = nums[i]
+            nums[L] = nums[i]
+        }
+
+        return L + 1
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,20 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_removeDuplicatesFromSortedArray() {
+        // given
+        var nums1 = [1,1,2]
+        var nums2 = [0,0,1,1,1,2,2,3,3,4]
+
+        // when
+        let k1 = sut.removeDuplicates(&nums1)
+        let k2 = sut.removeDuplicates(&nums2)
+
+        // then
+        let expected1 = [1, 2, 0]
+        let expected2 = [0, 1, 2, 3, 4, 0, 0, 0, 0, 0]
+
+        XCTAssertEqual(nums1[0..<k1], expected1[0..<k1])
+        XCTAssertEqual(nums2[0..<k2], expected2[0..<k2])
     }
 }
