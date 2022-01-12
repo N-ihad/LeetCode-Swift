1733b80c5d1049029fa24feecb120c2b5aa52008
Nihad Samedov <the_nihad@icloud.com>
2022-01-12 19:42:56 +0300

    Solution

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,43 @@
 import Foundation
 
 class Solution {
-    
+    func setZeroes(_ matrix: inout [[Int]]) {
+        var buff = -1
+
+        for i in 0..<matrix.count {
+            for j in 0..<matrix[0].count {
+                if i == 0 && matrix[i][j] == 0 { buff = 0 }
+                if i != 0 && matrix[i][j] == 0 {
+                    matrix[0][j] = 0
+                    matrix[i][0] = 0
+                }
+            }
+        }
+
+        for i in 1..<matrix.count {
+            for j in 1..<matrix[0].count {
+                if matrix[0][j] == 0 || matrix[i][0] == 0 { matrix[i][j] = 0 }
+            }
+        }
+
+        for i in 0..<matrix.count {
+            if matrix[0][0] == 0 { matrix[i][0] = 0 }
+        }
+
+        for j in 0..<matrix[0].count {
+            if buff == 0 { matrix[0][j] = 0 }
+        }
+    }
+
+    func printMatrix(_ m: [[Int]]) {
+        print("[")
+        for i in 0..<m.count {
+            print("[", terminator: "")
+            for j in 0..<m[0].count {
+                print(m[i][j], terminator: " ")
+            }
+            print("]")
+        }
+        print("]")
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,42 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_setZeroes() {
+        // given
+        var matrix1 = [[1,1,1],[1,0,1],[1,1,1]]
+        var matrix2 = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+        var matrix3 = [[1,0,3]]
+        var matrix4 = [[-4,-2147483648,6,-7,0],[-8,6,-8,-6,0],[2147483647,2,-9,-6,-10]]
+
+        // when
+        sut.setZeroes(&matrix1)
+        sut.setZeroes(&matrix2)
+        sut.setZeroes(&matrix3)
+        sut.setZeroes(&matrix4)
+
+        // then
+        let expected1 = [[1,0,1],[0,0,0],[1,0,1]]
+        let expected2 = [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+        let expected3 = [[0,0,0]]
+        let expected4 = [[0,0,0,0,0],[0,0,0,0,0],[2147483647,2,-9,-6,0]]
+
+        XCTAssertTrue(compareMatrices(matrix1, expected1))
+        XCTAssertTrue(compareMatrices(matrix2, expected2))
+        XCTAssertTrue(compareMatrices(matrix3, expected3))
+        XCTAssertTrue(compareMatrices(matrix4, expected4))
+    }
+
+    func compareMatrices(_ m1: [[Int]], _ m2: [[Int]]) -> Bool {
+        if (m1.count != m2.count) || (m1.isEmpty && !m2.isEmpty) || (!m1.isEmpty && m2.isEmpty) {
+            return false
+        }
+
+        for i in 0..<m1.count {
+            for j in 0..<m1[0].count {
+                if m1[i][j] != m2[i][j] { return false }
+            }
+        }
+
+        return true
     }
 }
