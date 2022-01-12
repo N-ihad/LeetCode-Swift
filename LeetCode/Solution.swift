9d6b568466b353732670a2ba8abaab6315c5164b
Nihad Samedov <the_nihad@icloud.com>
2022-01-12 17:54:37 +0300

    Yet to be solved coz im lazy

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,28 @@
 
 import Foundation
 
-class Solution {
-    
+class Solution { // dvdg
+    func lengthOfLongestSubstring(_ s: String) -> Int {
+        let str = Array(s)
+        if s.isEmpty { return 0 }
+
+        var i = 0
+        var j = 0
+        var max = 0
+        var hashMap: [Character: Int] = [:]
+        while i < s.count {
+            while j != s.count && hashMap[str[j]] == nil {
+                hashMap[str[j]] = j
+                j += 1
+            }
+            max = j - i > max ? j - i : max
+            var dsdsd = Set([1, 2, 3,3 ])
+            dsdsd.remove(1)
+            let ii = i
+            i = (j < s.count && hashMap[str[j]] != nil) ? hashMap[str[j]]! + 1 : i + 1
+            for k in ii...j where k < s.count { hashMap[str[k]] = nil }
+        }
+
+        return max
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,40 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_lengthOfLongestSubstring() {
+        // given
+        let str1 = "abcabcbb"
+        let str2 = "bbbbb"
+        let str3 = "pwwkew"
+        let str4 = "aab"
+        let str5 = "dvdf"
+        let str6 = "cdd"
+        let str7 = "abba"
+
+        // when
+        let result7 = sut.lengthOfLongestSubstring(str7)
+        let result1 = sut.lengthOfLongestSubstring(str1)
+        let result2 = sut.lengthOfLongestSubstring(str2)
+        let result3 = sut.lengthOfLongestSubstring(str3)
+        let result4 = sut.lengthOfLongestSubstring(str4)
+        let result5 = sut.lengthOfLongestSubstring(str5)
+        let result6 = sut.lengthOfLongestSubstring(str6)
+
+        // then
+        let expected1 = 3
+        let expected2 = 1
+        let expected3 = 3
+        let expected4 = 2
+        let expected5 = 3
+        let expected6 = 2
+        let expected7 = 2
+
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
+        XCTAssertEqual(result3, expected3)
+        XCTAssertEqual(result4, expected4)
+        XCTAssertEqual(result5, expected5)
+        XCTAssertEqual(result6, expected6)
+        XCTAssertEqual(result7, expected7)
     }
 }
