2f2efb36cecf6ce7583bede0cac0d9eddbeb6aeb
Nihad Samedov <the_nihad@icloud.com>
2022-01-13 18:16:22 +0300

    Solution

--- LeetCode/main.swift
+++ LeetCode/main.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+class MinStack {
+    var min = Int.max
+    var stack: [Int] = []
+
+    init() {}
+
+    func push(_ val: Int) {
+        min = val < min ? val : min
+        stack.append(val)
+    }
+
+    func pop() {
+        stack.removeLast()
+        min = stack.isEmpty ? Int.max : stack.min()!
+    }
+
+    func top() -> Int {
+        return stack.last!
+    }
+
+    func getMin() -> Int {
+        return min
+    }
+}
