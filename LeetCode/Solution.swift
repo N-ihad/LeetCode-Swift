f71e13a27cbc900bad493103ee25891febec8e10
Nihad Samedov <the_nihad@icloud.com>
2022-01-13 05:17:15 +0300

    Solution: recursive (improved: less memory taken)

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -9,18 +9,14 @@ import Foundation
 
 class Solution {
     func uniquePaths(_ m: Int, _ n: Int) -> Int {
-        var res = 0
+        func BFS(_ mm: Int, _ nn: Int) -> Int {
+            if mm == m && nn == n { return 1 }
+            if mm == m { return 1 }
+            if nn == n { return 1 }
 
-        func BFS(_ mm: Int, _ nn: Int) {
-            if mm == m && nn == n { res += 1; return }
-            if mm == m { res += 1; return }
-            if nn == n { res += 1; return }
-            
-            BFS(mm + 1, nn)
-            BFS(mm, nn + 1)
+            return BFS(mm + 1, nn) + BFS(mm, nn + 1)
         }
-        BFS(1, 1)
 
-        return res
+        return BFS(1, 1)
     }
 }
