ccd2d29b72553d901302e3f41b3118db82aaecb2
Nihad Samedov <the_nihad@icloud.com>
2022-01-13 17:49:07 +0300

    Solution: DFS

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,22 @@
 import Foundation
 
 class Solution {
-    
+    func subsets(_ nums: [Int]) -> [[Int]] {
+        var res = [[Int]]()
+        var subset = [Int]()
+
+        func dfs(_ i: Int) {
+            if i >= nums.count {
+                res.append(subset)
+                return
+            }
+            subset.append(nums[i])
+            dfs(i + 1)
+            subset.removeLast()
+            dfs(i + 1)
+        }
+
+        dfs(0)
+        return res
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,20 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_subsets() {
+        // given
+        let nums1 = [1,2,3]
+        let nums2 = [0]
+
+        // when
+        let result1 = sut.subsets(nums1)
+        let result2 = sut.subsets(nums2)
+
+        // then
+        let expected1 = [[1, 2, 3], [1, 2], [1, 3], [1], [2, 3], [2], [3], []]
+        let expected2 = [[0], []]
+
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
     }
 }
