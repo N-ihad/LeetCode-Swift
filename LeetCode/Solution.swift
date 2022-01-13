86b89f4dcca8a64541592d6e6e449bd3f14ee0c5
Nihad Samedov <the_nihad@icloud.com>
2022-01-14 02:23:27 +0300

    Solution: DFS recursively with memoization

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,27 @@
 import Foundation
 
 class Solution {
-    
+    // MARK: - Recursive with memoization
+    func climbStairs(_ n: Int) -> Int {
+        var cache = Array<Int?>(repeating: nil, count: n+2)
+        func climb(_ nn: Int) -> Int {
+            if let val = cache[nn] { return val }
+            if nn > n { return 0 }
+            if nn == n { return 1 }
+            cache[nn+1] = climb(nn + 1)
+            cache[nn+2] = climb(nn + 2)
+            return cache[nn+1]! + cache[nn+2]!
+        }
+        return climb(0)
+    }
+
+    // MARK: - Recursive without memoization
+//    func climbStairs(_ n: Int) -> Int {
+//        func climb(_ nn: Int) -> Int {
+//            if nn > n { return 0 }
+//            if nn == n { return 1 }
+//            return climb(nn + 1) + climb(nn + 2)
+//        }
+//        return climb(0)
+//    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,20 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_climbStairs() {
+        // given
+        let n1 = 2
+        let n2 = 3
+
+        // when
+        let result1 = sut.climbStairs(n1)
+        let result2 = sut.climbStairs(n2)
+
+        // then
+        let expected1 = 2
+        let expected2 = 3
+
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
     }
 }
