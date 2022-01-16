c689f9e88fe0cd0f66f02233f86c79205ebbedd9
Nihad Samedov <the_nihad@icloud.com>
2022-01-16 23:42:15 +0300

    Solution: formula of sum from 1 to n

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,12 @@
 import Foundation
 
 class Solution {
-    
+    func missingNumber(_ nums: [Int]) -> Int {
+        let trueSum = (nums.count*(nums.count + 1))/2
+        var sum = 0
+        for i in 0..<nums.count {
+            sum += nums[i]
+        }
+        return trueSum - sum
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,24 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_missingNumber() {
+        // given
+        let nums1 = [3,0,1]
+        let nums2 = [0,1]
+        let nums3 = [9,6,4,2,3,5,7,0,1]
+
+        // when
+        let result1 = sut.missingNumber(nums1)
+        let result2 = sut.missingNumber(nums2)
+        let result3 = sut.missingNumber(nums3)
+
+        // then
+        let expected1 = 2
+        let expected2 = 2
+        let expected3 = 8
+
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
+        XCTAssertEqual(result3, expected3)
     }
 }
