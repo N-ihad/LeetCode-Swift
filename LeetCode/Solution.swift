8de043f922772b802ce5eed4c3f4c07761b9ca0b
Nihad Samedov <the_nihad@icloud.com>
2022-01-16 07:25:30 +0300

    Solution

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,16 @@
 import Foundation
 
 class Solution {
-    
+    func rotate(_ matrix: inout [[Int]]) {
+        for i in 0..<Int(floor(Double(matrix.count)/2)) {
+            for j in i..<matrix.count-1-i {
+                let temp1 = matrix[j][matrix.count-1-i]
+                let temp2 = matrix[matrix.count-1-j][i]
+                matrix[j][matrix.count-1-i] = matrix[i][j]
+                matrix[matrix.count-1-j][i] = matrix[matrix.count-1-i][matrix.count-1-j]
+                matrix[matrix.count-1-i][matrix.count-1-j] = temp1
+                matrix[i][j] = temp2
+            }
+        }
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,20 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_rotateImage() {
+        // given
+        var matrix2 = [[1,2,3],[4,5,6],[7,8,9]]
+        var matrix3 = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+
+        // when
+        sut.rotate(&matrix2)
+        sut.rotate(&matrix3)
+
+        // then
+        let expected2 = [[7,4,1],[8,5,2],[9,6,3]]
+        let expected3 = [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+
+        XCTAssertEqual(matrix2, expected2)
+        XCTAssertEqual(matrix3, expected3)
     }
 }
