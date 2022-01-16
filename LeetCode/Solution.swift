04f3669c1a69ce7364014a29462baada91d34a10
Nihad Samedov <the_nihad@icloud.com>
2022-01-16 23:31:15 +0300

    Initial setup

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,7 @@
 import Foundation
 
 class Solution {
-    
+    func searchMatrix(_ matrix: [[Int]], _ target: Int) -> Bool {
+        return false
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,21 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_searchMatrix() {
+        // given
+        let matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]
+        let target1 = 5
+        let target2 = 20
+
+        // when
+        let result1 = sut.searchMatrix(matrix, target1)
+        let result2 = sut.searchMatrix(matrix, target2)
+
+        // then
+        let expected1 = true
+        let expected2 = false
+
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
     }
 }
