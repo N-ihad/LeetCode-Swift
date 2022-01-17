12174dfc995b0f36241a56e5dddd17738848bcde
Nihad Samedov <the_nihad@icloud.com>
2022-01-17 17:51:31 +0300

    Solution: Kadanes algorithm

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,18 @@
 import Foundation
 
 class Solution {
-    
+    func maxSubArray(_ nums: [Int]) -> Int {
+        var absoluteMax = Int.min
+        var max = 0
+        for i in 0..<nums.count {
+            max += nums[i]
+            if nums[i] > max {
+                max = nums[i]
+            }
+            if max > absoluteMax {
+                absoluteMax = max
+            }
+        }
+        return absoluteMax
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,24 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_maxSubArray() {
+        // given
+        let nums1 = [-2,1,-3,4,-1,2,1,-5,4]
+        let nums2 = [1]
+        let nums3 = [5,4,-1,7,8]
+
+        // when
+        let result1 = sut.maxSubArray(nums1)
+        let result2 = sut.maxSubArray(nums2)
+        let result3 = sut.maxSubArray(nums3)
+
+        // then
+        let expected1 = 6
+        let expected2 = 1
+        let expected3 = 23
+
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
+        XCTAssertEqual(result3, expected3)
     }
 }
