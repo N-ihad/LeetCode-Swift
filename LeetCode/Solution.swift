9c342ff29b7bfbc290118dc0ad73fb3e0a7a3fad
Nihad Samedov <the_nihad@icloud.com>
2022-01-17 20:56:51 +0300

    Solution

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,20 @@
 import Foundation
 
 class Solution {
-    
+    func plusOne(_ digits: [Int]) -> [Int] {
+        var res: [Int] = []
+        var remainder = 1
+        for i in 0..<digits.count {
+            if digits[digits.count-1-i] + remainder == 10 {
+                res.insert(0, at: 0)
+            } else {
+                res.insert(digits[digits.count-1-i] + remainder, at: 0)
+                remainder = 0
+            }
+        }
+        if remainder == 1 {
+            res.insert(1, at: 0)
+        }
+        return res
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,24 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_plusOne() {
+        // given
+        let digits1 = [1,2,3]
+        let digits2 = [4,3,2,1]
+        let digits3 = [9]
+
+        // when
+        let result1 = sut.plusOne(digits1)
+        let result2 = sut.plusOne(digits2)
+        let result3 = sut.plusOne(digits3)
+
+        // then
+        let expected1 = [1,2,4]
+        let expected2 = [4,3,2,2]
+        let expected3 = [1,0]
+
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
+        XCTAssertEqual(result3, expected3)
     }
 }
