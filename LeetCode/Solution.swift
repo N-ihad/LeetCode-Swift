76f589fb3a69296c6b820a0b1a60dcf775999074
Nihad Samedov <the_nihad@icloud.com>
2022-01-17 16:49:32 +0300

    Solution

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -9,6 +9,15 @@ import Foundation
 
 class Solution {
     func searchMatrix(_ matrix: [[Int]], _ target: Int) -> Bool {
+        var (i, j) = (matrix.count-1, 0)
+        while i >= 0 && j < matrix[0].count {
+            if matrix[i][j] == target { return true }
+            if target > matrix[i][j] {
+                j += 1
+            } else {
+                i -= 1
+            }
+        }
         return false
     }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -24,19 +24,25 @@ class SolutionTests: XCTestCase {
 
     func testSolution_searchMatrix() {
         // given
-        let matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]
+        let matrix1 = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]
+        let matrix2 = [[-1,3]]
+
         let target1 = 5
         let target2 = 20
+        let target3 = 3
 
         // when
-        let result1 = sut.searchMatrix(matrix, target1)
-        let result2 = sut.searchMatrix(matrix, target2)
+        let result3 = sut.searchMatrix(matrix2, target3)
+        let result1 = sut.searchMatrix(matrix1, target1)
+        let result2 = sut.searchMatrix(matrix1, target2)
 
         // then
         let expected1 = true
         let expected2 = false
+        let expected3 = true
 
         XCTAssertEqual(result1, expected1)
         XCTAssertEqual(result2, expected2)
+        XCTAssertEqual(result3, expected3)
     }
 }
