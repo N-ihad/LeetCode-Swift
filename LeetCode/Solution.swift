dd161a4e7e224825bf1ebcefe8b60fc17c461c79
Nihad Samedov <the_nihad@icloud.com>
2022-01-17 19:40:27 +0300

    Initial setup

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,20 @@
 
 import Foundation
 
+class TreeNode {
+    var val: Int
+    var left: TreeNode?
+    var right: TreeNode?
+
+    init(_ val: Int = 0, _ left: TreeNode? = nil, _ right: TreeNode? = nil) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
 class Solution {
-    
+    func kthSmallest(_ root: TreeNode?, _ k: Int) -> Int {
+        return 0
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,23 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_kthSmallest() {
+        // given
+        let root1 = TreeNode(3, TreeNode(1, nil, TreeNode(2)), TreeNode(4))
+        let k1 = 1
+
+        let root2 = TreeNode(5, TreeNode(3, TreeNode(2, TreeNode(1)), TreeNode(4)), TreeNode(6))
+        let k2 = 3
+
+        // when
+        let result1 = sut.kthSmallest(root1, k1)
+        let result2 = sut.kthSmallest(root2, k2)
+
+        // then
+        let expected1 = 1
+        let expected2 = 3
+
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
     }
 }
