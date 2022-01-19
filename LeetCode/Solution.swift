ca58d06dcc08d9d1ebdd7a9d62e3dc5a8ba5919b
Nihad Samedov <the_nihad@icloud.com>
2022-01-19 14:44:27 +0300

    Solution: Prefix Postfix mutiplication

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -9,6 +9,21 @@ import Foundation
 
 class Solution {
     func productExceptSelf(_ nums: [Int]) -> [Int] {
-        return []
+        var res: [Int] = []
+        var buff = 1
+        res.append(buff)
+
+        for i in 0..<nums.count-1 {
+            buff *= nums[i]
+            res.append(buff)
+        }
+
+        buff = 1
+        for i in 1..<res.count {
+            buff *= nums[nums.count-i]
+            res[res.count-1-i] *= buff
+        }
+
+        return res
     }
 }
