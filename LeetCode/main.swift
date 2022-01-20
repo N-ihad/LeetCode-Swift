a85fdab7d29e82305487f220270bf7f0a9f550cd
Nihad Samedov <the_nihad@icloud.com>
2022-01-20 16:14:06 +0300

    Initial setup

--- LeetCode/main.swift
+++ LeetCode/main.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+class TreeNode {
+    var val: Int
+    var left: TreeNode?
+    var right: TreeNode?
+
+    init(_ val: Int) {
+        self.val = val
+        self.left = nil
+        self.right = nil
+    }
+}
+
+let root = TreeNode(1)
+let node1_3 = TreeNode(3)
+root.left = TreeNode(2)
+root.right = node1_3
+node1_3.left = TreeNode(4)
+node1_3.right = TreeNode(5)
+
+class Codec {
+    func serialize(_ root: TreeNode?) -> String {
+        return ""
+    }
+
+    func deserialize(_ data: String) -> TreeNode? {
+        return nil
+    }
+}
+
