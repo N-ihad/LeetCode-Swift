863e39f97acbaaa099b2face0fe70cc7c22ec332
Nihad Samedov <the_nihad@icloud.com>
2022-01-20 14:20:01 +0300

    Solution: maxL, maxR, L, R; min(L, R) – height[i]

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,34 @@
 import Foundation
 
 class Solution {
-    
+    func trap(_ height: [Int]) -> Int {
+        var maxL = height[0]
+        var maxR = height[height.count-1]
+        var L = 0
+        var R = height.count-1
+        var res = 0
+
+        while L != R {
+            while maxL <= maxR && L != R {
+                res = maxL - height[L] > 0 ? res + maxL - height[L] : res
+                if height[L] > maxL {
+                    maxL = height[L]
+                    break
+                } else {
+                    L += 1
+                }
+            }
+            while maxR <= maxL && L != R {
+                res = maxR - height[R] > 0 ? res + maxR - height[R] : res
+                if height[R] > maxR {
+                    maxR = height[R]
+                    break
+                } else {
+                    R -= 1
+                }
+            }
+        }
+
+        return res
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,20 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_trapWater() {
+        // given
+        let heights1 = [0,1,0,2,1,0,1,3,2,1,2,1]
+        let heights2 = [4,2,0,3,2,5]
+
+        // when
+        let result1 = sut.trap(heights1)
+        let result2 = sut.trap(heights2)
+
+        // then
+        let expected1 = 6
+        let expected2 = 9
+
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
     }
 }
