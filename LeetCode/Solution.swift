d38378693c488f41961e35e84db6147eb9a29b93
Nihad Samedov <the_nihad@icloud.com>
2022-01-25 01:49:44 +0300

    Solution: Set

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,18 @@
 import Foundation
 
 class Solution {
-    
+    func longestConsecutive(_ nums: [Int]) -> Int {
+        let nums = Set(nums)
+        var res = 0
+
+        for num in nums {
+            if !nums.contains(num-1) {
+                var tempNum = num + 1
+                while nums.contains(tempNum) { tempNum += 1 }
+                res = tempNum - num > res ? tempNum - num : res
+            }
+        }
+
+        return res
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,20 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_longestConsecutive() {
+        // given
+        let nums1 = [100,4,200,1,3,2]
+        let nums2 = [0,3,7,2,5,8,4,6,0,1]
+
+        // when
+        let result1 = sut.longestConsecutive(nums1)
+        let result2 = sut.longestConsecutive(nums2)
+
+        // then
+        let expected1 = 4
+        let expected2 = 9
+
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
     }
 }
