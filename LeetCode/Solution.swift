a116c1186a3597bdbff8b9d5ea392f96d605d844
Nihad Samedov <the_nihad@icloud.com>
2022-01-30 22:43:21 +0300

    Solution: Floyd's algorithm: fast and slow and then again slow pointers

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,22 @@
 import Foundation
 
 class Solution {
-    
+    func findDuplicate(_ nums: [Int]) -> Int {
+
+        var S1 = 0
+        var F = 0
+
+        repeat {
+            S1 = nums[S1]
+            F = nums[nums[F]]
+        } while S1 != F
+
+        var S2 = 0
+        repeat {
+            S1 = nums[S1]
+            S2 = nums[S2]
+        } while S1 != S2
+
+        return S1
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,24 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_findDuplicate() {
+        // given
+        let nums1 = [2,5,9,6,9,3,8,9,7,1]
+        let nums2 = [1,3,4,2,2]
+        let nums3 = [3,1,3,4,2]
+
+        // when
+        let result1 = sut.findDuplicate(nums1)
+        let result2 = sut.findDuplicate(nums2)
+        let result3 = sut.findDuplicate(nums3)
+
+        // then
+        let expected1 = 9
+        let expected2 = 2
+        let expected3 = 3
+
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
+        XCTAssertEqual(result3, expected3)
     }
 }
