54f439ee95301cdeb5724ff48efb7bffde2dce49
Nihad Samedov <the_nihad@icloud.com>
2022-01-30 21:37:57 +0300

    Yet to be solved: dk what to do with the last node (there's cycle)

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,27 @@
 import Foundation
 
 class Solution {
-    
+    func oddEvenList(_ head: ListNode?) -> ListNode? {
+        if head == nil || head?.next == nil {
+            return head
+        }
+
+        let origin = head
+        var buff = head
+        var head = head
+        let evenListHeadOrigin = head!.next
+        var flag = false
+        while head?.next != nil && !flag {
+            print(head!.val)
+            Helper.printList(origin)
+            let temp = head?.next
+            buff?.next = head?.next?.next
+            temp?.next = head?.next?.next
+            buff = buff?.next
+            buff?.next = evenListHeadOrigin
+            head = temp
+            Helper.printList(origin)
+        }
+        return origin
+    }
 }
--- LeetCode/main.swift
+++ LeetCode/main.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+// given
+let sut = Solution()
+
+let head1 = [1, 2, 3, 4, 5]
+let head2 = [2, 1, 3, 5, 6, 4, 7]
+
+// when
+let result1 = sut.oddEvenList(Helper.makeList(fromArray: head1))
+let result2 = sut.oddEvenList(Helper.makeList(fromArray: head2))
+
+// then
+Helper.printList(result1)
+Helper.printList(Helper.makeList(fromArray: head1))
+Helper.printList(result2)
+Helper.printList(Helper.makeList(fromArray: head2))
