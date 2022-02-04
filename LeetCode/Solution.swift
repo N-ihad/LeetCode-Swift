2c23cf96d085296e3a1e843cc3511455a75ed20e
Nihad Samedov <the_nihad@icloud.com>
2022-02-05 00:02:10 +0300

    Solution: backtracking

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,33 @@
 import Foundation
 
 class Solution {
-    
+    func letterCombinations(_ digits: String) -> [String] {
+        let hashMap: [Character: [Character]] = [
+            "2": ["a", "b", "c"],
+            "3": ["d", "e", "f"],
+            "4": ["g", "h", "i"],
+            "5": ["j", "k", "l"],
+            "6": ["m", "n", "o"],
+            "7": ["p", "q", "r", "s"],
+            "8": ["t", "u", "v"],
+            "9": ["w", "x", "y", "z"],
+        ]
+        let digits = Array(digits)
+        var result: [String] = []
+
+        func backtrack(_ i: Int, _ res: String) {
+            if res.count == digits.count {
+                if !res.isEmpty { result.append(res) }
+                return
+            }
+
+            for letter in hashMap[digits[i]]! {
+                backtrack(i + 1, res + String(letter))
+            }
+        }
+
+        backtrack(0, "")
+
+        return result
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,28 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_letterCombinations() {
+        // given
+        let digits1 = "23"
+        let digits2 = ""
+        let digits3 = "2"
+        let digits4 = "234"
+
+        // when
+        let result1 = sut.letterCombinations(digits1)
+        let result2 = sut.letterCombinations(digits2)
+        let result3 = sut.letterCombinations(digits3)
+        let result4 = sut.letterCombinations(digits4)
+
+        // then
+        let expected1 = ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+        let expected2 = [String]()
+        let expected3 = ["a","b","c"]
+        let expected4 = ["adg","adh","adi","aeg","aeh","aei","afg","afh","afi","bdg","bdh","bdi","beg","beh","bei","bfg","bfh","bfi","cdg","cdh","cdi","ceg","ceh","cei","cfg","cfh","cfi"]
+
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
+        XCTAssertEqual(result3, expected3)
+        XCTAssertEqual(result4, expected4)
     }
 }
