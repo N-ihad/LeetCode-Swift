ef873e9692c8680000622dc0f040348db161c0b5
Nihad Samedov <the_nihad@icloud.com>
2022-02-04 03:56:35 +0300

    Solution: queue

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,42 @@
 import Foundation
 
 class Solution {
-    
+    func zigzagLevelOrder(_ root: TreeNode?) -> [[Int]] {
+        if root == nil { return [] }
+        var queue: [TreeNode?] = [root]
+        var result: [[Int]] = [[root!.val]]
+        var turnAround = false
+
+        while !queue.isEmpty {
+            var tempQueue: [TreeNode?] = []
+            var tempResult: [Int] = []
+            for _ in 0..<queue.count {
+                let popped = queue.removeLast()!
+                if turnAround {
+                    if popped.left != nil {
+                        tempQueue.append(popped.left)
+                        tempResult.append(popped.left!.val)
+                    }
+                    if popped.right != nil {
+                        tempQueue.append(popped.right)
+                        tempResult.append(popped.right!.val)
+                    }
+                } else {
+                    if popped.right != nil {
+                        tempQueue.append(popped.right)
+                        tempResult.append(popped.right!.val)
+                    }
+                    if popped.left != nil {
+                        tempQueue.append(popped.left)
+                        tempResult.append(popped.left!.val)
+                    }
+                }
+            }
+            queue = tempQueue
+            turnAround = !turnAround
+            if !tempResult.isEmpty { result.append(tempResult) }
+        }
+
+        return result
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,28 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_zigzagLevelOrder() {
+        // given
+        let root1 = TreeNode(1, TreeNode(2, TreeNode(4)), TreeNode(3, nil, TreeNode(5)))
+        let root2 = TreeNode(3, TreeNode(9), TreeNode(20, TreeNode(15), TreeNode(7)))
+        let root3 = TreeNode(1)
+        let root4: TreeNode? = nil
+
+        // when
+        let result1 = sut.zigzagLevelOrder(root1)
+        let result2 = sut.zigzagLevelOrder(root2)
+        let result3 = sut.zigzagLevelOrder(root3)
+        let result4 = sut.zigzagLevelOrder(root4)
+
+        // then
+        let expected1 = [[1], [3, 2], [4, 5]]
+        let expected2 = [[3], [20, 9], [15, 7]]
+        let expected3 = [[1]]
+        let expected4 = [[Int]]()
+
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
+        XCTAssertEqual(result3, expected3)
+        XCTAssertEqual(result4, expected4)
     }
 }
