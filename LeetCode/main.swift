6be698bb4c915b5251db2a1ba998e5dd13b6ec1a
Nihad Samedov <the_nihad@icloud.com>
2022-02-04 04:20:57 +0300

    Solution: just using Set

--- LeetCode/main.swift
+++ LeetCode/main.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+class RandomizedSet {
+
+    var set: Set<Int> = []
+
+    init() { }
+
+    func insert(_ val: Int) -> Bool {
+        return set.insert(val).0
+    }
+
+    func remove(_ val: Int) -> Bool {
+        return set.remove(val) != nil
+    }
+
+    func getRandom() -> Int {
+        return set.randomElement()!
+    }
+}
+
+// Your RandomizedSet object will be instantiated and called as such:
+let obj = RandomizedSet()
+let ret_1: Bool = obj.insert(2)
+let ret_2: Bool = obj.remove(2)
+let ret_3: Int = obj.getRandom()
+
