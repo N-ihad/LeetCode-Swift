7c619cfce7d6b192c2f83b69a3e05b28c00f3266
Nihad Samedov <the_nihad@icloud.com>
2022-02-04 03:57:07 +0300

    Add TreeNode class

--- LeetCode/Helper.swift
+++ LeetCode/Helper.swift
@@ -7,6 +7,18 @@
 
 import Foundation
 
+class TreeNode {
+    var val: Int
+    var left: TreeNode?
+    var right: TreeNode?
+
+    init(_ val: Int, _ left: TreeNode? = nil, _ right: TreeNode? = nil) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
 struct Helper {
     static func printMatrix(_ matrix: inout [[Int]]) {
         guard !matrix.isEmpty else {
