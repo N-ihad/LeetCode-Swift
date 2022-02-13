cf412ebed35a2e983ac4cc8cb78ba0f2ddef0ed8
Nihad Samedov <the_nihad@icloud.com>
2022-02-13 12:04:41 +0300

    Solution

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,13 @@
 import Foundation
 
 class Solution {
-    
+    func getIntersectionNode(_ headA: ListNode?, _ headB: ListNode?) -> ListNode? {
+        var _headA = headA
+        var _headB = headB
+        while _headA !== _headB {
+            _headA = _headA != nil ? _headA!.next : headB
+            _headB = _headB != nil ? _headB!.next : headA
+        }
+        return _headA
+    }
 }
