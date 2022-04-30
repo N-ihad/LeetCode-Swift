a67a367453cc88b87612e5c5ad470a6154a6c023
Nihad Samedov <the_nihad@icloud.com>
2022-04-30 13:48:01 +0300

    Solution

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,53 @@
 import Foundation
 
 class Solution {
-    
+    func gameOfLife(_ board: inout [[Int]]) {
+        let boardCopy = board
+        var aliveCellsCounter = 0
+
+        for i in 0..<boardCopy.count {
+            for j in 0..<boardCopy[0].count {
+                aliveCellsCounter = 0
+
+                if i-1 >= 0 && j-1 >= 0 && boardCopy[i-1][j-1] == 1 {
+                    aliveCellsCounter += 1
+                }
+                if i-1 >= 0 && boardCopy[i-1][j] == 1 {
+                    aliveCellsCounter += 1
+                }
+                if i-1 >= 0 && j+1 < boardCopy[0].count && boardCopy[i-1][j+1] == 1 {
+                    aliveCellsCounter += 1
+                }
+                if j-1 >= 0 && boardCopy[i][j-1] == 1 {
+                    aliveCellsCounter += 1
+                }
+                if j+1 < boardCopy[0].count && boardCopy[i][j+1] == 1 {
+                    aliveCellsCounter += 1
+                }
+                if i+1 < boardCopy.count && j-1 >= 0 && boardCopy[i+1][j-1] == 1 {
+                    aliveCellsCounter += 1
+                }
+                if i+1 < boardCopy.count && boardCopy[i+1][j] == 1 {
+                    aliveCellsCounter += 1
+                }
+                if i+1 < boardCopy.count && j+1 < boardCopy[0].count && boardCopy[i+1][j+1] == 1{
+                    aliveCellsCounter += 1
+                }
+
+                if boardCopy[i][j] == 1 {
+                    if aliveCellsCounter < 2 {
+                        board[i][j] = 0
+                    } else if aliveCellsCounter == 2 || aliveCellsCounter == 3 {
+                        board[i][j] = 1
+                    } else {
+                        board[i][j] = 0
+                    }
+                } else {
+                    if aliveCellsCounter == 3 {
+                        board[i][j] = 1
+                    }
+                }
+            }
+        }
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -23,6 +23,19 @@ class SolutionTests: XCTestCase {
     }
 
     func testSolution_() {
-        
+        // given
+        var board1: [[Int]] = [[0,1,0],[0,0,1],[1,1,1],[0,0,0]]
+        var board2: [[Int]] = [[1,1],[1,0]]
+
+        // where
+        sut.gameOfLife(&board1)
+        sut.gameOfLife(&board2)
+
+        // then
+        let expected1: [[Int]] = [[0,0,0],[1,0,1],[0,1,1],[0,1,0]]
+        let expected2: [[Int]] = [[1,1],[1,1]]
+
+        XCTAssertEqual(board1, expected1)
+        XCTAssertEqual(board2, expected2)
     }
 }
