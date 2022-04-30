2e562b011def4b0c75c6dea69e7cdcc1f1da6c13
Nihad Samedov <the_nihad@icloud.com>
2022-04-30 12:35:07 +0300

    Solution

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,21 @@
 import Foundation
 
 class Solution {
-    
+    func fizzBuzz(_ n: Int) -> [String] {
+        var result: [String] = []
+
+        for i in 1...n {
+            if i % 3 == 0 && i % 5 == 0 {
+                result.append("FizzBuzz")
+            } else if i % 3 == 0 {
+                result.append("Fizz")
+            } else if i % 5 == 0 {
+                result.append("Buzz")
+            } else {
+                result.append("\(i)")
+            }
+        }
+
+        return result
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -23,6 +23,23 @@ class SolutionTests: XCTestCase {
     }
 
     func testSolution_() {
-        
+        // given
+        let n1 = 3
+        let n2 = 5
+        let n3 = 15
+
+        // where
+        let result1 = sut.fizzBuzz(n1)
+        let result2 = sut.fizzBuzz(n2)
+        let result3 = sut.fizzBuzz(n3)
+
+        // expected
+        let expected1 = ["1","2","Fizz"]
+        let expected2 = ["1","2","Fizz","4","Buzz"]
+        let expected3 = ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+
+        XCTAssertTrue(result1 == expected1)
+        XCTAssertTrue(result2 == expected2)
+        XCTAssertTrue(result3 == expected3)
     }
 }
