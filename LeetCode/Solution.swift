04f401a6dd139cdc462008c51a5105a514fe2dd5
Nihad Samedov <the_nihad@icloud.com>
2022-05-02 03:23:11 +0300

    Solution

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,25 @@
 import Foundation
 
 class Solution {
-    
+    func backspaceCompare(_ s: String, _ t: String) -> Bool {
+        var resultS = ""
+        for c in s {
+            if c != "#" {
+                resultS.append(c)
+            } else if !resultS.isEmpty {
+                resultS.removeLast()
+            }
+        }
+
+        var resultT = ""
+        for c in t {
+            if c != "#" {
+                resultT.append(c)
+            } else if !resultT.isEmpty {
+                resultT.removeLast()
+            }
+        }
+
+        return resultS == resultT
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,35 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_backspaceCompare() {
+        // given
+        let s1 = "ab#c"
+        let t1 = "ad#c"
+
+        let s2 = "ab##"
+        let t2 = "c#d#"
+
+        let s3 = "a#c"
+        let t3 = "b"
+
+        let s4 = "a##c"
+        let t4 = "#a#c"
+
+        // when
+        let result1 = sut.backspaceCompare(s1, t1)
+        let result2 = sut.backspaceCompare(s2, t2)
+        let result3 = sut.backspaceCompare(s3, t3)
+        let result4 = sut.backspaceCompare(s4, t4)
+
+        // then
+        let expected1 = true
+        let expected2 = true
+        let expected3 = false
+        let expected4 = true
+
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
+        XCTAssertEqual(result3, expected3)
+        XCTAssertEqual(result4, expected4)
     }
 }
