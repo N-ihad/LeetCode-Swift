cc96190e18debaf5b77064591e3963ef061a1fd1
Nihad Samedov <the_nihad@icloud.com>
2022-05-02 04:04:45 +0300

    Solution

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,27 @@
 import Foundation
 
 class Solution {
-    
+    func isAnagram(_ s: String, _ t: String) -> Bool {
+        if s.count != t.count { return false }
+
+        var hashTable: [Character: Int] = [:]
+        for c in s {
+            if hashTable[c] != nil {
+                hashTable[c]! += 1
+            } else {
+                hashTable[c] = 1
+            }
+        }
+
+        for c in t {
+            if hashTable[c] == nil {
+                return false
+            } else {
+                if hashTable[c]! == 0 { return false }
+                hashTable[c]! -= 1
+            }
+        }
+
+        return true
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,29 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_isAnagram() {
+        // given
+        let s1 = "anagram"
+        let t1 = "nagaram"
+
+        let s2 = "rat"
+        let t2 = "car"
+
+        let s3 = "ab"
+        let t3 = "a"
+
+        // when
+        let result1 = sut.isAnagram(s1, t1)
+        let result2 = sut.isAnagram(s2, t2)
+        let result3 = sut.isAnagram(s3, t3)
+
+        // then
+        let expected1 = true
+        let expected2 = false
+        let expected3 = false
+
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
+        XCTAssertEqual(result3, expected3)
     }
 }
