942956a9317c7b766aa2ddf3c30d86e552bf0cfc
Nihad Samedov <the_nihad@icloud.com>
2022-05-08 14:58:29 +0300

    Solution: slow, Time Limit Exceeded

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,45 @@
 import Foundation
 
 class Solution {
-    
+
+    class Stack {
+        var val: [[Character: Int]] = []
+
+        var top: [Character: Int]? {
+            val.last
+        }
+
+        func pop() {
+            val.removeLast()
+        }
+
+        func add(_ pair: [Character: Int]) {
+            val.append(pair)
+        }
+
+        func increment() {
+            guard !val.isEmpty else { return }
+            val[val.count-1][val[val.count-1].keys.first!]! += 1
+        }
+    }
+
+    func removeDuplicates(_ s: String, _ k: Int) -> String {
+        let stack = Stack()
+
+        for c in s {
+            if let top = stack.top, top.keys.first! == c {
+                stack.increment()
+            } else {
+                stack.add([c: 1])
+            }
+
+            if let top = stack.top, top.values.first! == k {
+                stack.pop()
+            }
+        }
+
+        var result = ""
+        stack.val.forEach { result.append(String(repeating: $0.keys.first!, count: $0.values.first!)) }
+        return result
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,29 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_removeDuplicates() {
+        // given
+        let s1 = "abcd"
+        let k1 = 2
+
+        let s2 = "deeedbbcccbdaa"
+        let k2 = 3
+
+        let s3 = "pbbcggttciiippooaais"
+        let k3 = 2
+
+        // when
+        let result1 = sut.removeDuplicates(s1, k1)
+        let result2 = sut.removeDuplicates(s2, k2)
+        let result3 = sut.removeDuplicates(s3, k3)
+
+        // then
+        let expected1 = "abcd"
+        let expected2 = "aa"
+        let expected3 = "ps"
+
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
+        XCTAssertEqual(result3, expected3)
     }
 }
