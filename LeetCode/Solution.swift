3292755652db6441ea20bfe2fd869c926de93ebd
Nihad Samedov <the_nihad@icloud.com>
2022-05-10 14:23:39 +0300

    Solution: hashTable

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,20 @@
 import Foundation
 
 class Solution {
-    
+    func firstUniqChar(_ s: String) -> Int {
+        var dict: [Character: Int] = [:]
+        let s = Array(s)
+
+        for i in 0..<s.count {
+            dict[s[i]] = dict[s[i]] == nil ? i : -1
+        }
+
+        for i in 0..<s.count {
+            if dict[s[i]] != -1 {
+                return dict[s[i]]!
+            }
+        }
+
+        return -1
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,24 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_firstUniqChar() {
+        // given
+        let s1 = "leetcode"
+        let s2 = "loveleetcode"
+        let s3 = "aabb"
+
+        // when
+        let result1 = sut.firstUniqChar(s1)
+        let result2 = sut.firstUniqChar(s2)
+        let result3 = sut.firstUniqChar(s3)
+
+        // then
+        let expected1 = 0
+        let expected2 = 2
+        let expected3 = -1
+
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
+        XCTAssertEqual(result3, expected3)
     }
 }
