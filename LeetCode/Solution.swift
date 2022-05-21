d757fe399cff37806b18c9b6b7f87309a5e5954e
Nihad Samedov <the_nihad@icloud.com>
2022-05-21 13:02:10 +0300

    Solution: but slow

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,66 @@
 import Foundation
 
 class Solution {
-    
+    func spiralOrder(_ matrix: [[Int]]) -> [Int] {
+        enum Direction {
+            case right
+            case down
+            case left
+            case up
+        }
+
+        var direction: Direction = .right
+        var result: [Int] = []
+        let (m, n) = (matrix.count, matrix[0].count)
+        let count = m * n
+        var (i, j) = (0, 0) // (row, column)
+        var hashTable: [Int: Bool] = [:]
+
+        while result.count != count {
+            switch direction {
+            case .right:
+                if j < n && hashTable[matrix[i][j]] == nil {
+                    hashTable[matrix[i][j]] = true
+                    result.append(matrix[i][j])
+                    j += 1
+                } else {
+                    direction = .down
+                    j -= 1
+                    i += 1
+                }
+            case .down:
+                if i < m && hashTable[matrix[i][j]] == nil {
+                    hashTable[matrix[i][j]] = true
+                    result.append(matrix[i][j])
+                    i += 1
+                } else {
+                    direction = .left
+                    i -= 1
+                    j -= 1
+                }
+            case .left:
+                if j >= 0 && hashTable[matrix[i][j]] == nil {
+                    hashTable[matrix[i][j]] = true
+                    result.append(matrix[i][j])
+                    j -= 1
+                } else {
+                    direction = .up
+                    j += 1
+                    i -= 1
+                }
+            case .up:
+                if i >= 0 && hashTable[matrix[i][j]] == nil {
+                    hashTable[matrix[i][j]] = true
+                    result.append(matrix[i][j])
+                    i -= 1
+                } else {
+                    direction = .right
+                    i += 1
+                    j += 1
+                }
+            }
+        }
+
+        return result
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,20 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_spiralOrder() {
+        // given
+        let matrix1: [[Int]] = [[1,2,3],[4,5,6],[7,8,9]]
+        let matrix2: [[Int]] = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+
+        // when
+        let result2 = sut.spiralOrder(matrix2)
+        let result1 = sut.spiralOrder(matrix1)
+
+        // then
+        let expected1 = [1,2,3,6,9,8,7,4,5]
+        let expected2 = [1,2,3,4,8,12,11,10,9,5,6,7]
+
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
     }
 }
