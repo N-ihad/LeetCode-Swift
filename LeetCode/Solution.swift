0c10df4015767df2891e5f621523a374f2407b68
Nihad Samedov <the_nihad@icloud.com>
2022-06-22 17:47:14 +0300

    Solution: backtracking

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,28 @@
 import Foundation
 
 class Solution {
-    
+    func combinationSum2(_ candidates: [Int], _ target: Int) -> [[Int]] {
+        var paths: [[Int]] = [], path: [Int] = [], candidates = candidates.sorted()
+        dfs(&candidates, &paths, &path, target, 0)
+        return paths
+    }
+
+    private func dfs(_ candidates: inout [Int], _ paths: inout [[Int]], _ path: inout [Int], _ target: Int, _ j: Int) {
+        if target < 0 {
+            return
+        }
+
+        if target == 0 {
+            paths.append(path)
+            return
+        }
+
+        for i in j..<candidates.count {
+            if i == j || candidates[i] != candidates[i-1] {
+                path.append(candidates[i])
+                dfs(&candidates, &paths, &path, target - candidates[i], i + 1)
+                path.removeLast()
+            }
+        }
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,23 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_combinationSum2() {
+        // given
+        let candidates1 = [10,1,2,7,6,1,5]
+        let target1 = 8
+
+        let candidates2 = [2,5,2,1,2]
+        let target2 = 5
+
+        // when
+        let result1 = sut.combinationSum2(candidates1, target1)
+        let result2 = sut.combinationSum2(candidates2, target2)
+
+        // then
+        let expected1 = [[1,1,6],[1,2,5],[1,7],[2,6]]
+        let expected2 = [[1,2,2],[5]]
+
+        XCTAssertEqual(Set<Array<Int>>(result1), Set<Array<Int>>(expected1))
+        XCTAssertEqual(Set<Array<Int>>(result2), Set<Array<Int>>(expected2))
     }
 }
