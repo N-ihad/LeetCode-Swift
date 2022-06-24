2975afca022479bf4b100cd07c0887d22e3b43c4
Nihad Samedov <the_nihad@icloud.com>
2022-06-24 18:42:06 +0300

    Solution: backtracking

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,50 @@
 import Foundation
 
 class Solution {
-    
+    func solveSudoku(_ board: inout [[Character]]) {
+        solve(&board)
+    }
+
+    @discardableResult
+    private func solve(_ board: inout [[Character]]) -> Bool {
+        let sudokuChars: [Character] = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        for i in 0..<9 {
+            for j in 0..<9 {
+                if board[i][j] == "." {
+                    for k in sudokuChars {
+                        if isValid(&board, i, j, k) {
+                            board[i][j] = k
+                            if solve(&board) {
+                                return true
+                            } else {
+                                board[i][j] = "."
+                            }
+                        }
+                    }
+                    return false
+                }
+            }
+        }
+        return true
+    }
+
+    func isValid(_ board: inout [[Character]], _ i: Int, _ j: Int, _ char: Character) -> Bool {
+        for q in 0..<9 where board[i][q] == char {
+            return false
+        }
+
+        for q in 0..<9 where board[q][j] == char {
+            return false
+        }
+
+        let y = (i / 3) * 3
+        let x = (j / 3) * 3
+        for w in y..<y+3 {
+            for e in x..<x+3 where board[w][e] == char {
+                return false
+            }
+        }
+
+        return true
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -11,18 +11,68 @@ import XCTest
 class SolutionTests: XCTestCase {
 
     var sut: Solution!
+    var board: [[Character]]!
 
     override func setUp() {
         super.setUp()
         sut = Solution()
+        board = [["5","3",".",".","7",".",".",".","."],
+                 ["6",".",".","1","9","5",".",".","."],
+                 [".","9","8",".",".",".",".","6","."],
+                 ["8",".",".",".","6",".",".",".","3"],
+                 ["4",".",".","8",".","3",".",".","1"],
+                 ["7",".",".",".","2",".",".",".","6"],
+                 [".","6",".",".",".",".","2","8","."],
+                 [".",".",".","4","1","9",".",".","5"],
+                 [".",".",".",".","8",".",".","7","9"]]
     }
 
     override func tearDown() {
         sut = nil
+        board = nil
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_sudokuSolver() {
+        // when
+        sut.solveSudoku(&board)
+
+        // then
+        let expected: [[Character]] = [["5","3","4","6","7","8","9","1","2"],
+                                       ["6","7","2","1","9","5","3","4","8"],
+                                       ["1","9","8","3","4","2","5","6","7"],
+                                       ["8","5","9","7","6","1","4","2","3"],
+                                       ["4","2","6","8","5","3","7","9","1"],
+                                       ["7","1","3","9","2","4","8","5","6"],
+                                       ["9","6","1","5","3","7","2","8","4"],
+                                       ["2","8","7","4","1","9","6","3","5"],
+                                       ["3","4","5","2","8","6","1","7","9"]]
+
+        XCTAssertEqual(board, expected)
+    }
+
+    func test_isValidCharInSudokuBoard() {
+        // given
+        let char1: Character = "4" // valid
+        let char2: Character = "1" // square
+        let char3: Character = "7" // row
+        let char4: Character = "9" // column
+
+        // when
+        let result1 = sut.isValid(&board, 0, 5, char1)
+        let result2 = sut.isValid(&board, 0, 5, char2)
+        let result3 = sut.isValid(&board, 0, 5, char3)
+        let result4 = sut.isValid(&board, 0, 5, char4)
+
+        // then
+        let expected1 = true
+        let expected2 = false
+        let expected3 = false
+        let expected4 = false
+
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
+        XCTAssertEqual(result3, expected3)
+        XCTAssertEqual(result4, expected4)
     }
 }
