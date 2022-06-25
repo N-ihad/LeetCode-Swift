1c0b7c33d089517f75d884bfe699dfd364a6ad12
Nihad Samedov <the_nihad@icloud.com>
2022-06-25 18:22:06 +0300

    Solution: backtracking (almost solved, but there are some cases when there're more solutions when first row queen for example stays in the same place)

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,128 @@
 import Foundation
 
 class Solution {
-    
+    func solveNQueens(_ n: Int) -> [[String]] {
+        var boards = Set<[[Int]]>()
+        var result: [[String]] = []
+        for j in 0..<n {
+            var board = Array(repeating: Array(repeating: 0, count: n), count: n)
+            var queensPlaced = 0
+            var indices: [Int] = []
+            solve(&board, n, &queensPlaced, &indices, j)
+            if !indices.isEmpty {
+                boards.insert(board)
+            }
+        }
+
+        let _boards = Array(boards)
+        for k in 0..<_boards.count {
+            var strs: [String] = []
+            for i in 0..<_boards[0].count {
+                var str = ""
+                for j in 0..<_boards[0][0].count {
+                    if _boards[k][i][j] != 1 {
+                        str.append(".")
+                    } else {
+                        str.append("Q")
+                    }
+                }
+                strs.append(str)
+            }
+            result.append(strs)
+        }
+//        result.enu .forEach { result[$0][indices[$0]] = Q }
+
+        return result
+    }
+
+    @discardableResult
+    private func solve(_ board: inout [[Int]], _ n: Int, _ queensPlaced: inout Int, _ indices: inout [Int], _ jStart: Int = 0) -> Bool {
+        for i in 0..<n {
+            for j in jStart..<n {
+                if board[i][j] == 0 {
+                    board[i][j] = 1
+                    updateHitCells(&board, n, i, j, true)
+                    indices.append(j)
+                    queensPlaced += 1
+                    if !solve(&board, n, &queensPlaced, &indices) {
+                        board[i][j] = 0
+                        queensPlaced -= 1
+                        updateHitCells(&board, n, i, j, false)
+                        indices.removeLast()
+                    }
+                }
+            }
+        }
+        return queensPlaced == n
+    }
+
+    func updateHitCells(_ board: inout [[Int]], _ n: Int, _ y: Int, _ x: Int, _ shouldHit: Bool) {
+        for j in 0..<x {
+            board[y][j] = !shouldHit && canRemoveHit(&board, n, y, j) ? 0 : -1
+        }
+
+        for j in x+1..<n {
+            board[y][j] = !shouldHit && canRemoveHit(&board, n, y, j) ? 0 : -1
+        }
+
+        for i in 0..<y {
+            board[i][x] = !shouldHit && canRemoveHit(&board, n, i, x) ? 0 : -1
+        }
+
+        for i in y+1..<n {
+            board[i][x] = !shouldHit && canRemoveHit(&board, n, i, x) ? 0 : -1
+        }
+
+        for (i, j) in zip((0..<y).reversed(), (0..<x).reversed()) {
+            board[i][j] = !shouldHit && canRemoveHit(&board, n, i, j) ? 0 : -1
+        }
+
+        for (i, j) in zip(y+1..<n, x+1..<n) {
+            board[i][j] = !shouldHit && canRemoveHit(&board, n, i, j) ? 0 : -1
+        }
+
+        for (i, j) in zip((0..<y).reversed(), x+1..<n) {
+            board[i][j] = !shouldHit && canRemoveHit(&board, n, i, j) ? 0 : -1
+        }
+
+        for (i, j) in zip(y+1..<n, (0..<x).reversed()) {
+            board[i][j] = !shouldHit && canRemoveHit(&board, n, i, j) ? 0 : -1
+        }
+    }
+
+    func canRemoveHit(_ board: inout [[Int]], _ n: Int, _ y: Int, _ x: Int) -> Bool {
+        for j in 0..<x where board[y][j] == 1 {
+            return false
+        }
+
+        for j in x+1..<n where board[y][j] == 1 {
+            return false
+        }
+
+        for i in 0..<y where board[i][x] == 1 {
+            return false
+        }
+
+        for i in y+1..<n where board[i][x] == 1 {
+            return false
+        }
+
+        for (i, j) in zip((0..<y).reversed(), (0..<x).reversed()) where board[i][j] == 1 {
+            return false
+        }
+
+        for (i, j) in zip(y+1..<n, x+1..<n) where board[i][j] == 1 {
+            return false
+        }
+
+        for (i, j) in zip((0..<y).reversed(), x+1..<n) where board[i][j] == 1 {
+            return false
+        }
+
+        for (i, j) in zip(y+1..<n, (0..<x).reversed()) where board[i][j] == 1 {
+            return false
+        }
+
+        return true
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,68 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_NQueens() {
+        // given
+        let n1 = 4
+        let n2 = 1
+        let n3 = 2
+
+        // when
+        let result1 = sut.solveNQueens(n1)
+        let result2 = sut.solveNQueens(n2)
+        let result3 = sut.solveNQueens(n3)
+
+        // then
+        let expected1 = [[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
+        let expected2 = [["Q"]]
+        let expected3: [[String]] = []
+
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
+        XCTAssertEqual(result3, expected3)
+    }
+
+    func test_updateHitCells() {
+        // given
+        var board1 = Array(repeating: Array(repeating: 0, count: 1), count: 1)
+        var board2 = Array(repeating: Array(repeating: 0, count: 2), count: 2)
+        var board3 = Array(repeating: Array(repeating: 0, count: 3), count: 3)
+        var board4 = Array(repeating: Array(repeating: 0, count: 4), count: 4)
+        var board5 = Array(repeating: Array(repeating: 0, count: 4), count: 4)
+
+        // when
+        sut.updateHitCells(&board1, 1, 0, 0, true)
+        sut.updateHitCells(&board2, 2, 1, 1, true)
+        sut.updateHitCells(&board3, 3, 0, 1, true)
+        sut.updateHitCells(&board4, 4, 3, 2, true)
+        sut.updateHitCells(&board5, 4, 3, 2, true)
+        sut.updateHitCells(&board5, 4, 3, 2, false)
+
+        // then
+        let expected1 = [
+            [0]
+        ]
+        let expected2 = [
+            [-1, -1],
+            [-1, 0]
+        ]
+        let expected3 = [
+            [-1, 0, -1],
+            [-1, -1, -1],
+            [0, -1, 0]
+        ]
+        let expected4 = [
+            [0, 0, -1, 0],
+            [-1, 0, -1, 0],
+            [0, -1, -1, -1],
+            [-1, -1, 0, -1]
+        ]
+        let expected5 = Array(repeating: Array(repeating: 0, count: 4), count: 4)
+
+        XCTAssertEqual(board1, expected1)
+        XCTAssertEqual(board2, expected2)
+        XCTAssertEqual(board3, expected3)
+        XCTAssertEqual(board4, expected4)
+        XCTAssertEqual(board5, expected5)
     }
 }
