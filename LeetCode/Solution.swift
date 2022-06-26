f0b5e74314fe6ad949d861511cd7ea58b3ee1f98
Nihad Samedov <the_nihad@icloud.com>
2022-06-26 22:10:12 +0300

    Solution: backtracking

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,27 @@
 import Foundation
 
 class Solution {
-    
+    func combine(_ n: Int, _ k: Int) -> [[Int]] {
+        var result: [[Int]] = []
+        var subArray: [Int] = []
+        combine(&result, &subArray, n, k, 1)
+        return result
+    }
+
+    private func combine(_ result: inout [[Int]], _ subArray: inout [Int], _ n: Int, _ k: Int, _ j: Int) {
+        if subArray.count == k {
+            result.append(subArray)
+            return
+        }
+
+        if j > n {
+            return
+        }
+
+        for i in j...n {
+            subArray.append(i)
+            combine(&result, &subArray, n, k, i + 1)
+            subArray.removeLast()
+        }
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,30 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_combinations() {
+        // given
+        let n1 = 4
+        let k1 = 2
+
+        let n2 = 1
+        let k2 = 1
+
+        // when
+        let result1 = sut.combine(n1, k1)
+        let result2 = sut.combine(n2, k2)
+
+        // then
+        let expected1 = [
+            [2,4],
+            [3,4],
+            [2,3],
+            [1,2],
+            [1,3],
+            [1,4],
+        ]
+        let expected2 = [[1]]
+
+        XCTAssertEqual(Set<[Int]>(result1), Set<[Int]>(expected1))
+        XCTAssertEqual(Set<[Int]>(result2), Set<[Int]>(expected2))
     }
 }
