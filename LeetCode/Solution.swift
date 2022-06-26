1d42eed96e921601c3e45eecfcfd03cbd7af139b
Nihad Samedov <the_nihad@icloud.com>
2022-06-26 22:20:46 +0300

    Solution: backtracking

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,103 @@
 import Foundation
 
 class Solution {
-    
+    func totalNQueens(_ n: Int) -> Int {
+        var solutions = 0
+        var board = Array(repeating: Array(repeating: 0, count: n), count: n)
+        var queensPlaced = 0
+        solve(&board, &queensPlaced, &solutions, 0)
+        return solutions
+    }
+
+    private func solve(_ board: inout [[Int]], _ queensPlaced: inout Int, _ solutions: inout Int, _ jj: Int) {
+        for i in 0..<board.count {
+            for j in jj..<board.count {
+                if board[i][j] == 0 {
+                    board[i][j] = 1
+                    updateHitCells(&board, i, j, true)
+                    queensPlaced += 1
+
+                    solve(&board, &queensPlaced, &solutions, j + 1)
+
+                    board[i][j] = 0
+                    queensPlaced -= 1
+                    updateHitCells(&board, i, j, false)
+                }
+            }
+        }
+
+        if queensPlaced == board.count {
+            solutions += 1
+        }
+    }
+
+    func updateHitCells(_ board: inout [[Int]], _ y: Int, _ x: Int, _ shouldHit: Bool) {
+        for j in 0..<x {
+            board[y][j] = !shouldHit && canRemoveHit(&board, y, j) ? 0 : -1
+        }
+
+        for j in x+1..<board.count {
+            board[y][j] = !shouldHit && canRemoveHit(&board, y, j) ? 0 : -1
+        }
+
+        for i in 0..<y {
+            board[i][x] = !shouldHit && canRemoveHit(&board, i, x) ? 0 : -1
+        }
+
+        for i in y+1..<board.count {
+            board[i][x] = !shouldHit && canRemoveHit(&board, i, x) ? 0 : -1
+        }
+
+        for (i, j) in zip((0..<y).reversed(), (0..<x).reversed()) {
+            board[i][j] = !shouldHit && canRemoveHit(&board, i, j) ? 0 : -1
+        }
+
+        for (i, j) in zip(y+1..<board.count, x+1..<board.count) {
+            board[i][j] = !shouldHit && canRemoveHit(&board, i, j) ? 0 : -1
+        }
+
+        for (i, j) in zip((0..<y).reversed(), x+1..<board.count) {
+            board[i][j] = !shouldHit && canRemoveHit(&board, i, j) ? 0 : -1
+        }
+
+        for (i, j) in zip(y+1..<board.count, (0..<x).reversed()) {
+            board[i][j] = !shouldHit && canRemoveHit(&board, i, j) ? 0 : -1
+        }
+    }
+
+    func canRemoveHit(_ board: inout [[Int]], _ y: Int, _ x: Int) -> Bool {
+        for j in 0..<x where board[y][j] == 1 {
+            return false
+        }
+
+        for j in x+1..<board.count where board[y][j] == 1 {
+            return false
+        }
+
+        for i in 0..<y where board[i][x] == 1 {
+            return false
+        }
+
+        for i in y+1..<board.count where board[i][x] == 1 {
+            return false
+        }
+
+        for (i, j) in zip((0..<y).reversed(), (0..<x).reversed()) where board[i][j] == 1 {
+            return false
+        }
+
+        for (i, j) in zip(y+1..<board.count, x+1..<board.count) where board[i][j] == 1 {
+            return false
+        }
+
+        for (i, j) in zip((0..<y).reversed(), x+1..<board.count) where board[i][j] == 1 {
+            return false
+        }
+
+        for (i, j) in zip(y+1..<board.count, (0..<x).reversed()) where board[i][j] == 1 {
+            return false
+        }
+
+        return true
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,98 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_NQueens() {
+        // given
+        let n1 = 1
+        let n2 = 2
+        let n3 = 3
+        let n4 = 4
+        let n5 = 5
+        let n6 = 6
+//        let n7 = 7
+//        let n8 = 8
+//        let n9 = 9
+
+        // when
+        let result1 = sut.totalNQueens(n1)
+        let result2 = sut.totalNQueens(n2)
+        let result3 = sut.totalNQueens(n3)
+        let result4 = sut.totalNQueens(n4)
+        let result5 = sut.totalNQueens(n5)
+        let result6 = sut.totalNQueens(n6)
+//        let result7 = sut.totalNQueens(n7)
+//        let result8 = sut.totalNQueens(n8)
+//        let result9 = sut.totalNQueens(n9)
+
+        // then
+        let expected1 = 1
+        let expected2 = 0
+        let expected3 = 0
+        let expected4 = 2
+        let expected5 = 10
+        let expected6 = 4
+//        let expected7 = 40
+//        let expected8 = 92
+//        let expected9 = 352
+
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
+        XCTAssertEqual(result3, expected3)
+        XCTAssertEqual(result4, expected4)
+        XCTAssertEqual(result5, expected5)
+        XCTAssertEqual(result6, expected6)
+//        XCTAssertEqual(result7, expected7)
+//        XCTAssertEqual(result8, expected8)
+//        XCTAssertEqual(result9, expected9)
+    }
+
+    func test_updateHitCells() {
+        // given
+        var board1 = Array(repeating: Array(repeating: 0, count: 1), count: 1)
+        var board2 = Array(repeating: Array(repeating: 0, count: 2), count: 2)
+        var board3 = Array(repeating: Array(repeating: 0, count: 3), count: 3)
+        var board4 = Array(repeating: Array(repeating: 0, count: 4), count: 4)
+        var board5 = Array(repeating: Array(repeating: 0, count: 4), count: 4)
+
+        // when
+        sut.updateHitCells(&board1, 0, 0, true)
+        sut.updateHitCells(&board2, 1, 1, true)
+        sut.updateHitCells(&board3, 0, 1, true)
+        sut.updateHitCells(&board4, 3, 2, true)
+        sut.updateHitCells(&board5, 3, 2, true)
+        sut.updateHitCells(&board5, 3, 2, false)
+
+        // then
+        let expected1 = [
+            [0]
+        ]
+        let expected2 = [
+            [-1, -1],
+            [-1, 0]
+        ]
+        let expected3 = [
+            [-1, 0, -1],
+            [-1, -1, -1],
+            [0, -1, 0]
+        ]
+        let expected4 = [
+            [0, 0, -1, 0],
+            [-1, 0, -1, 0],
+            [0, -1, -1, -1],
+            [-1, -1, 0, -1]
+        ]
+        let expected5 = Array(repeating: Array(repeating: 0, count: 4), count: 4)
+
+        XCTAssertEqual(board1, expected1)
+        XCTAssertEqual(board2, expected2)
+        XCTAssertEqual(board3, expected3)
+        XCTAssertEqual(board4, expected4)
+        XCTAssertEqual(board5, expected5)
+    }
+
+    func testPerformanceExample() throws {
+        measure {
+            
+        }
     }
 }
