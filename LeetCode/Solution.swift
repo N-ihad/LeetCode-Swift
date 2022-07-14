2bc4e6032ab63d0fd5cdbc8e7be9720f2c86fa14
Nihad Samedov <the_nihad@icloud.com>
2022-07-14 21:42:49 +0300

    Solution: Dynamic Programming

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,29 @@
 import Foundation
 
 class Solution {
-    
+    func minDistance(_ word1: String, _ word2: String) -> Int {
+        var matrix: [[Int]] = Array(repeating: Array(repeating: 0, count: word1.count + 1), count: word2.count + 1)
+        let word1 = Array(word1)
+        let word2 = Array(word2)
+
+        for i in 0..<matrix.count {
+            matrix[i][0] = i
+        }
+
+        for i in 0..<matrix[0].count {
+            matrix[0][i] = i
+        }
+
+        for i in 1..<matrix.count {
+            for j in 1..<matrix[0].count {
+                if word1[j-1] != word2[i-1] {
+                    matrix[i][j] = min(matrix[i][j-1], matrix[i-1][j], matrix[i-1][j-1]) + 1
+                } else {
+                    matrix[i][j] = matrix[i-1][j-1]
+                }
+            }
+        }
+
+        return matrix[word2.count][word1.count]
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,23 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_minDistance() {
+        // given
+        let word11 = "horse"
+        let word12 = "ros"
+
+        let word21 = "intention"
+        let word22 = "execution"
+
+        // when
+        let result1 = sut.minDistance(word11, word12)
+        let result2 = sut.minDistance(word21, word22)
+
+        // then
+        let expected1 = 3
+        let expected2 = 5
+
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
     }
 }
