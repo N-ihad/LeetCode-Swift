96fb3350c4b232f40253bcd3674a1b53893b2600
Nihad Samedov <the_nihad@icloud.com>
2022-07-15 13:23:36 +0300

    Solution

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,18 @@
 import Foundation
 
 class Solution {
-    
+    func pivotIndex(_ nums: [Int]) -> Int {
+        let allSum = nums.reduce(0) { $0 + $1 }
+        var currentSum = 0
+
+        for i in 0..<nums.count {
+            currentSum += nums[i]
+
+            if (allSum - currentSum) == (currentSum - nums[i]) {
+                return i
+            }
+        }
+
+        return -1
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,24 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_pivotIndex() {
+        // given
+        let nums1 = [1,7,3,6,5,6]
+        let nums2 = [1,2,3]
+        let nums3 = [2,1,-1]
+
+        // when
+        let result1 = sut.pivotIndex(nums1)
+        let result2 = sut.pivotIndex(nums2)
+        let result3 = sut.pivotIndex(nums3)
+
+        // then
+        let expected1 = 3
+        let expected2 = -1
+        let expected3 = 0
+
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
+        XCTAssertEqual(result3, expected3)
     }
 }
