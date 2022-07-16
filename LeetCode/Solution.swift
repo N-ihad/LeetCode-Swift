e0c937262805a86c72e866adbe3f19737447665d
Nihad Samedov <the_nihad@icloud.com>
2022-07-16 14:48:20 +0300

    Solution: Partitioning

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,60 @@
 
 import Foundation
 
+extension ArraySlice where Element == Int {
+    var rightBound: Int {
+        self.last ?? Int.min
+    }
+
+    var leftBound: Int {
+        self.first ?? Int.max
+    }
+}
+
 class Solution {
-    
+    func findMedianSortedArrays(_ nums1: [Int], _ nums2: [Int]) -> Double {
+        var arr1: [Int]
+        var arr2: [Int]
+
+        if nums1.count <= nums2.count {
+            arr1 = nums1
+            arr2 = nums2
+        } else {
+            arr1 = nums2
+            arr2 = nums1
+        }
+
+        var arr1LeftHalf: ArraySlice<Int> = []
+        var arr1RightHalf: ArraySlice<Int> = []
+        var arr2LeftHalf: ArraySlice<Int> = []
+        var arr2RightHalf: ArraySlice<Int> = []
+        var start = 0
+        var end = arr1.count
+
+        repeat {
+            let partitionX = (start + end) / 2
+            let partitionY = (arr1.count + arr2.count + 1) / 2 - partitionX
+
+            arr1LeftHalf = arr1[0..<partitionX]
+            arr1RightHalf = arr1[partitionX..<arr1.count]
+
+            arr2LeftHalf = arr2[0..<partitionY]
+            arr2RightHalf = arr2[partitionY..<arr2.count]
+
+            if arr1LeftHalf.rightBound > arr2RightHalf.leftBound {
+                end = partitionX - 1
+            } else if arr2LeftHalf.rightBound > arr1RightHalf.leftBound {
+                start = partitionX + 1
+            }
+
+        } while !(arr1LeftHalf.rightBound <= arr2RightHalf.leftBound && arr2LeftHalf.rightBound <= arr1RightHalf.leftBound)
+
+        let isEven = (arr1.count + arr2.count) % 2 == 0
+
+        if isEven {
+            return (Double(max(arr1LeftHalf.rightBound, arr2LeftHalf.rightBound) + min(arr1RightHalf.leftBound, arr2RightHalf.leftBound))) / 2
+        } else {
+            return Double(max(arr1LeftHalf.rightBound, arr2LeftHalf.rightBound))
+        }
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,35 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_findMedianSortedArrays() {
+        // given
+        let nums11 = [1,3]
+        let nums12 = [2]
+
+        let nums21 = [1,2]
+        let nums22 = [3,4]
+
+        let nums31 = [1,3,8,9,15]
+        let nums32 = [7,11,18,19,21,25]
+
+        let nums41 = [23,26,31,35]
+        let nums42 = [3,5,7,9,11,16]
+
+        // when
+        let result1 = sut.findMedianSortedArrays(nums11, nums12)
+        let result2 = sut.findMedianSortedArrays(nums21, nums22)
+        let result3 = sut.findMedianSortedArrays(nums31, nums32)
+        let result4 = sut.findMedianSortedArrays(nums41, nums42)
+
+        // then
+        let expected1 = 2.0
+        let expected2 = 2.5
+        let expected3 = 11.0
+        let expected4 = 13.5
+
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
+        XCTAssertEqual(result3, expected3)
+        XCTAssertEqual(result4, expected4)
     }
 }
