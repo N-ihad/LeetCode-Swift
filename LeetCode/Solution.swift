e1967b51a5009bf6a14280ee35d719218f839731
Nihad Samedov <the_nihad@icloud.com>
2022-07-18 21:35:36 +0300

    Solution: dfs

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,21 @@
 import Foundation
 
 class Solution {
-    
+    func maxPathSum(_ root: TreeNode?) -> Int {
+        var maxSum = -1002
+        dfs(root, &maxSum)
+        return maxSum
+    }
+
+    @discardableResult
+    private func dfs(_ node: TreeNode?, _ maxSum: inout Int) -> Int {
+        guard let node = node else {
+            return 0
+        }
+        let (left, right) = (max(dfs(node.left, &maxSum), 0), max(dfs(node.right, &maxSum), 0))
+
+        maxSum = max(maxSum, left + right + node.val)
+
+        return max(left, right) + node.val
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,32 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_maxPathSum() {
+        // given
+        let root1 = TreeNode(1, TreeNode(2), TreeNode(3))
+        let root2 = TreeNode(-10, TreeNode(9), TreeNode(20, TreeNode(15), TreeNode(7)))
+        let root3 = TreeNode(2, TreeNode(-1))
+        let root4 = TreeNode(1, TreeNode(-2), TreeNode(3))
+        let root5 = TreeNode(9, TreeNode(6), TreeNode(-3, TreeNode(-6), TreeNode(2, TreeNode(2, TreeNode(-6, TreeNode(-6)), TreeNode(-6)))))
+
+        // when
+        let result1 = sut.maxPathSum(root1)
+        let result2 = sut.maxPathSum(root2)
+        let result3 = sut.maxPathSum(root3)
+        let result4 = sut.maxPathSum(root4)
+        let result5 = sut.maxPathSum(root5)
+
+        // then
+        let expected1 = 6
+        let expected2 = 42
+        let expected3 = 2
+        let expected4 = 4
+        let expected5 = 16
+
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
+        XCTAssertEqual(result3, expected3)
+        XCTAssertEqual(result4, expected4)
+        XCTAssertEqual(result5, expected5)
     }
 }
