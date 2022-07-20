fc398a45944c31bcbefc7240e1b38154546b2a53
Nihad Samedov <the_nihad@icloud.com>
2022-07-20 22:17:45 +0300

    Solution

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,26 @@
 import Foundation
 
 class Solution {
-    
+    func firstMissingPositive(_ nums: [Int]) -> Int {
+        var nums = nums
+        var containsOne = false
+
+        for i in 0..<nums.count {
+            if nums[i] == 1 { containsOne = true }
+            nums[i] = nums[i] > nums.count || nums[i] <= 0 ? 1 : nums[i]
+        }
+
+        for i in 0..<nums.count {
+            nums[abs(nums[i])-1] = nums[abs(nums[i])-1] < 0 ? nums[abs(nums[i])-1] : -nums[abs(nums[i])-1]
+        }
+
+        if !containsOne { return 1 }
+
+        for i in 0..<nums.count {
+            if nums[i] > 0 { return i + 1 }
+            if i + 1 == nums.count { return nums.count + 1 }
+        }
+
+        return -1
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,32 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_wordBreak() {
+        // given
+        let nums1 = [1,2,0]
+        let nums2 = [3,4,-1,1]
+        let nums3 = [7,8,9,11,12]
+        let nums4 = [1,1]
+        let nums5 = [1]
+
+        // when
+        let result1 = sut.firstMissingPositive(nums1)
+        let result2 = sut.firstMissingPositive(nums2)
+        let result3 = sut.firstMissingPositive(nums3)
+        let result4 = sut.firstMissingPositive(nums4)
+        let result5 = sut.firstMissingPositive(nums5)
+
+        // then
+        let expected1 = 3
+        let expected2 = 2
+        let expected3 = 1
+        let expected4 = 2
+        let expected5 = 2
+
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
+        XCTAssertEqual(result3, expected3)
+        XCTAssertEqual(result4, expected4)
+        XCTAssertEqual(result5, expected5)
     }
 }
