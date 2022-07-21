a2252004915357e92e18199e5a85021825226f27
Nihad Samedov <the_nihad@icloud.com>
2022-07-21 22:15:08 +0300

    Solution: Hash Table with Sets

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,40 @@
 import Foundation
 
 class Solution {
-    
+    func maxPoints(_ points: [[Int]]) -> Int {
+
+        if points.count == 1 {
+            return 1
+        }
+
+        var hashTable: [String: Set<[Int]>] = [:]
+
+        for i in 0..<points.count {
+            for j in i+1..<points.count {
+                let lineEquation = getLineEquationString(points[i][0], points[i][1], points[j][0], points[j][1])
+                if hashTable[lineEquation] == nil {
+                    hashTable[lineEquation] = [[points[i][0], points[i][1]], [points[j][0], points[j][1]]]
+                } else {
+                    hashTable[lineEquation]?.insert([points[i][0], points[i][1]])
+                    hashTable[lineEquation]?.insert([points[j][0], points[j][1]])
+                }
+            }
+        }
+
+        var maxVal = 0
+        for (_, value) in hashTable {
+            if value.count > maxVal {
+                maxVal = value.count
+            }
+        }
+
+        return maxVal
+    }
+
+    func getLineEquationString(_ x0: Int, _ y0: Int, _ x1: Int, _ y1: Int) -> String {
+        if x0 == x1 { return "\(x0),nil" }
+        let k = (Double(y1) - Double(y0)) / (Double(x1) - Double(x0))
+        let c = Double(y0) - k * Double(x0)
+        return "\(k),\(c)"
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,28 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_maxPoints() {
+        // given
+        let points1 = [[1,1],[2,2,],[3,3]]
+        let points2 = [[1,1],[3,2],[5,3],[4,1],[2,3],[1,4]]
+        let points3 = [[0,0]]
+        let points4 = [[0,0],[4,5],[7,8],[8,9],[5,6],[3,4],[1,1]]
+
+        // when
+//        let result1 = sut.maxPoints(points1)
+//        let result2 = sut.maxPoints(points2)
+//        let result3 = sut.maxPoints(points3)
+        let result4 = sut.maxPoints(points4)
+
+        // then
+        let expected1 = 3
+        let expected2 = 4
+        let expected3 = 1
+        let expected4 = 5
+
+//        XCTAssertEqual(result1, expected1)
+//        XCTAssertEqual(result2, expected2)
+//        XCTAssertEqual(result3, expected3)
+        XCTAssertEqual(result4, expected4)
     }
 }
