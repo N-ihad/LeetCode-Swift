9636944d0e59f6c347bd648803d0e3193d40ca4f
Nihad Samedov <the_nihad@icloud.com>
2022-07-26 21:52:28 +0300

    Solution: Hash Table

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -9,25 +9,26 @@ import Foundation
 
 class Solution {
     func isValidSudoku(_ board: [[Character]]) -> Bool {
-        var rows = [Int: Character]()
-        var cols = [Int: Character]()
-        var squares = [String: Character]()
+        var rows: [Int: Set<Character>] = [:]
+        var cols: [Int: Set<Character>] = [:]
+        var squares: [String: Set<Character>] = [:]
 
         for i in 0..<9 {
+            if rows[i] == nil { rows[i] = [] }
             for j in 0..<9 {
-                if board[i][j] == "." {
-                    continue
-                }
+                if board[i][j] == "." { continue }
+                if squares["\(i/3)\(j/3)"] == nil { squares["\(i/3)\(j/3)"] = [] }
+                if cols[j] == nil { cols[j] = [] }
 
-                if board[i][j] == rows[i] ||
-                   board[i][j] == cols[j] ||
-                   board[i][j] == squares["\(i/3),\(j/3)"] {
+                if rows[i]!.contains(board[i][j]) ||
+                   cols[j]!.contains(board[i][j]) ||
+                   squares["\(i/3)\(j/3)"]!.contains(board[i][j]) {
                     return false
                 }
 
-                rows[i] = board[i][j]
-                cols[j] = board[i][j]
-                squares["\(i/3),\(j/3)"] = board[i][j]
+                rows[i]!.insert(board[i][j])
+                cols[j]!.insert(board[i][j])
+                squares["\(i/3)\(j/3)"]!.insert(board[i][j])
             }
         }
 
