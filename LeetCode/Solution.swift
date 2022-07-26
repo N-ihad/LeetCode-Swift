a4b6d80b90ba2536aef8e791b60c3a0fe9072de4
Nihad Samedov <the_nihad@icloud.com>
2022-07-26 17:18:25 +0300

    Refactoring

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,45 +7,27 @@
 
 import Foundation
 
-struct Pair: Hashable{
-    var r: Int = 0
-    var c: Int = 0
-
-    var hashValue: Int {
-        "(\(r),\(c))".hashValue
-    }
-
-    init(_ r: Int, _ c: Int) {
-        self.r = r
-        self.c = c
-    }
-
-    static func == (lhs: Pair, rhs: Pair) -> Bool {
-        return lhs.r == rhs.r && lhs.c == rhs.c
-    }
-}
-
 class Solution {
     func isValidSudoku(_ board: [[Character]]) -> Bool {
         var rows = [Int: Character]()
         var cols = [Int: Character]()
-        var squares = [Pair: Character]()
+        var squares = [String: Character]()
 
-        for r in 0..<9 {
-            for c in 0..<9 {
-                if board[r][c] == "." {
+        for i in 0..<9 {
+            for j in 0..<9 {
+                if board[i][j] == "." {
                     continue
                 }
 
-                if board[r][c] == rows[r] ||
-                   board[r][c] == cols[c] ||
-                   board[r][c] == squares[Pair(r / 3, c / 3)] {
+                if board[i][j] == rows[i] ||
+                   board[i][j] == cols[j] ||
+                   board[i][j] == squares["\(i/3),\(j/3)"] {
                     return false
                 }
 
-                rows[r] = board[r][c]
-                cols[c] = board[r][c]
-                squares[Pair(r / 3, c / 3)] = board[r][c]
+                rows[i] = board[i][j]
+                cols[j] = board[i][j]
+                squares["\(i/3),\(j/3)"] = board[i][j]
             }
         }
 
