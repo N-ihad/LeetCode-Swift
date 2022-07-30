abe3b77c04c3f124fc72b464791720c487dd817d
Nihad Samedov <the_nihad@icloud.com>
2022-07-30 20:49:44 +0300

    Solution

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,14 @@
 import Foundation
 
 class Solution {
-    
+    var prev: TreeNode? = nil
+    func flatten(_ root: TreeNode?) {
+        if root == nil { return }
+
+        flatten(root!.right)
+        flatten(root!.left)
+        root!.right = prev
+        root!.left = nil
+        prev = root!
+    }
 }
