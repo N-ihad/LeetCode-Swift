5928cbb2169e8b0fe2423c38420e7a6932fd1924
Nihad Samedov <the_nihad@icloud.com>
2022-08-01 14:58:09 +0300

    Solution: Backtracking + Memoization

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,27 @@
 import Foundation
 
 class Solution {
-    
+    func wordBreak(_ s: String, _ wordDict: [String]) -> Bool {
+        let wordDict = Set<[Character]>(wordDict.map { Array($0) })
+        let s = Array(s)
+        var cache: [[Character]: Bool] = [:]
+        return _wordBreak(s, wordDict, &cache)
+    }
+
+    private func _wordBreak(_ word: [Character], _ wordDict: Set<[Character]>, _ cache: inout [[Character]: Bool]) -> Bool {
+        if let cacheResult = cache[word] { return cacheResult }
+        if word.count == 0 { return true }
+
+        var match: [Character] = []
+        for (i, c) in word.enumerated() {
+            match.append(c)
+
+            cache[match] = wordDict.contains(match)
+            if wordDict.contains(match) && _wordBreak(Array(word[i+1..<word.count]), wordDict, &cache) {
+                return true
+            }
+        }
+
+        return false
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,73 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_wordBreak_1() {
+        // given
+        let s = "leetcode"
+        let wordDict = ["leet","code"]
+
+        // when
+        let result = sut.wordBreak(s, wordDict)
+
+        // then
+        let expected = true
+
+        XCTAssertEqual(result, expected)
+    }
+
+    func testSolution_wordBreak_2() {
+        // given
+        let s = "applepenapple"
+        let wordDict = ["apple","pen"]
+
+        // when
+        let result = sut.wordBreak(s, wordDict)
+
+        // then
+        let expected = true
+
+        XCTAssertEqual(result, expected)
+    }
+
+    func testSolution_wordBreak_3() {
+        // given
+        let s = "catsandog"
+        let wordDict = ["cats","dog","sand","and","cat"]
+
+        // when
+        let result = sut.wordBreak(s, wordDict)
+
+        // then
+        let expected = false
+
+        XCTAssertEqual(result, expected)
+    }
+
+    func testSolution_wordBreak_4() {
+        // given
+        let s = "aaaaaaa"
+        let wordDict = ["aaaa","aaa"]
+
+        // when
+        let result = sut.wordBreak(s, wordDict)
+
+        // then
+        let expected = true
+
+        XCTAssertEqual(result, expected)
+    }
+
+    func testSolution_wordBreak_5() {
+        // given
+        let s = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab"
+        let wordDict = ["a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"]
+
+        // when
+        let result = sut.wordBreak(s, wordDict)
+
+        // then
+        let expected = false
+
+        XCTAssertEqual(result, expected)
     }
 }
