f203fa35cae43ef0535fedca8ea8cd800d05071f
Nihad Samedov <the_nihad@icloud.com>
2022-08-02 20:00:39 +0300

    Solution: Backtracking

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,29 @@
 import Foundation
 
 class Solution {
-    
+    func wordBreak(_ s: String, _ wordDict: [String]) -> [String] {
+        let wordDict = Set<[Character]>(wordDict.map { Array($0) })
+        let s = Array(s)
+        var partialResult: [String] = []
+        var result: [String] = []
+        _wordBreak(s, wordDict, &partialResult, &result)
+        return result
+    }
+
+    private func _wordBreak(_ word: [Character], _ wordDict: Set<[Character]>, _ partialResult: inout [String], _ result: inout [String]) {
+        if word.count == 0 {
+            result.append(partialResult.joined(separator: " "))
+            return
+        }
+
+        var match: [Character] = []
+        for (i, c) in word.enumerated() {
+            match.append(c)
+            if wordDict.contains(match) {
+                partialResult.append(String(match))
+                _wordBreak(Array(word[i+1..<word.count]), wordDict, &partialResult, &result)
+                partialResult.removeLast()
+            }
+        }
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,45 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_wordBreak_1() {
+        // given
+        let s = "catsanddog"
+        let wordDict = ["cat","cats","and","sand","dog"]
+
+        // when
+        let result = sut.wordBreak(s, wordDict)
+
+        // then
+        let expected = ["cats and dog", "cat sand dog"]
+
+        XCTAssertEqual(Set<String>(result), Set<String>(expected))
+    }
+
+    func testSolution_wordBreak_2() {
+        // given
+        let s = "pineapplepenapple"
+        let wordDict = ["apple","pen","applepen","pine","pineapple"]
+
+        // when
+        let result = sut.wordBreak(s, wordDict)
+
+        // then
+        let expected = ["pine apple pen apple","pineapple pen apple","pine applepen apple"]
+
+        XCTAssertEqual(Set<String>(result), Set<String>(expected))
+    }
+
+    func testSolution_wordBreak_3() {
+        // given
+        let s = "catsandog"
+        let wordDict = ["cats","dog","sand","and","cat"]
+
+        // when
+        let result = sut.wordBreak(s, wordDict)
+
+        // then
+        let expected: [String] = []
+
+        XCTAssertEqual(Set<String>(result), Set<String>(expected))
     }
 }
