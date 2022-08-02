49424ee2a77d4faa9a3740390e7d10b9e4c0620f
Nihad Samedov <the_nihad@icloud.com>
2022-08-03 02:21:50 +0300

    Solution (2): Recursion

--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,7 +22,55 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        
+    func testSolution_inorderTraversal_1() {
+        // given
+        let root: TreeNode? = TreeNode(1, nil, TreeNode(2, TreeNode(3)))
+
+        // when
+        let result = sut.inorderTraversal(root)
+
+        // then
+        let expected = [1,3,2]
+
+        XCTAssertEqual(result, expected)
+    }
+
+    func testSolution_inorderTraversal_2() {
+        // given
+        let root: TreeNode? = nil
+
+        // when
+        let result = sut.inorderTraversal(root)
+
+        // then
+        let expected: [Int] = []
+
+        XCTAssertEqual(result, expected)
+    }
+
+    func testSolution_inorderTraversal_3() {
+        // given
+        let root: TreeNode? = TreeNode(1)
+
+        // when
+        let result = sut.inorderTraversal(root)
+
+        // then
+        let expected = [1]
+
+        XCTAssertEqual(result, expected)
+    }
+
+    func testSolution_inorderTraversal_4() {
+        // given
+        let root: TreeNode? = TreeNode(3, TreeNode(1), TreeNode(2))
+
+        // when
+        let result = sut.inorderTraversal(root)
+
+        // then
+        let expected = [1,3,2]
+
+        XCTAssertEqual(result, expected)
     }
 }
