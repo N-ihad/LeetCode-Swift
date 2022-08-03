0453e04dea576dafba1d5566153fe649ae9dd932
Nihad Samedov <the_nihad@icloud.com>
2022-08-03 11:22:37 +0300

    Solution: slow tho, need to use Binary Search Tree or smth

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,38 @@
 import Foundation
 
 class Solution {
+    class MyCalendar {
 
+        var bookings: [[Int]] = []
+
+        init() {
+
+        }
+
+        func book(_ start: Int, _ end: Int) -> Bool {
+
+            if bookings.count == 0 {
+                bookings.append([start, end])
+                return true
+            }
+
+            for i in 0..<bookings.count {
+                if (start < bookings[i][1] && start >= bookings[i][0]) ||
+                   (end > bookings[i][0] && end <= bookings[i][1]) ||
+                   (start <= bookings[i][0] && end >= bookings[i][1]) {
+                    return false
+                }
+            }
+
+            for i in 0..<bookings.count {
+                if start < bookings[i][0] {
+                    bookings.insert([start, end], at: i)
+                    return true
+                }
+            }
+
+            bookings.append([start, end])
+            return true
+        }
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -10,11 +10,11 @@ import XCTest
 
 class SolutionTests: XCTestCase {
 
-    var sut: Solution!
+    var sut: Solution.MyCalendar!
 
     override func setUp() {
         super.setUp()
-        sut = Solution()
+        sut = Solution.MyCalendar()
     }
 
     override func tearDown() {
@@ -22,11 +22,42 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
-        // given
-
+    func testSolution_MyCalendar_1() {
         // when
+        let result = sut.book(10, 20)
 
         // then
+        let expected = true
+
+        XCTAssertEqual(result, expected)
+    }
+
+    func testSolution_MyCalendar_2() {
+        // when
+        let result1 = sut.book(10, 20)
+        let result2 = sut.book(15, 25)
+
+        // then
+        let expected1 = true
+        let expected2 = false
+
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
+    }
+
+    func testSolution_MyCalendar_3() {
+        // when
+        let result1 = sut.book(10, 20)
+        let result2 = sut.book(15, 25)
+        let result3 = sut.book(20, 30)
+
+        // then
+        let expected1 = true
+        let expected2 = false
+        let expected3 = true
+
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
+        XCTAssertEqual(result3, expected3)
     }
 }
