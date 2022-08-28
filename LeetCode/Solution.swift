664b2dc051ef92a604030b06950bebc9b6a7e2e3
Nihad Samedov <the_nihad@icloud.com>
2022-08-29 00:25:36 +0300

    Solution

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,56 @@
 import Foundation
 
 class Solution {
+    func diagonalSort(_ mat: [[Int]]) -> [[Int]] {
+        var result: [[Int]] = mat
+        for i in 0..<mat.count {
+            if (mat.count - 1 - i) + 1 >= mat.count {
+                continue
+            }
 
+            var _i = mat.count - 1 - i
+            var j = 0
+            var arr: [Int] = []
+            while _i < mat.count && _i >= 0 && j < mat[0].count && j >= 0 {
+                arr.append(mat[_i][j])
+                _i += 1
+                j += 1
+            }
+
+            _i = mat.count - 1 - i
+            j = 0
+            arr.sort()
+            for i in 0..<arr.count {
+                result[_i][j] = arr[i]
+                _i += 1
+                j += 1
+            }
+        }
+
+        for j in 1..<mat[0].count {
+            if j + 1 >= mat[0].count {
+                continue
+            }
+
+            var i = 0
+            var _j = j
+            var arr: [Int] = []
+            while i < mat.count && i >= 0 && _j < mat[0].count && _j >= 0 {
+                arr.append(mat[i][_j])
+                i += 1
+                _j += 1
+            }
+
+            i = 0
+            _j = j
+            arr.sort()
+            for j in 0..<arr.count {
+                result[i][_j] = arr[j]
+                i += 1
+                _j += 1
+            }
+        }
+
+        return result
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,11 +22,35 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
+    func testSolution_diagonalSort_1() {
         // given
+        let mat = [[3,3,1,1],
+                   [2,2,1,2],
+                   [1,1,1,2]]
 
         // when
+        let result = sut.diagonalSort(mat)
 
         // then
+        let expected = [[1,1,1,1],[1,2,2,2],[1,2,3,3]]
+
+        XCTAssertEqual(result, expected)
+    }
+
+    func testSolution_diagonalSort_2() {
+        // given
+        let mat = [[11,25,66,1,69,7],
+                   [23,55,17,45,15,52],
+                   [75,31,36,44,58,8],
+                   [22,27,33,25,68,4],
+                   [84,28,14,11,5,50]]
+
+        // when
+        let result = sut.diagonalSort(mat)
+
+        // then
+        let expected = [[5,17,4,1,52,7],[11,11,25,45,8,69],[14,23,25,44,58,15],[22,27,31,36,50,66],[84,28,75,33,55,68]]
+
+        XCTAssertEqual(result, expected)
     }
 }
