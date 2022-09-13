d43b26263bc49929aea56c28117817a6483ce9cb
Nihad Samedov <the_nihad@icloud.com>
2022-09-13 23:47:29 +0300

    Solution: Trie + DFS

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -9,4 +9,130 @@ import Foundation
 
 class Solution {
 
+    // MARK: - Using Trie and cache
+    private final class TrieNode: Hashable, Equatable {
+
+        var value: Character
+        var isTerminal: Bool
+        var children: [Character: TrieNode] = [:]
+
+        init(_ value: Character, isTerminal: Bool = false) {
+            self.value = value
+            self.isTerminal = isTerminal
+        }
+
+        func hash(into hasher: inout Hasher) {
+            hasher.combine(value)
+        }
+
+        static func ==(lhs: TrieNode, rhs: TrieNode) -> Bool {
+            return lhs.value == rhs.value
+        }
+    }
+
+    class WordDictionary {
+
+        private var foundWordsCache: [String: Bool] = [:]
+        private var root: TrieNode = TrieNode("R")
+        private var needToUpdateCache: Bool = false
+
+        func addWord(_ word: String) {
+            var current = root
+            for c in word {
+                if current.children[c] == nil {
+                    current.children[c] = TrieNode(c)
+                }
+                current = current.children[c]!
+            }
+            current.isTerminal = true
+            needToUpdateCache = true
+        }
+
+        func search(_ word: String) -> Bool {
+            let _word = Array(word)
+            guard !needToUpdateCache else {
+                foundWordsCache = [:]
+                foundWordsCache[word] = _search(_word)
+                needToUpdateCache = false
+                return foundWordsCache[word]!
+            }
+
+            if let wasFound = foundWordsCache[word] {
+                return wasFound
+            }
+
+            foundWordsCache[word] = _search(_word)
+            return foundWordsCache[word]!
+        }
+
+        private func _search(_ word: [Character]) -> Bool {
+            let word = Array(word)
+            func dfs(j: Int, root: TrieNode) -> Bool {
+                var current = root
+
+                for i in j..<word.count {
+                    let c = word[i]
+
+                    if c == "." {
+                        for child in current.children.values {
+                            if dfs(j: i + 1, root: child) {
+                                return true
+                            }
+                        }
+                        return false
+                    } else {
+                        if current.children[c] == nil {
+                            return false
+                        }
+                        current = current.children[c]!
+                    }
+                }
+
+                return current.isTerminal
+            }
+            return dfs(j: 0, root: root)
+        }
+    }
+
+    // MARK: - Other solution without use of Tries
+//    class WordDictionary {
+//
+//        var dict: [Int: Set<[Character]>] = [:]
+//
+//        func addWord(_ word: String) {
+//            dict[word.count, default: []].insert(Array(word))
+//        }
+//
+//        func search(_ word: String) -> Bool {
+//
+//            guard let set = dict[word.count] else { return false }
+//            guard word.contains(".") else { return set.contains(Array(word)) }
+//
+//            var letters = [(Character, Int)]()
+//            for (index, char) in word.enumerated() {
+//                if char != "." {
+//                    letters.append((char, index))
+//                }
+//            }
+//
+//            if letters.isEmpty {
+//                return true
+//            }
+//
+//            for a in set {
+//                var match = true
+//                for (char, index) in letters {
+//                    if a[index] != char {
+//                        match = false
+//                        break
+//                    }
+//                }
+//                if match {
+//                    return true
+//                }
+//            }
+//
+//            return false
+//        }
+//    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,11 +22,56 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
+    func testSolution_WordDictionary_1() {
         // given
+        let wordDictionary = Solution.WordDictionary()
 
         // when
+        wordDictionary.addWord("bad")
+        wordDictionary.addWord("dad")
+        wordDictionary.addWord("ddc")
+        wordDictionary.addWord("dd")
+        wordDictionary.addWord("mad")
 
         // then
+        XCTAssertEqual(wordDictionary.search("pad"), false)
+        XCTAssertEqual(wordDictionary.search("bad"), true)
+        XCTAssertEqual(wordDictionary.search(".ad"), true)
+        XCTAssertEqual(wordDictionary.search("b.."), true)
     }
+
+    func testSolution_WordDictionary_2() {
+        // given
+        let wordDictionary = Solution.WordDictionary()
+
+        // when
+        wordDictionary.addWord("a")
+        wordDictionary.addWord("ab")
+
+        // then
+        XCTAssertEqual(wordDictionary.search("a"), true)
+        XCTAssertEqual(wordDictionary.search("a."), true)
+        XCTAssertEqual(wordDictionary.search("ab"), true)
+        XCTAssertEqual(wordDictionary.search(".a"), false)
+        XCTAssertEqual(wordDictionary.search(".b"), true)
+        XCTAssertEqual(wordDictionary.search("ab."), false)
+        XCTAssertEqual(wordDictionary.search(".."), true)
+        XCTAssertEqual(wordDictionary.search("."), true)
+    }
+
+//    func testSolution_WordDictionary_3() {
+//        // given
+//        let wordDictionary = Solution.WordDictionary()
+//
+//        // when
+////        wordDictionary.addWord("lfohkladaibkdemlnonbnhba")
+//
+//        // then
+//        var answer: Bool!
+//        for _ in 0..<10000000 {
+//            answer = wordDictionary.search("lfohkladaibkdemlnonbnhba")
+//        }
+//
+//        XCTAssertEqual(answer, true)
+//    }
 }
