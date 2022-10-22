5f4a9c2665663d75c7873d57e237ca6fb794675e
Nihad Samedov <the_nihad@icloud.com>
2022-10-22 20:14:59 +0400

    Solution

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,23 @@
 import Foundation
 
 class Solution {
+    func maxArea(_ height: [Int]) -> Int {
+        var (maxArea, L, R) = (0, 0, height.count-1)
 
+        while L != R {
+            let area = min(height[L], height[R]) * (R - L)
+
+            if area > maxArea {
+                maxArea = area
+            }
+
+            if height[L] < height[R] {
+                L += 1
+            } else {
+                R -= 1
+            }
+        }
+
+        return maxArea
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,11 +22,42 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
+    func testSolution_maxArea_1() {
         // given
+        let height = [1,8,6,2,5,4,8,3,7]
 
         // when
+        let result = sut.maxArea(height)
 
         // then
+        let expected = 49
+
+        XCTAssertEqual(result, expected)
+    }
+
+    func testSolution_maxArea_2() {
+        // given
+        let height = [1,1]
+
+        // when
+        let result = sut.maxArea(height)
+
+        // then
+        let expected = 1
+
+        XCTAssertEqual(result, expected)
+    }
+
+    func testSolution_maxArea_3() {
+        // given
+        let height = [2,3,4,5,18,17,6]
+
+        // when
+        let result = sut.maxArea(height)
+
+        // then
+        let expected = 17
+
+        XCTAssertEqual(result, expected)
     }
 }
