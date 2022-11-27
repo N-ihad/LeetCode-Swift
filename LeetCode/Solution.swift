1c3c86242dcedce3238ad2fe49f0d63743c1d2dc
Namik <84244347+Nam-Namazov@users.noreply.github.com>
2022-11-27 16:49:25 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -9,33 +9,30 @@ import Foundation
 
 class Solution {
     func trap(_ height: [Int]) -> Int {
-        var maxL = height[0]
-        var maxR = height[height.count-1]
-        var L = 0
-        var R = height.count-1
-        var res = 0
+        var leftPointer = 0
+        var rightPointer = height.count - 1
+        var leftMax = 0
+        var rightMax = 0
+        var result = 0
 
-        while L != R {
-            while maxL <= maxR && L != R {
-                res = maxL - height[L] > 0 ? res + maxL - height[L] : res
-                if height[L] > maxL {
-                    maxL = height[L]
-                    break
+        while leftPointer < rightPointer {
+            if height[leftPointer] < height[rightPointer] {
+                if height[leftPointer] >= leftMax {
+                    leftMax = height[leftPointer]
                 } else {
-                    L += 1
+                    result += leftMax - height[leftPointer]
                 }
-            }
-            while maxR <= maxL && L != R {
-                res = maxR - height[R] > 0 ? res + maxR - height[R] : res
-                if height[R] > maxR {
-                    maxR = height[R]
-                    break
+                leftPointer += 1
+            } else {
+                if height[rightPointer] >= rightMax {
+                    rightMax = height[rightPointer]
                 } else {
-                    R -= 1
+                    result += rightMax - height[rightPointer]
                 }
+                rightPointer -= 1
             }
         }
-
-        return res
+        
+        return result
     }
 }
