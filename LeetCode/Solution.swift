6f14415c82909965f16985af029edd5fd32f855d
Nihad <43179082+N-ihad@users.noreply.github.com>
2022-12-15 20:00:05 +0400

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,22 @@
 import Foundation
 
 class Solution {
+    func minimumRecolors(_ blocks: String, _ k: Int) -> Int {
+        let blocks = Array(blocks)
+        var min = blocks.count
 
+        for i in 0...blocks.count - k {
+            var counter = 0
+
+            for j in 0..<k where blocks[i + j] == "W" {
+                counter += 1
+            }
+
+            if counter < min {
+                min = counter
+            }
+        }
+
+        return min
+    }
 }
