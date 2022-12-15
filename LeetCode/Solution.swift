30c1be462bcf2405ee8de81295feee58df900aee
Nihad <43179082+N-ihad@users.noreply.github.com>
2022-12-15 19:48:07 +0400

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,15 @@
 import Foundation
 
 class Solution {
-
+    func minimumDifference(_ nums: [Int], _ k: Int) -> Int {
+        let nums = nums.sorted()
+        var min = 100000
+        for i in 0...nums.count - k {
+            let difference = nums[i + k - 1] - nums[i]
+            if difference < min {
+                min = difference
+            }
+        }
+        return min
+    }
 }
