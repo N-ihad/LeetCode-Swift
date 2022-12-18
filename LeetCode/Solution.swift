8e37dac1e84029d51781c5898f4c8e65297e1c34
Nihad <43179082+N-ihad@users.noreply.github.com>
2022-12-19 00:24:45 +0400

    Solution: Greedy O(n)

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,17 @@
 import Foundation
 
 class Solution {
+    func canJump(_ nums: [Int]) -> Bool {
+        var goal = nums.count - 1
+        var i = nums.count - 2
 
+        while i >= 0 {
+            if i + nums[i] >= goal {
+                goal = i
+            }
+            i -= 1
+        }
+
+        return goal == 0
+    }
 }
