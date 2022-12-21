c1814d53863ab94168ccb0e0507d03251fc7956b
Nihad Samedov <the_nihad@icloud.com>
2022-12-21 03:13:58 +0300

    Solution: Recursion

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,27 @@
 import Foundation
 
 class Solution {
+    func invertTree(_ root: TreeNode?) -> TreeNode? {
+        _invertTree(root)
+        return root
+    }
 
+    private func _invertTree(_ node: TreeNode?) {
+        if node == nil { return }
+
+        _invertTree(node?.left)
+        _invertTree(node?.right)
+
+        if node?.left != nil && node?.right == nil {
+            node?.right = node?.left
+            node?.left = nil
+        } else if node?.left == nil && node?.right != nil {
+            node?.left = node?.right
+            node?.right = nil
+        } else if node?.left != nil && node?.right != nil {
+            let temp = node?.left
+            node?.left = node?.right
+            node?.right = temp
+        }
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,11 +22,42 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
+    func testSolution_invertTree_1() {
         // given
+        let root = TreeNode(4, TreeNode(2, TreeNode(1), TreeNode(3)), TreeNode(7, TreeNode(6), TreeNode(9)))
 
         // when
+        let result = Helper.treeString(sut.invertTree(root))
 
         // then
+        let expected = Helper.treeString(TreeNode(4, TreeNode(7, TreeNode(9), TreeNode(6)), TreeNode(2, TreeNode(3), TreeNode(1))))
+
+        XCTAssertEqual(result, expected)
+    }
+
+    func testSolution_invertTree_2() {
+        // given
+        let root = TreeNode(2, TreeNode(1), TreeNode(3))
+
+        // when
+        let result = Helper.treeString(sut.invertTree(root))
+
+        // then
+        let expected = Helper.treeString(TreeNode(2, TreeNode(3), TreeNode(1)))
+
+        XCTAssertEqual(result, expected)
+    }
+
+    func testSolution_invertTree_3() {
+        // given
+        let root: TreeNode? = nil
+
+        // when
+        let result = Helper.treeString(sut.invertTree(root))
+
+        // then
+        let expected = Helper.treeString(nil)
+
+        XCTAssertEqual(result, expected)
     }
 }
