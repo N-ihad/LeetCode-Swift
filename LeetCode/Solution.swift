a5ab5d8d199d5b1cfd00021f6e44327e82a5aa87
Nihad Samedov <the_nihad@icloud.com>
2022-12-24 22:26:53 +0300

    Solution: DFS + reverse

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,23 @@
 import Foundation
 
 class Solution {
+    func levelOrderBottom(_ root: TreeNode?) -> [[Int]] {
+        var result: [[Int]] = []
+        _levelOrderBottom(root, &result, 0)
+        return result.reversed()
+    }
 
+    private func _levelOrderBottom(_ root: TreeNode?, _ result: inout [[Int]], _ level: Int) {
+        if root == nil { return }
+        let isIndexExists = result.indices.contains(level)
+
+        if isIndexExists {
+            result[level].append(root!.val)
+        } else {
+            result.append([root!.val])
+        }
+
+        _levelOrderBottom(root!.left, &result, level + 1)
+        _levelOrderBottom(root!.right, &result, level + 1)
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,11 +22,42 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
+    func testSolution_levelOrderBottom_1() {
         // given
+        let root = TreeNode(3, TreeNode(9), TreeNode(20, TreeNode(15), TreeNode(7)))
 
         // when
+        let expected = [[15,7],[9,20],[3]]
 
         // then
+        let result = sut.levelOrderBottom(root)
+
+        XCTAssertEqual(expected, result)
+    }
+
+    func testSolution_levelOrderBottom_2() {
+        // given
+        let root = TreeNode(1)
+
+        // when
+        let expected = [[1]]
+
+        // then
+        let result = sut.levelOrderBottom(root)
+
+        XCTAssertEqual(expected, result)
+    }
+
+    func testSolution_levelOrderBottom_3() {
+        // given
+        let root: TreeNode? = nil
+
+        // when
+        let expected: [[Int]] = []
+
+        // then
+        let result = sut.levelOrderBottom(root)
+
+        XCTAssertEqual(expected, result)
     }
 }
