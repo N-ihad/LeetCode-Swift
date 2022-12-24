0b4994ae8eb97067edf689aeae1dffdcdeb6d91a
Nihad Samedov <the_nihad@icloud.com>
2022-12-24 20:49:42 +0300

    Solution: DFS

--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,11 +22,45 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
+    func testSolution_isSameTree_1() {
         // given
+        let root1 = TreeNode(1, TreeNode(2), TreeNode(3))
+        let root2 = TreeNode(1, TreeNode(2), TreeNode(3))
 
         // when
+        let result = sut.isSameTree(root1, root2)
 
         // then
+        let expected = true
+
+        XCTAssertEqual(result, expected)
+    }
+
+    func testSolution_isSameTree_2() {
+        // given
+        let root1 = TreeNode(1, TreeNode(2))
+        let root2 = TreeNode(1, nil, TreeNode(2))
+
+        // when
+        let result = sut.isSameTree(root1, root2)
+
+        // then
+        let expected = false
+
+        XCTAssertEqual(result, expected)
+    }
+
+    func testSolution_isSameTree_3() {
+        // given
+        let root1 = TreeNode(1, TreeNode(2), TreeNode(1))
+        let root2 = TreeNode(1, TreeNode(1), TreeNode(2))
+
+        // when
+        let result = sut.isSameTree(root1, root2)
+
+        // then
+        let expected = false
+
+        XCTAssertEqual(result, expected)
     }
 }
