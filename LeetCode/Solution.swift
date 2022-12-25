9b823fb577cb1b85bf5f09e81d9657cda9d8b196
Nihad Samedov <the_nihad@icloud.com>
2022-12-25 05:05:00 +0300

    Revert commit

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,22 +8,5 @@
 import Foundation
 
 class Solution {
-    var (maxLevel, maxValue) = (0, -1)
-
-    func findBottomLeftValue(_ root: TreeNode?) -> Int {
-        DFS(root, 1)
-        return maxValue
-    }
-
-    func DFS(_ root: TreeNode?, _ currentLevel: Int) {
-        if root == nil { return }
-
-        if currentLevel > maxLevel {
-            maxLevel = currentLevel
-            maxValue = root!.val
-        }
-
-        DFS(root!.left, currentLevel + 1)
-        DFS(root!.right, currentLevel + 1)
-    }
+    
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,42 +22,11 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_findBottomLeftValue_1() {
+    func testSolution_() {
         // given
-        let root = TreeNode(2, TreeNode(1), TreeNode(3))
 
         // when
-        let result = sut.findBottomLeftValue(root)
 
         // then
-        let expected = 1
-
-        XCTAssertEqual(result, expected)
-    }
-
-    func testSolution_findBottomLeftValue_2() {
-        // given
-        let root = TreeNode(1, TreeNode(2, TreeNode(4)), TreeNode(3, TreeNode(5, TreeNode(7)), TreeNode(6)))
-
-        // when
-        let result = sut.findBottomLeftValue(root)
-
-        // then
-        let expected = 7
-
-        XCTAssertEqual(result, expected)
-    }
-
-    func testSolution_findBottomLeftValue_3() {
-        // given
-        let root = TreeNode(0)
-
-        // when
-        let result = sut.findBottomLeftValue(root)
-
-        // then
-        let expected = 0
-
-        XCTAssertEqual(result, expected)
     }
 }
