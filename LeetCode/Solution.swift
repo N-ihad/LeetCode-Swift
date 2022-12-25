171e084258a1510508b4ec3d224618b254882623
Nihad Samedov <the_nihad@icloud.com>
2022-12-25 13:32:13 +0300

    Solution: DFS

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,24 @@
 import Foundation
 
 class Solution {
-    
+    func pathSum(_ root: TreeNode?, _ targetSum: Int) -> [[Int]] {
+        var paths: [[Int]] = []
+        _pathSum(root, targetSum, [], &paths)
+        return paths
+    }
+
+    private func _pathSum(_ root: TreeNode?, _ targetSum: Int, _ path: [Int], _ paths: inout [[Int]]) {
+        if root == nil { return }
+
+        var path = path
+        path.append(root!.val)
+
+        if root!.left == nil && root!.right == nil && targetSum - root!.val == 0 {
+            paths.append(path)
+            return
+        }
+        
+        _pathSum(root!.left, targetSum - root!.val, path, &paths)
+        _pathSum(root!.right, targetSum - root!.val, path, &paths)
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,11 +22,42 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
+    func testSolution_pathSum_1() {
         // given
+        let root = TreeNode(5, TreeNode(4, TreeNode(11, TreeNode(7), TreeNode(2))), TreeNode(8, TreeNode(13), TreeNode(4, TreeNode(5), TreeNode(1))))
 
         // when
+        let result = sut.pathSum(root, 22)
 
         // then
+        let expected = [[5,4,11,2],[5,8,4,5]]
+
+        XCTAssertEqual(result, expected)
+    }
+
+    func testSolution_pathSum_2() {
+        // given
+        let root = TreeNode(1, TreeNode(2), TreeNode(3))
+
+        // when
+        let result = sut.pathSum(root, 5)
+
+        // then
+        let expected: [[Int]] = []
+
+        XCTAssertEqual(result, expected)
+    }
+
+    func testSolution_pathSum_3() {
+        // given
+        let root: TreeNode? = nil
+
+        // when
+        let result = sut.pathSum(root, 0)
+
+        // then
+        let expected: [[Int]] = []
+
+        XCTAssertEqual(result, expected)
     }
 }
