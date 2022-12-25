ebbff34cd766fe14b96b98f91e1808667154f707
Nihad Samedov <the_nihad@icloud.com>
2022-12-25 12:53:46 +0300

    Solution: DFS

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,10 @@
 import Foundation
 
 class Solution {
-    
+    func hasPathSum(_ root: TreeNode?, _ targetSum: Int) -> Bool {
+        if root == nil { return false }
+        if root!.left == nil && root!.right == nil && targetSum - root!.val == 0 { return true }
+
+        return hasPathSum(root!.left, targetSum - root!.val) || hasPathSum(root!.right, targetSum - root!.val)
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,11 +22,42 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
+    func testSolution_hasPathSum_1() {
         // given
+        let root = TreeNode(5, TreeNode(4, TreeNode(11, TreeNode(7), TreeNode(2))), TreeNode(8, TreeNode(13), TreeNode(4, nil, TreeNode(1))))
 
         // when
+        let result = sut.hasPathSum(root, 22)
 
         // then
+        let expected = true
+
+        XCTAssertEqual(result, expected)
+    }
+
+    func testSolution_hasPathSum_2() {
+        // given
+        let root = TreeNode(1, TreeNode(2), TreeNode(3))
+
+        // when
+        let result = sut.hasPathSum(root, 5)
+
+        // then
+        let expected = false
+
+        XCTAssertEqual(result, expected)
+    }
+
+    func testSolution_hasPathSum_3() {
+        // given
+        let root: TreeNode? = nil
+
+        // when
+        let result = sut.hasPathSum(root, 0)
+
+        // then
+        let expected = false
+
+        XCTAssertEqual(result, expected)
     }
 }
