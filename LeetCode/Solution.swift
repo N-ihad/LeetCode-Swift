afe4ef036e05b2bce238bc2510db4608778a5164
Nihad Samedov <the_nihad@icloud.com>
2023-01-06 14:31:12 +0300

    Solution

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,20 @@
 import Foundation
 
 class Solution {
-    
+    func maxIceCream(_ costs: [Int], _ coins: Int) -> Int {
+        let costs = costs.sorted()
+        var coins = coins
+
+        for i in 0..<costs.count {
+            if costs[i] > coins {
+                return i
+            }
+
+            if i == costs.count - 1 { return i + 1 }
+
+            coins -= costs[i]
+        }
+
+        return 0
+    }
 }
--- LeetCodeTests/SolutionTests.swift
+++ LeetCodeTests/SolutionTests.swift
@@ -22,11 +22,45 @@ class SolutionTests: XCTestCase {
         super.tearDown()
     }
 
-    func testSolution_() {
+    func testSolution_maxIceCream_1() {
         // given
+        let costs = [1,3,2,4,1]
+        let coins = 7
 
         // when
+        let result = sut.maxIceCream(costs, coins)
 
         // then
+        let expected = 4
+
+        XCTAssertEqual(result, expected)
+    }
+
+    func testSolution_maxIceCream_2() {
+        // given
+        let costs = [10,6,8,7,7,8]
+        let coins = 5
+
+        // when
+        let result = sut.maxIceCream(costs, coins)
+
+        // then
+        let expected = 0
+
+        XCTAssertEqual(result, expected)
+    }
+
+    func testSolution_maxIceCream_3() {
+        // given
+        let costs = [1,6,3,1,2,5]
+        let coins = 20
+
+        // when
+        let result = sut.maxIceCream(costs, coins)
+
+        // then
+        let expected = 6
+
+        XCTAssertEqual(result, expected)
     }
 }
