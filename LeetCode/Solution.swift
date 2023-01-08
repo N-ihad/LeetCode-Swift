1476beac0d0f4213dde362073c65ecc3cce694c7
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-01-08 20:36:53 +0400

    Solution

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -6,7 +6,40 @@
 //
 
 import Foundation
-
 class Solution {
-    
+    func sortList(_ head: ListNode?) -> ListNode? {
+        if head?.next == nil { return head }
+        
+        var slow = head
+        var fast = head?.next
+        while fast != nil && fast?.next != nil {
+            slow = slow?.next
+            fast = fast?.next?.next
+        }
+        
+        let leftHalf = head
+        let rightHalf = slow?.next
+        
+        slow?.next = nil
+        
+        var currLeft = sortList(leftHalf)
+        var currRight = sortList(rightHalf)
+        
+        let dummy = ListNode(-1)
+        var curr: ListNode? = dummy
+        
+        while currLeft != nil || currRight != nil {
+            if currLeft?.val ?? Int.max <= currRight?.val ?? Int.max {
+                curr?.next = currLeft
+                currLeft = currLeft?.next
+            } else {
+                curr?.next = currRight
+                currRight = currRight?.next
+            }
+            
+            curr = curr?.next
+        }
+        
+        return dummy.next
+    }
 }
