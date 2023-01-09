1aec01d4f783f1779e8b3c86a96c27f47a26b3ff
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-01-09 22:35:42 +0400

    Solution

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,48 @@
 import Foundation
 
 class Solution {
+    func preorderTraversal(_ root: TreeNode?) -> [Int] {
+        if root == nil { return [] }
+        
+        var stack = Stack(root!)
+        
+        while !stack.isEmpty {
+            let poppedElement = stack.pop()
+            stack.push(poppedElement?.left)
+            stack.push(poppedElement?.right)
+        }
+        
+        return stack.popHistory
+    }
+}
+
+struct Stack {
     
+    private var elements: [TreeNode] = []
+    private(set) var popHistory: [Int] = []
+    
+    init(_ element: TreeNode) {
+        elements = [element]
+    }
+    
+    var isEmpty: Bool {
+        elements.isEmpty
+    }
+    
+    var top: TreeNode? {
+        elements.last
+    }
+    
+    mutating func push(_ newElement: TreeNode?) {
+        if newElement != nil {
+            elements.append(newElement!)
+        }
+    }
+    
+    mutating func pop() -> TreeNode? {
+        if elements.isEmpty { return nil }
+        let poppedElement = elements.removeLast()
+        popHistory.append(poppedElement.val)
+        return poppedElement
+    }
 }
