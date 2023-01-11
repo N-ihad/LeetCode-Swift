afe134f9c8b44978695956b11b0339a1ebfc5df4
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-01-12 02:39:11 +0400

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,32 @@
 import Foundation
 
 class Solution {
+    func minTime(_ n: Int, _ edges: [[Int]], _ hasApple: [Bool]) -> Int {
+        var adjacencyList: [[Int]] = Array(repeating: [], count: n)
+        
+        for edge in edges {
+            let currentNode = edge[0]
+            let neighbour = edge[1]
+            
+            adjacencyList[currentNode].append(neighbour)
+            adjacencyList[neighbour].append(currentNode)
+        }
+        
+        return dfs(adjacencyList, hasApple, 0, -1)
+    }
     
+    private func dfs(_ adjacencyList: [[Int]], _ hasApple: [Bool], _ currentNode: Int, _ parentNode: Int) -> Int {
+        var time = 0
+        
+        for child in adjacencyList[currentNode] {
+            if child == parentNode { continue }
+            
+            let childTime = dfs(adjacencyList, hasApple, child, currentNode)
+            
+            if childTime != 0 || hasApple[child] {
+                time += 2 + childTime
+            }
+        }
+        return time
+    }
 }
