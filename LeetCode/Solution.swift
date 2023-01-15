d8da0158aa8ec609c2cde986c50ca27feaf1037b
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-01-15 04:05:43 +0400

    Solution

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,23 @@
 import Foundation
 
 class Solution {
-    
+    func swapPairs(_ head: ListNode?) -> ListNode? {
+        if head == nil || head?.next == nil { return head }
+
+        let dummy = ListNode(-1)
+
+        var prev: ListNode? = dummy
+        var curr: ListNode? = head
+        while curr != nil && curr!.next != nil {
+            let nextPair = curr!.next!.next
+            let next = curr!.next
+            prev!.next = next
+            curr!.next = nextPair
+            next!.next = curr
+            prev = curr
+            curr = nextPair
+        }
+        
+        return dummy.next
+    }
 }
