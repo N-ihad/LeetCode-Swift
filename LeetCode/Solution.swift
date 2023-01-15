156ac774d3641f41eaf13efdb5b86461dcbed99c
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-01-16 03:51:02 +0400

    Solution

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,13 @@
 import Foundation
 
 class Solution {
-    
+    func isBalanced(_ root: TreeNode?) -> Bool {
+        if root == nil { return true }
+        return abs(height(root!.left) - height(root!.right)) <= 1 && isBalanced(root!.left) && isBalanced(root!.right)
+    }
+
+    private func height(_ root: TreeNode?) -> Int {
+        if root == nil { return 0 }
+        return 1 + max(height(root!.left), height(root!.right))
+    }
 }
