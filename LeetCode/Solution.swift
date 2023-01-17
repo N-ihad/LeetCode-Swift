60782c3c9db295da5c8f2f7708fa79b823fa4329
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-01-17 06:00:19 +0400

    Solution

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,30 @@
 import Foundation
 
 class Solution {
-    
+    func insert(_ intervals: [[Int]], _ newInterval: [Int]) -> [[Int]] {
+        var newIntervals: [[Int]] = intervals
+
+        for i in 0..<intervals.count {
+            if newInterval[0] <= intervals[i][0] {
+                newIntervals.insert(newInterval, at: i)
+                break
+            }
+        }
+
+        if newIntervals.count == intervals.count {
+            newIntervals.append(newInterval)
+        }
+
+        var result = [newIntervals.first!]
+        for interval in newIntervals {
+            let n = result.count
+            if result[n-1][1] >= interval[0] {
+                result[n-1][1] = max(result[n-1][1], interval[1])
+            } else {
+                result.append(interval)
+            }
+        }
+
+        return result
+    }
 }
