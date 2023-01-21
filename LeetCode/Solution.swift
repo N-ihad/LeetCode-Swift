7bb267d42f12ba9b4657477b0139355236928199
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-01-21 23:23:42 +0400

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,21 @@
 import Foundation
 
 class Solution {
-    
+    func isPalindrome(_ s: String) -> Bool {
+        let s = Array(s)
+        var (L, R) = (0, s.count-1)
+        
+        while L <= R {
+            if !s[L].isLetter && !s[L].isNumber {
+                L += 1
+            } else if !s[R].isLetter && !s[R].isNumber {
+                R -= 1
+            } else {
+                if s[L].lowercased() != s[R].lowercased() { return false }
+                (L, R) = (L+1, R-1)
+            }
+        }
+
+        return true
+    }
 }
