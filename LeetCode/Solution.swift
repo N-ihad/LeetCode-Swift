e1a26ea90974a2ba89266d761bbedf81baa0faf7
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-01-23 03:29:18 +0400

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,8 @@
 import Foundation
 
 class Solution {
-    
+    func intersection(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
+        let intersection = Set(nums1).intersection(Set(nums2))
+        return Array(intersection)
+    }
 }
