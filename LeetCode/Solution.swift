271bbb860ffd91ea3176f8deaacf08102d4fba70
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-01-22 20:49:46 +0400

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,24 @@
 import Foundation
 
 class Solution {
-    
+    func removeElements(_ head: ListNode?, _ val: Int) -> ListNode? {
+        let dummy = ListNode(-1, head)
+        
+        var prev: ListNode? = dummy
+        var curr: ListNode? = head
+        while curr != nil {
+            let next = curr!.next
+
+            if curr!.val == val {
+                curr!.next = nil
+                prev!.next = next
+            } else {
+                prev = curr
+            }
+
+            curr = next
+        }
+
+        return dummy.next
+    }
 }
