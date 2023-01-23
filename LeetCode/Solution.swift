85d69a100dcee9487085ab6c9a8d0801c70a38de
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-01-24 03:56:32 +0400

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,19 @@
 import Foundation
 
 class Solution {
-    
+    func containsNearbyDuplicate(_ nums: [Int], _ k: Int) -> Bool {
+        var hashSet = Set<Int>(minimumCapacity: nums.count)
+
+        for i in 0..<nums.count {
+            if !hashSet.insert(nums[i]).inserted {
+                return true
+            }
+
+            if i >= k {
+                hashSet.remove(nums[i-k])
+            }
+        }
+
+        return false
+    }
 }
