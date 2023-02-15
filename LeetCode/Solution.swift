60edfaae6899bb31ee22f2c7ca3ecf8b9dd4de01
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-02-16 00:36:54 +0400

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,23 @@
 import Foundation
 
 class Solution {
-    
+    func twoSum(_ numbers: [Int], _ target: Int) -> [Int] {
+        var (L, R) = (0, numbers.count-1)
+        
+        while L < R {
+            let sum = numbers[L] + numbers[R]
+
+            if sum == target {
+                return [L+1, R+1]
+            }
+
+            if sum > target {
+                R -= 1
+            } else {
+                L += 1
+            }
+        }
+
+        return []
+    }
 }
