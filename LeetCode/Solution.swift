5f794bb6e82fd722c71e7b7cd8f3fcdfe9308b8e
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-02-19 00:14:15 +0400

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,19 @@
 import Foundation
 
 class Solution {
-    
+    func replaceElements(_ arr: [Int]) -> [Int] {
+        let n = arr.count
+        var result = arr
+        var maximum = Int.min
+
+        result.reserveCapacity(arr.count)
+
+        for i in 0..<n {
+            result[n-1-i] = maximum
+            maximum = max(maximum, arr[n-1-i])
+        }
+
+        result[n-1] = -1
+        return result
+    }
 }
