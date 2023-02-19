1f27d455ff45018ec758b6bd26d272481f96e9e2
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-02-19 19:17:29 +0400

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,26 @@
 import Foundation
 
 class Solution {
-    
+    func canConstruct(_ ransomNote: String, _ magazine: String) -> Bool {
+        let ransomNote = Array(ransomNote)
+        let magazine = Array(magazine)
+
+        var characterCounts: [Character: Int] = [:]
+
+        characterCounts.reserveCapacity(magazine.count)
+
+        for i in 0..<magazine.count {
+            characterCounts[magazine[i], default: 0] += 1
+        }
+
+        for i in 0..<ransomNote.count {
+            let char = ransomNote[i]
+            if characterCounts[char] == nil || characterCounts[char]! == 0 {
+                return false
+            }
+            characterCounts[char]! -= 1
+        }
+
+        return true
+    }
 }
