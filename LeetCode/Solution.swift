c824760306c4cfcbb71d885aef76bc3c2629851a
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-02-20 00:19:49 +0400

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,16 @@
 import Foundation
 
 class Solution {
-    
+    func sumOfLeftLeaves(_ root: TreeNode?) -> Int {
+        if root == nil { return 0 }
+        return dfs(root!.left, isLeft: true) + dfs(root!.right, isLeft: false)
+    }
+
+    private func dfs(_ node: TreeNode?, isLeft: Bool) -> Int {
+        if node == nil { return 0 }
+        if node!.left == nil && node!.right == nil && isLeft {
+            return node!.val
+        }
+        return dfs(node!.left, isLeft: true) + dfs(node!.right, isLeft: false)
+    }
 }
