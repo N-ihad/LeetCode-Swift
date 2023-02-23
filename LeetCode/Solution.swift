2b37812317d879bbf7403c68bc02423562d8748c
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-02-23 12:58:29 +0400

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,18 @@
 import Foundation
 
 class Solution {
-    
+    func isSubsequence(_ s: String, _ t: String) -> Bool {
+        let (s, t) = (Array(s), Array(t))
+        var (i, j) = (0, 0)
+
+        while i != s.count && j != t.count {
+            if s[i] == t[j] {
+                i += 1
+            }
+            
+            j += 1
+        }
+
+        return i == s.count
+    }
 }
