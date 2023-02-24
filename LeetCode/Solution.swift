b1ec341d849807109b40b19574f4efa0ab7ccd7c
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-02-24 22:45:04 +0400

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,19 @@
 import Foundation
 
 class Solution {
-    
+    func findPeakElement(_ nums: [Int]) -> Int {
+        var L = 0
+        var R = nums.count - 1
+
+        while L != R {
+            let mid = L + (R - L) / 2
+            if nums[mid] < nums[mid + 1] {
+                L = mid + 1
+            } else {
+                R = mid
+            }
+        }
+
+        return L
+    }
 }
