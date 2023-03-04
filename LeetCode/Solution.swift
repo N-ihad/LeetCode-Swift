a9797a2d9a3a6cb8bcbbddc5d90f8b80aa43185a
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-03-04 20:11:35 +0400

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,28 @@
 import Foundation
 
 class Solution {
-    
+    func matrixReshape(_ mat: [[Int]], _ r: Int, _ c: Int) -> [[Int]] {
+        if r * c != mat.count * mat[0].count ||
+           r == mat.count && c == mat[0].count { 
+            return mat   
+        }
+
+        var result = Array(repeating: Array(repeating: 0, count: c), count: r)
+        var (row, column) = (0, 0)
+        
+        for i in 0..<mat.count {
+            for j in 0..<mat[0].count {
+                result[row][column] = mat[i][j]
+
+                if column + 1 > c - 1 {
+                    column = 0
+                    row += 1
+                } else {
+                    column += 1
+                }
+            }
+        }
+
+        return result
+    }
 }
