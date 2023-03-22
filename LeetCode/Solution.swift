d51b4c8a13aca3ac2a503c112855b621c98a5ba2
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-03-22 22:19:40 +0400

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,26 @@
 import Foundation
 
 class Solution {
-    
+    func reverseVowels(_ s: String) -> String {
+        var s = Array(s)
+        let vowels: Set<Character> = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+
+        var (L, R) = (0, s.count-1)
+        while L < R {
+            if vowels.contains(s[L]) && vowels.contains(s[R]) {
+                s.swapAt(L, R)
+                L += 1
+                R -= 1
+            } else if vowels.contains(s[L]) {
+                R -= 1
+            } else if vowels.contains(s[R]) {
+                L += 1
+            } else {
+                L += 1
+                R -= 1
+            }
+        }
+
+        return String(s)
+    }
 }
