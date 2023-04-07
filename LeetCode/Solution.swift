08a8467463101346925744bb0f6de29692482b1b
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-04-07 16:28:26 +0400

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,36 @@
 import Foundation
 
 class Solution {
-    
+    func evalRPN(_ tokens: [String]) -> Int {
+        var stack: [Int] = []
+
+        for i in 0..<tokens.count {
+            if let num = Int(tokens[i]) {
+                stack.append(num)
+            } else {
+                let (num2, num1) = (stack.removeLast(), stack.removeLast())
+                let operation = tokens[i]
+                let newNum = calculate(operation, num1, num2)
+                stack.append(newNum)
+            }
+        }
+
+        return stack[0]
+    }
+
+    @inline(__always)
+    private func calculate(_ operation: String, _ num1: Int, _ num2: Int) -> Int {
+        switch operation {
+        case "+":
+            return num1 + num2
+        case "-":
+            return num1 - num2
+        case "*":
+            return num1 * num2
+        case "/":
+            return num1 / num2
+        default:
+            fatalError("unexpected character encountered")
+        }
+    }
 }
