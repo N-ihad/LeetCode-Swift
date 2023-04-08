f2727bd6de87a01bce7642b8f63792d947197af9
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-04-08 18:25:01 +0400

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,20 @@
 import Foundation
 
 class Solution {
-    
+    func numIdenticalPairs(_ nums: [Int]) -> Int {
+        var counts: [Int: Int] = [:]
+
+        for i in 0..<nums.count {
+            counts[nums[i], default: -1] += 1
+        }
+
+        var counter = 0
+        for count in counts.values {
+            if count > 0 {
+                counter += count * (count + 1) / 2
+            }
+        }
+        
+        return counter
+    }
 }
