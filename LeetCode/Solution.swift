6ffb17aa93b5d2b6f58aa6e7950079caf0b1a067
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-04-09 21:28:52 +0400

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,22 @@
 import Foundation
 
 class Solution {
-    
+    func leftRigthDifference(_ nums: [Int]) -> [Int] {
+        var sum = 0
+        var result: [Int] = []
+        result.reserveCapacity(nums.count)
+
+        for i in 0..<nums.count {
+            result.append(sum)
+            sum += nums[i]
+        }
+
+        sum = 0
+        for i in 0..<nums.count {
+            result[nums.count-1-i] = abs(result[nums.count-1-i] - sum)
+            sum += nums[nums.count-1-i]
+        }
+
+        return result
+    }
 }
