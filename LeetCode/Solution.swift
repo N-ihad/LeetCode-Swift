9d50d601e33261108939632432ff270756ec8bd4
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-07-01 15:10:25 +0400

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,10 @@
 import Foundation
 
 class Solution {
-    
+    func largestNumber(_ nums: [Int]) -> String {
+        var nums = nums.map { String($0) }
+        nums.sort { $0 + $1 > $1 + $0 }
+        let result = nums.reduce("") { $0 + $1 }
+        return result.first == "0" ? "0" : result
+    }
 }
