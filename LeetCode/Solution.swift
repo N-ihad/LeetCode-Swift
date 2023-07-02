eb93700a60d127f81b465e93715e9a3de39c1415
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-07-03 03:23:36 +0400

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,34 @@
 import Foundation
 
 class Solution {
-    
+    func searchRange(_ nums: [Int], _ target: Int) -> [Int] {
+        let left = search(nums, target, isLeftDirected: true)
+        let right = search(nums, target, isLeftDirected: false)
+        return [left, right]
+    }
+
+    private func search(_ nums: [Int], _ target: Int, isLeftDirected: Bool) -> Int {
+        var (l, r) = (0, nums.count-1)
+        var result = -1
+        
+        while l <= r {
+            let m = l + (r - l) / 2
+            
+            if nums[m] > target {
+                r = m - 1
+            } else if nums[m] < target {
+                l = m + 1
+            } else {
+                result = m
+
+                if isLeftDirected {
+                    r = m - 1
+                } else {
+                    l = m + 1
+                }
+            }
+        }
+
+        return result
+    }
 }
