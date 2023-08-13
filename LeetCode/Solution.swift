a1ced1969108bd43635453e5ec2d8698d9e91526
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-08-13 16:10:38 +0400

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,22 @@
 import Foundation
 
 class Solution {
-    
+    func sortedArrayToBST(_ nums: [Int]) -> TreeNode? {
+        let root = sortedArrayToBST(nums, 0, nums.count - 1)
+        return root
+    }
+
+    private func sortedArrayToBST(_ nums: [Int], _ L: Int, _ R: Int) -> TreeNode? {
+        if L > R {
+            return nil
+        }
+
+        let mid = (L + R) / 2
+        
+        let node = TreeNode(nums[mid])
+        node.left = sortedArrayToBST(nums, L, mid - 1)
+        node.right = sortedArrayToBST(nums, mid + 1, R)
+
+        return node
+    }
 }
