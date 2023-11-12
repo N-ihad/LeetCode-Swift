e40b5dafa10e2f1bee0c2ec42cf40920299436a4
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-11-13 01:21:10 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,17 @@
 import Foundation
 
 class Solution {
-    
+    func coinChange(_ coins: [Int], _ amount: Int) -> Int {
+        var dp = Array(repeating: amount + 1, count: amount + 1)
+        dp[0] = 0
+
+        for i in 1..<dp.count {
+            for j in 0..<coins.count where i - coins[j] >= 0 {
+                let leftover = i - coins[j]
+                dp[i] = min(dp[i], 1 + dp[leftover])
+            }
+        }
+
+        return dp[amount] == amount + 1 ? -1 : dp[amount]
+    }
 }
