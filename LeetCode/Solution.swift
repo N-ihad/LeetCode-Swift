97a06edc01945e6aa60322d0606bbe634ac8bbf4
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-11-13 15:16:32 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,23 @@
 import Foundation
 
 class Solution {
-    
+    func numSquares(_ n: Int) -> Int {
+        var dp = Array(repeating: n + 1, count: n + 1)
+        dp[0] = 0
+        dp[1] = 1
+
+        for i in 1..<n+1 {
+            for j in 1..<i {
+                let square = j * j
+
+                if square > i {
+                    break
+                }
+
+                dp[i] = min(dp[i], 1 + dp[i - square])
+            }
+        }
+
+        return dp[n]
+    }
 }
