f005f42920e5bc182b3862842d63bed20e578e03
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-11-13 12:54:36 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,26 @@
 import Foundation
 
 class Solution {
-    
+    func eliminateMaximum(_ dist: [Int], _ speed: [Int]) -> Int {
+        var velocities: [Double] = []
+        velocities.reserveCapacity(speed.count)
+
+        for i in 0..<speed.count {
+            velocities.append(Double(dist[i]) / Double(speed[i]))
+        }
+
+        velocities.sort()
+
+        var maxNumberOfMonstersCanBiKilled = 1
+        
+        for i in 1..<dist.count {
+            if velocities[i] - Double(i) <= 0 {
+                break
+            }
+
+            maxNumberOfMonstersCanBiKilled += 1
+        }
+
+        return maxNumberOfMonstersCanBiKilled
+    }
 }
