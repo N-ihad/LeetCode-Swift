aeb7c6e15fc5a47ef4fd028b9f290a935ed163a1
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-11-19 14:45:55 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -10,9 +10,7 @@ import Foundation
 class Solution {
     func rotate(_ nums: inout [Int], _ k: Int) {
         let k = k % nums.count
-
-        if k == 0 { return }
-
+        
         for i in 0..<nums.count/2 {
             nums.swapAt(i, nums.count-1-i)
         }
@@ -20,9 +18,9 @@ class Solution {
         for i in 0..<k/2 {
             nums.swapAt(i, k-1-i)
         }
-
-        for i in k..<(nums.count+k)/2 {
-            nums.swapAt(i, nums.count-1+k-i)
+        
+        for i in 0..<(nums.count-k)/2 {
+            nums.swapAt(k+i, nums.count-1-i)
         }
     }
 }
