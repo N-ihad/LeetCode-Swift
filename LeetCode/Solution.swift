35b3d7f3076ba7fe0710a5d50484d262e91976c7
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-11-19 16:26:40 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,33 @@
 
 import Foundation
 
+/**
+ * Definition for a Node.
+ * public class Node {
+ *     public var val: Int
+ *     public var children: [Node]
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.children = []
+ *     }
+ * }
+ */
+
 class Solution {
-    
+    func preorder(_ root: Node?) -> [Int] {
+        var stack = root == nil ? [] : [root!]
+        var result: [Int] = []
+
+        while !stack.isEmpty {
+            let node = stack.removeLast()
+            result.append(node.val)
+
+            let n = node.children.count
+            for i in 0..<n {
+                stack.append(node.children[n-1-i])
+            }
+        }
+
+        return result
+    }
 }
