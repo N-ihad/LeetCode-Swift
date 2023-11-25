65b2ef875e1e90ff7ef3df494ebab5e5e097d5a1
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-11-26 02:51:25 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,16 @@
 import Foundation
 
 class Solution {
-    
+    func minimumTotal(_ triangle: [[Int]]) -> Int {
+        let n = triangle.count
+        var dp = triangle[n-1]
+
+        for row in 1..<n {
+            for col in 0..<triangle[n-1-row].count {
+                dp[col] = min(triangle[n-1-row][col] + dp[col], triangle[n-1-row][col] + dp[col+1])
+            }
+        }
+
+        return dp[0]
+    }
 }
