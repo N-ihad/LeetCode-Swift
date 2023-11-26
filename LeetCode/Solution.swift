1a328c5aaa8c8f4d7f0e4d4b717d5b7e0572c71c
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-11-26 03:12:17 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -21,3 +21,35 @@ class Solution {
         return dp[0]
     }
 }
+
+/*
+// MARK: - DFS + Memoization
+class Solution {
+    func minimumTotal(_ triangle: [[Int]]) -> Int {
+        if triangle.count == 1 {
+            return triangle[0][0]
+        }
+
+        var cache: [String: Int] = [:]
+        return min(triangle[0][0] + dfs(1, 0, triangle, &cache), triangle[0][0] + dfs(1, 1, triangle, &cache))
+    }
+
+    private func dfs(_ i: Int, _ j: Int, _ triangle: [[Int]], _ cache: inout [String: Int]) -> Int {
+        if i == triangle.count - 1 {
+            return triangle[i][j]
+        }
+
+        if cache["\(i),\(j)"] != nil {
+            return cache["\(i),\(j)"]!
+        }
+
+        let left = triangle[i][j] + dfs(i + 1, j, triangle, &cache)
+        let right = triangle[i][j] + dfs(i + 1, j + 1, triangle, &cache)
+
+        let minPath = min(left, right)
+        cache["\(i),\(j)"] = minPath
+
+        return minPath
+    }
+}
+*/
