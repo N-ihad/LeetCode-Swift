a936ab6ddd4dbfb476befe514eb162590db46a6f
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-12-04 23:09:35 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,58 @@
 import Foundation
 
 class Solution {
-    
+    func arrayStringsAreEqual(_ word1: [String], _ word2: [String]) -> Bool {
+        var (i, j) = (0, word2[0].startIndex) // word2 current array and char indices correspondingly
+
+        for word in word1 {
+            for char in word {
+                
+                if i >= word2.count || char != word2[i][j] {
+                    return false
+                }
+
+                if j == word2[i].index(before: word2[i].endIndex) {
+                    i += 1
+                    
+                    if i < word2.count {
+                        j = word2[i].startIndex
+                    }
+                } else {
+                    j = word2[i].index(after: j)
+                }
+            }
+        }
+
+        return i >= word2.count
+    }
 }
+
+/*
+// MARK: - Solution using normal indices like in literally any normal other programming language
+class Solution {
+    func arrayStringsAreEqual(_ word1: [String], _ word2: [String]) -> Bool {
+        let word1: [[Character]] = word1.map { Array($0) }
+        let word2: [[Character]] = word2.map { Array($0) }
+
+        var (i, j) = (0, 0) // word2 current array and char indices correspondingly
+
+        for word in word1 {
+            for char in word {
+
+                if i >= word2.count || char != word2[i][j] {
+                    return false
+                }
+
+                if j == word2[i].count - 1 {
+                    i += 1
+                    j = 0
+                } else {
+                    j += 1
+                }
+            }
+        }
+
+        return i >= word2.count
+    }
+}
+*/
