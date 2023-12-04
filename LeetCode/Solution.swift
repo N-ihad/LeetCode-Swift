a97eabad22d8c24be16e070ceadd85ebd3957c42
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-12-05 01:12:41 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,31 @@
 import Foundation
 
 class Solution {
-    
+    func largestGoodInteger(_ num: String) -> String {
+        var result: String = ""
+        var temp: String = ""
+
+        for char in num {
+            if temp.count == 3 {
+
+                if result.isEmpty || Int(temp)! > Int(result)! {
+                    result = temp
+                }
+
+                temp = ""
+            }
+
+            if char == temp.first {
+                temp += String(char)
+            } else {
+                temp = String(char)
+            }
+        }
+
+        if temp.count == 3 && (result.isEmpty || Int(temp)! > Int(result)!) {
+            result = temp
+        }
+
+        return result
+    }
 }
