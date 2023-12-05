2a2a821d5eb341fd4df36c359d04c946b75f205b
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-12-06 02:35:18 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,24 @@
 import Foundation
 
 class Solution {
-    
+    func numberOfMatches(_ n: Int) -> Int {
+        n-1
+    }
 }
+
+/*
+// MARK: - Iterative solution via simulation
+class Solution {
+    func numberOfMatches(_ n: Int) -> Int {
+        var matches = 0
+        var teams = n
+        
+        while teams != 1 {
+            matches += teams / 2
+            teams = Int(ceil(Double(teams) / 2))
+        }
+        
+        return matches
+    }
+}
+*/
