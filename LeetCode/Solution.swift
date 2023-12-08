165c7fbd775f26db117bfb09f526deb2f637e885
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-12-08 13:40:25 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -10,19 +10,13 @@ import Foundation
 class Solution {
     func maxProfit(_ prices: [Int]) -> Int {
         var profit = 0
-        var low = prices[0]
-        var potentialProfit = 0
 
         for i in 0..<prices.count-1 {
-            if prices[i] < prices[i+1] { // upward trend, bullish
-                potentialProfit = prices[i+1] - low
-            } else if prices[i] > prices[i+1] { // downward trend, bearish
-                profit += potentialProfit
-                low = prices[i+1]
-                potentialProfit = 0
+            if prices[i] < prices[i+1] { // upward trend
+                profit += prices[i+1] - prices[i]
             }
         }
 
-        return profit + potentialProfit
+        return profit
     }
 }
