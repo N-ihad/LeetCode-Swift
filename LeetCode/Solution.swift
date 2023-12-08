9a62be24bd689c7a5ddbc08dd932477604dab239
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-12-08 13:35:00 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,31 @@
 import Foundation
 
 class Solution {
-    
+    func tree2str(_ root: TreeNode?) -> String {
+        var result = "\(root!.val)"
+        dfs(root, &result)
+        return result
+    }
+
+    private func dfs(_ node: TreeNode?, _ result: inout String) {
+        if node == nil || (node!.left == nil && node!.right == nil) {
+            return
+        }
+
+        if node!.left != nil && node!.right != nil {
+            result += "(" + String(node!.left!.val)
+            dfs(node!.left, &result)
+            result += ")(" + String(node!.right!.val)
+            dfs(node!.right, &result)
+            result += ")"
+        } else if node!.left == nil && node!.right != nil {
+            result += "()(" + String(node!.right!.val)
+            dfs(node!.right, &result)
+            result += ")"
+        } else if node!.left != nil && node!.right == nil {
+            result += "(" + String(node!.left!.val)
+            dfs(node!.left, &result)
+            result += ")"
+        }
+    }
 }
