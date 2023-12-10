bf8a7886a58a0172a13ed9fa137f954a3c49ff9a
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-12-10 21:42:01 +0200

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,13 @@
 import Foundation
 
 class Solution {
-    
+    func transpose(_ matrix: [[Int]]) -> [[Int]] {
+        var result = Array(repeating: [Int](), count: matrix[0].count)
+        for i in 0..<matrix.count {
+            for j in 0..<matrix[i].count {
+                result[j].append(matrix[i][j])
+            }
+        }
+        return result
+    }
 }
