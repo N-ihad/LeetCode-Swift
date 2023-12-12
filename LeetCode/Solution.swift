798bef2d164031bf55343641dc0a2d86cc3ba890
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-12-12 14:07:59 +0200

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,11 +8,11 @@
 import Foundation
 
 /*
-Iterate through an array and increment sum by 111, check if we are out of bounds of an array for next 
-element i+1 OR next element is not equal to the current element arr[i] ≠ arr[i+1], these 2 conditions mean 
-following: either we are on the last element and we should compute the result or we reached the end of the repeating element. 
-So, we check if our so far sum divided by arr.count is greater than 0.25 (25%), 
-if so then we found the result, if not we are nullifying sum and going to the next iteration (if there's).
+    Iterate through an array and increment sum by 111, check if we are out of bounds of an array for next 
+    element i+1 OR next element is not equal to the current element arr[i] ≠ arr[i+1], these 2 conditions mean 
+    following: either we are on the last element and we should compute the result or we reached the end of the repeating element. 
+    So, we check if our so far sum divided by arr.count is greater than 0.25 (25%), 
+    if so then we found the result, if not we are nullifying sum and going to the next iteration (if there's).
 */
 
 class Solution {
