4962a443c1cc19da60577dfbb043632af65978ef
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-12-16 14:24:02 +0200

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,26 +7,34 @@
 
 import Foundation
 
+/*
+    https://leetcode.com/problems/valid-anagram/solutions/4412466/swift-solution-with-explanation/
+
+    Iterate through the first word s counting letter occurrences in our hash table letterCounts, 
+    then iterate through the second word t: if we see a missing character from
+    our table (meaning there's a character that was not present in the first word s) letterCounrs[c] == nil or 
+    we run out of character occurrence letterCounts[c] == 0 we return false,
+    otherwise we decrease occurence of character by 1 letterCounts[c] -= 1 and proceed further.
+*/
+
 class Solution {
     func isAnagram(_ s: String, _ t: String) -> Bool {
-        if s.count != t.count { return false }
+        if s.count != t.count {
+            return false
+        }
 
-        var hashTable: [Character: Int] = [:]
+        var letterCounts: [Character: Int] = [:]
+
         for c in s {
-            if hashTable[c] != nil {
-                hashTable[c]! += 1
-            } else {
-                hashTable[c] = 1
-            }
+            letterCounts[c, default: 0] += 1
         }
 
         for c in t {
-            if hashTable[c] == nil {
+            if letterCounts[c] == nil || letterCounts[c] == 0 {
                 return false
-            } else {
-                if hashTable[c]! == 0 { return false }
-                hashTable[c]! -= 1
             }
+
+            letterCounts[c]! -= 1
         }
 
         return true
