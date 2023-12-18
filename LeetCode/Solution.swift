14e7a225bf0aedff9baf812678cd6df412440e75
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-12-18 12:45:43 +0200

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,26 @@
 import Foundation
 
 class Solution {
-    
+    func maxProductDifference(_ nums: [Int]) -> Int {
+        var (min1, min2, max1, max2) = (Int.max, Int.max, Int.min, Int.min)
+
+        for i in 0..<nums.count {
+            if nums[i] < min1 {
+                min2 = min1
+                min1 = nums[i]
+            } else if nums[i] < min2 {
+                min2 = nums[i]
+            }
+
+            if nums[i] > max1 {
+                max2 = max1
+                max1 = nums[i]
+            } else if nums[i] > max2 {
+                max2 = nums[i]
+            }
+        }
+
+        return max1 * max2 - min1 * min2
+    }
 }
+
