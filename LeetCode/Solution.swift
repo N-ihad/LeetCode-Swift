8dea595ca42691ab4095b67d38565f689b9a4f74
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-12-18 17:10:42 +0200

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,5 +8,15 @@
 import Foundation
 
 class Solution {
-    
+    func getRow(_ rowIndex: Int) -> [Int] {
+        var result: [Int] = []
+        var binomialCoefficient = 1
+
+        for i in 0...rowIndex {
+            result.append(binomialCoefficient)
+            binomialCoefficient = binomialCoefficient * (rowIndex - i) / (i + 1)
+        }
+
+        return result
+    }
 }
