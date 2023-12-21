d564ae7535af1f26d5b873138c99a82864c1b4e7
Nihad <43179082+N-ihad@users.noreply.github.com>
2023-12-21 17:58:51 +0200

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,23 @@
 
 import Foundation
 
+/*
+    https://leetcode.com/problems/widest-vertical-area-between-two-points-containing-no-points/solutions/4436668/swift-solution-with-explanation/
+
+    Sort points array by x (y part is irrelevant to us) in non-decreasing order.
+    Then just compute the difference by x-axis between 2 neighboring points and
+    update widestArea variable which keeps track of the max difference: widestArea = max(widestArea, points[i+1][0] - points[i][0]),
+    it is ensured that between those points there are no other points because we sorted points array, 
+    all elements there are closest to each other.
+*/
+
 class Solution {
-    
+    func maxWidthOfVerticalArea(_ points: [[Int]]) -> Int {
+        let points = points.sorted { $0[0] < $1[0] }
+        var widestArea = 0
+        for i in 0..<points.count-1 {
+            widestArea = max(widestArea, points[i+1][0] - points[i][0])
+        }
+        return widestArea
+    }
 }
