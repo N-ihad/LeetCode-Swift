cb22489467912ddd07dcc5f821b6000bd7a162e7
Nihad <43179082+N-ihad@users.noreply.github.com>
2024-01-19 00:33:15 +0200

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,28 +7,39 @@
 
 import Foundation
 
-class Solution {
-    // MARK: - Recursive with memoization
-    func climbStairs(_ n: Int) -> Int {
-        var cache = Array<Int?>(repeating: nil, count: n+2)
-        func climb(_ nn: Int) -> Int {
-            if let val = cache[nn] { return val }
-            if nn > n { return 0 }
-            if nn == n { return 1 }
-            cache[nn+1] = climb(nn + 1)
-            cache[nn+2] = climb(nn + 2)
-            return cache[nn+1]! + cache[nn+2]!
-        }
-        return climb(0)
-    }
+/*
+    https://leetcode.com/problems/climbing-stairs/solutions/4589680/swift-solution-dynamic-programming-bottom-up-approach/
 
-    // MARK: - Recursive without memoization
-//    func climbStairs(_ n: Int) -> Int {
-//        func climb(_ nn: Int) -> Int {
-//            if nn > n { return 0 }
-//            if nn == n { return 1 }
-//            return climb(nn + 1) + climb(nn + 2)
-//        }
-//        return climb(0)
-//    }
+    Using dynamic programming bottom up approach we can calculate the number of distinct ways to climb to the n
+    
+    n = 1 => 1
+    n = 2 => 2
+    n = 3 => 3
+    n = 4 => 5
+    n = 5 => 8
+    
+    We can clearly see that the next element is formed by adding previous 2:
+    F(x) = F(x-1) + F(x-2)
+    
+    Our base is:
+    F(0) = 1
+    F(1) = 1
+*/
+
+class Solution {
+    func climbStairs(_ n: Int) -> Int {
+        if n == 1 {
+            return 1
+        }
+
+        var dp = Array(repeating: 0, count: n + 1)
+        dp[0] = 1
+        dp[1] = 1
+
+        for i in 2...n {
+            dp[i] = dp[i-1] + dp[i-2]
+        }
+
+        return dp[n]
+    }
 }
