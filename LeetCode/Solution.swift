6f6f35aa619892f4a8c87cb2eaa9d53a4d684fcc
Nihad <43179082+N-ihad@users.noreply.github.com>
2024-03-28 22:20:23 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -8,12 +8,11 @@
 import Foundation
 
 /*
-    https://leetcode.com/problems/length-of-longest-subarray-with-at-most-k-frequency/solutions/4939205/swift-solution-sliding-window-hash-table-counting-with-explanation/
+    https://leetcode.com/problems/length-of-longest-subarray-with-at-most-k-frequency/solutions/4939225/swift-solution-sliding-window-hash-table-counting-with-explanation/
 
     We initialize our sliding window which is represented with left edge `l` and right edge `r`. 
-    Iterating through an array and counting frequency of each `num`, 
-    if frequency is more than allowed `k` we start moving our left edge of 
-    sliding window `l` by 1 each iteration (we shouldn't also be greater than right edge `r`). 
+    Iterating through an array and counting frequency of each `num`, if frequency is more than
+    allowed `k` we start moving our left edge of sliding window `l` by 1 each iteration. 
     Otherwise we just calculate our maximum `result = max(result, r - l + 1)`.
 */
 
@@ -22,16 +21,15 @@ class Solution {
         var freqs: [Int: Int] = [:]
         var (l, r, result) = (0, 0, 0)
 
-        while r < nums.count {
+        for r in 0..<nums.count {
             freqs[nums[r], default: 0] += 1
 
-            while freqs[nums[r]]! > k && l <= r {
+            while freqs[nums[r]]! > k {
                 freqs[nums[l]]! -= 1
                 l += 1
             }
 
             result = max(result, r - l + 1)
-            r += 1
         }
 
         return result
