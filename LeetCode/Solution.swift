f07334366109a1826d4d0474c1daa5656f8f01f3
Nihad <43179082+N-ihad@users.noreply.github.com>
2024-03-28 22:15:24 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,33 @@
 
 import Foundation
 
+/*
+    https://leetcode.com/problems/length-of-longest-subarray-with-at-most-k-frequency/solutions/4939205/swift-solution-sliding-window-hash-table-counting-with-explanation/
+
+    We initialize our sliding window which is represented with left edge `l` and right edge `r`. 
+    Iterating through an array and counting frequency of each `num`, 
+    if frequency is more than allowed `k` we start moving our left edge of 
+    sliding window `l` by 1 each iteration (we shouldn't also be greater than right edge `r`). 
+    Otherwise we just calculate our maximum `result = max(result, r - l + 1)`.
+*/
+
 class Solution {
-    
+    func maxSubarrayLength(_ nums: [Int], _ k: Int) -> Int {
+        var freqs: [Int: Int] = [:]
+        var (l, r, result) = (0, 0, 0)
+
+        while r < nums.count {
+            freqs[nums[r], default: 0] += 1
+
+            while freqs[nums[r]]! > k && l <= r {
+                freqs[nums[l]]! -= 1
+                l += 1
+            }
+
+            result = max(result, r - l + 1)
+            r += 1
+        }
+
+        return result
+    }
 }
