74ac758e2730f300612b2ec930a8c16d92a36c80
Nihad <43179082+N-ihad@users.noreply.github.com>
2024-10-10 12:15:15 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,31 @@
 
 import Foundation
 
+/*
+    https://leetcode.com/problems/search-insert-position/solutions/5895056/swift-solution/
+
+    Just a simple binary search
+*/
+
 class Solution {
-    
+    func searchInsert(_ nums: [Int], _ target: Int) -> Int {
+        var a = 0
+        var b = nums.count - 1
+
+        while a <= b {
+            let mid = (a + b) / 2
+
+            if nums[mid] == target {
+                return mid
+            }
+
+            if target > nums[mid] {
+                a = mid + 1
+            } else {
+                b = mid - 1
+            }
+        }
+
+        return a
+    }
 }
