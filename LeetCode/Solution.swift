afcd40dd969c8c202fe321607ddff75b7376082f
Nihad <43179082+N-ihad@users.noreply.github.com>
2025-09-14 01:38:59 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,24 @@
 
 import Foundation
 
+/*
+
+
+*/
+
 class Solution {
-    
+    func maxFreqSum(_ s: String) -> Int {
+        let vowels: Set<Character> = ["a", "e", "i", "o", "u"]
+        var letterFrequency: [Character: Int] = [:]
+        var (maxVowelsFrequencyCount, maxConsonantsFrequencyCount) = (0, 0)
+        for c in s {
+            letterFrequency[c, default: 0] += 1
+            if vowels.contains(c) {
+                maxVowelsFrequencyCount = max(letterFrequency[c]!, maxVowelsFrequencyCount)
+            } else {
+                maxConsonantsFrequencyCount = max(letterFrequency[c]!, maxConsonantsFrequencyCount)
+            }
+        }
+        return maxVowelsFrequencyCount + maxConsonantsFrequencyCount
+    }
 }
