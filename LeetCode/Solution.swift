37b363d6df5ea9233c0a4d519d3d825c067fe07b
Nihad <43179082+N-ihad@users.noreply.github.com>
2026-01-04 05:13:06 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,42 @@
 
 import Foundation
 
+/*
+    https://leetcode.com/problems/four-divisors/solutions/7463721/swift-solution-with-explanation-by-nihad-a0z7
+
+    In our solution, we iterate through each number in the array and explicitly count its divisors while also summing them. For every number, we 
+    try all possible divisor candidates from 1 up to the number itself, and whenever a candidate divides the number evenly, we 
+    increment a divisor counter and add it to the running sum. To keep the logic efficient and intuitive, we 
+    immediately stop checking further if the divisor count exceeds four, since such a number can no longer qualify. 
+    After finishing the checks for a number, we only add its divisor sum to the final result if it has exactly four divisors.
+
+    Time complexity: O(nm), m – is the maximum number in nums array
+    Space complexity: O(1)
+*/
+
 class Solution {
-    
+    func sumFourDivisors(_ nums: [Int]) -> Int {
+        var result = 0
+        for num in nums {
+            var count = 0
+            var sum = 0
+            for divisorCandidate in 1...num {
+                if num % divisorCandidate == 0 {
+                    sum += divisorCandidate
+                    count += 1
+                }
+
+                if count > 4 {
+                    sum = 0
+                    break
+                }
+            }
+
+            if count == 4 {
+                result += sum
+            }
+        }
+
+        return result
+    }
 }
