cfebcf52c109113ceb4a891cef987d4d233e76a4
Nihad <43179082+N-ihad@users.noreply.github.com>
2026-02-08 22:23:23 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,14 +7,22 @@
 
 import Foundation
 
+/*
+    Top-down O(n^2) approach:
+    https://leetcode.com/problems/balanced-binary-tree/solutions/7564177/swift-solution-top-down-approach-with-ex-bnsa
+
+    Bottom-up O(n) approach:
+    https://leetcode.com/problems/balanced-binary-tree/solutions/7564188/swift-solution-on-bottom-up-approach-wit-qlvs
+*/
+
 class Solution {
     func isBalanced(_ root: TreeNode?) -> Bool {
         if root == nil { return true }
         return abs(height(root!.left) - height(root!.right)) <= 1 && isBalanced(root!.left) && isBalanced(root!.right)
     }
 
-    private func height(_ root: TreeNode?) -> Int {
-        if root == nil { return 0 }
-        return 1 + max(height(root!.left), height(root!.right))
+    private func height(_ node: TreeNode?) -> Int {
+        if node == nil { return 0 }
+        return 1 + max(height(node!.left), height(node!.right))
     }
 }
