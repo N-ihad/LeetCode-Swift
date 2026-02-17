656e48ac8646ca3b9c1292de3c5cbf40726737dd
Nihad <43179082+N-ihad@users.noreply.github.com>
2026-02-17 20:46:04 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,24 @@
 
 import Foundation
 
+/*
+    https://leetcode.com/problems/binary-watch/solutions/7587050/swift-solution-constant-o1-time-with-exp-xk41
+*/
+
 class Solution {
-    
+    func readBinaryWatch(_ turnedOn: Int) -> [String] {
+        var result: [String] = []
+
+        for h in 0...11 {
+            for m in 0...59 {
+                let total = h.nonzeroBitCount + m.nonzeroBitCount
+                if total == turnedOn {
+                    let m = m < 10 ? "0\(m)" : "\(m)"
+                    result.append("\(h):\(m)")
+                }
+            }
+        }
+
+        return result
+    }
 }
