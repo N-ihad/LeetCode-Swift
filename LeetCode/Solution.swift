07957c0368957d25ffa8164a0f8d4825c69872fe
Nihad <43179082+N-ihad@users.noreply.github.com>
2026-02-20 03:29:13 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,30 @@
 
 import Foundation
 
+/*
+    https://leetcode.com/problems/count-binary-substrings/solutions/7593025/swift-solution-with-explanation-by-nihad-pplm
+*/
+
 class Solution {
-    
+    func countBinarySubstrings(_ s: String) -> Int {
+        var s = Array(s)
+        var groups: [Int] = []
+        var currentLength = 1
+        for i in 1..<s.count {
+            if s[i] == s[i-1] {
+                currentLength += 1
+            } else {
+                groups.append(currentLength)
+                currentLength = 1
+            }
+        }
+        groups.append(currentLength)
+
+        var result = 0
+        for i in 0..<groups.count - 1 {
+            result += min(groups[i], groups[i+1])
+        }
+
+        return result
+    }
 }
