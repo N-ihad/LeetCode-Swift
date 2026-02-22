82ab3184bf133d97e26adb02a9d36a4c65a976de
Nihad <43179082+N-ihad@users.noreply.github.com>
2026-02-22 19:55:35 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,25 @@
 
 import Foundation
 
+/*
+    https://leetcode.com/problems/binary-gap/solutions/7600426/swift-solution-with-explanation-by-nihad-1ttk
+*/
+
 class Solution {
-    
+    func binaryGap(_ n: Int) -> Int {
+        let binaryString = Array(String(n, radix: 2))
+        var result = 0
+        var startIndex: Int?
+        for i in 0..<binaryString.count {
+            if binaryString[i] == "1" {
+                if startIndex == nil {
+                    startIndex = i
+                } else {
+                    result = max(result, i - startIndex!)
+                    startIndex = i
+                }
+            }
+        }
+        return result
+    }
 }
