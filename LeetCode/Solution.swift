5cb9e1ee0289d44a0897fae47507178819935d29
Nihad <43179082+N-ihad@users.noreply.github.com>
2026-02-23 19:55:28 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,34 @@
 
 import Foundation
 
+/*
+    https://leetcode.com/problems/champagne-tower/solutions/7603165/swift-solution-with-explanation-by-nihad-fcnd
+*/
+
 class Solution {
-    
+    func champagneTower(_ poured: Int, _ query_row: Int, _ query_glass: Int) -> Double {
+        if query_row == 0 {
+            return min(1.0, Double(poured))
+        }
+
+        var prev = [Double(poured)]
+
+        for row in 1...query_row {
+            var current = Array(repeating: 0.0, count: row + 1)
+
+            for j in 0...row {
+                // From left parent
+                if j - 1 >= 0 {
+                    current[j] += max(0.0, prev[j - 1] - 1.0) / 2.0
+                }
+                // From right parent
+                if j <= prev.count - 1 {
+                    current[j] += max(0.0, prev[j] - 1.0) / 2.0
+                }
+            }
+            prev = current
+        }
+
+        return min(1.0, prev[query_glass])
+    }
 }
