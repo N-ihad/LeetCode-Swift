b996be78d3c622382c2a7cf81f9221fced83b94d
Nihad <43179082+N-ihad@users.noreply.github.com>
2026-02-23 05:00:13 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,29 @@
 
 import Foundation
 
+/*
+    https://leetcode.com/problems/check-if-a-string-contains-all-binary-codes-of-size-k/solutions/7601177/swift-solution-with-explanation-by-nihad-a2ca
+*/
+
 class Solution {
-    
+    func hasAllCodes(_ s: String, _ k: Int) -> Bool {
+        let s = Array(s)
+
+        if s.count < k { 
+            return false
+        }
+
+        let target = 1 << k
+        var seen: Set<String> = []
+        for i in 0...s.count-k {
+            let substr = String(s[i..<i+k])
+            seen.insert(substr)
+
+            if seen.count == target {
+                return true
+            }
+        }
+
+        return seen.count == target
+    }
 }
