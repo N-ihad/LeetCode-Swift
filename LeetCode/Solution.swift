3c5e439b4869f4251015950b4235cf68ebc74e56
Nihad <43179082+N-ihad@users.noreply.github.com>
2026-02-24 23:18:50 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,26 @@
 
 import Foundation
 
+/*
+    https://leetcode.com/problems/sum-of-root-to-leaf-binary-numbers/solutions/7606495/swift-solution-using-left-bit-shift-with-funz
+*/
+
 class Solution {
-    
+    func sumRootToLeaf(_ root: TreeNode?) -> Int {
+        return dfs(root, 0)
+    }
+
+    private func dfs(_ node: TreeNode?, _ current: Int) -> Int {
+        guard let node else {
+            return 0
+        }
+
+        let value = (current << 1) | node.val
+
+        if node.left == nil && node.right == nil {
+            return value
+        }
+
+        return dfs(node.left, value) + dfs(node.right, value)
+    }
 }
