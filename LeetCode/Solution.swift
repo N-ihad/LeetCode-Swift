b1e52ea13f44600ad3de23a2b15f477b3d670b41
Nihad <43179082+N-ihad@users.noreply.github.com>
2026-02-26 16:10:43 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,18 @@
 
 import Foundation
 
+/*
+    https://leetcode.com/problems/sort-integers-by-the-number-of-1-bits/solutions/7610906/swift-solution-with-explanation-by-nihad-1zr2
+*/
+
 class Solution {
-    
+    func sortByBits(_ arr: [Int]) -> [Int] {
+        let arr = arr.sorted {
+            if $0.nonzeroBitCount == $1.nonzeroBitCount {
+                return $0 < $1
+            }
+            return $0.nonzeroBitCount < $1.nonzeroBitCount
+        }
+        return arr
+    }
 }
