627eb2a57564f78863097cd478b42ac904130ed7
Nihad <43179082+N-ihad@users.noreply.github.com>
2026-03-06 18:38:51 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,18 @@
 
 import Foundation
 
+/*
+    https://leetcode.com/problems/check-if-binary-string-has-at-most-one-segment-of-ones/solutions/7630341/swift-solution-with-explanation-by-nihad-dag8
+*/
+
 class Solution {
-    
+    func checkOnesSegment(_ s: String) -> Bool {
+        let s = Array(s)
+        for i in 1..<s.count {
+            if s[i-1] == "0" && s[i] == "1" {
+                return false
+            }
+        }
+        return true
+    }
 }
