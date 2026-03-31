5a942fc9aab696254ed3846f463713bb7723570f
Nihad <43179082+N-ihad@users.noreply.github.com>
2026-04-01 02:18:53 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,28 @@
 
 import Foundation
 
+/*
+    https://leetcode.com/problems/check-if-strings-can-be-made-equal-with-operations-ii/solutions/7734527/swift-solution-optimal-with-explanation-bplid
+*/
+
 class Solution {
-    
+    func checkStrings(_ s1: String, _ s2: String) -> Bool {
+        let s1 = Array(s1)
+        let s2 = Array(s2)
+
+        var count = Array(repeating: Array(repeating: 0, count: 26), count: 2)
+        for i in 0..<s1.count {
+            let parity = i % 2
+            count[parity][Int(s1[i].asciiValue! - 97)] += 1
+            count[parity][Int(s2[i].asciiValue! - 97)] -= 1
+        }
+
+        for i in 0..<26 {
+            if count[0][i] != 0 || count[1][i] != 0 {
+                return false
+            }
+        }
+
+        return true
+    }
 }
