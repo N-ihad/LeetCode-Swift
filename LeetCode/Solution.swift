f4d7f19854c0060560c9cffb762b368c24d63297
Nihad <43179082+N-ihad@users.noreply.github.com>
2026-04-05 03:25:14 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,22 @@
 
 import Foundation
 
+/*
+    https://leetcode.com/problems/robot-return-to-origin/solutions/7782076/swift-solution-with-explanation-by-nihad-iks2
+*/
+
 class Solution {
-    
+    func judgeCircle(_ moves: String) -> Bool {
+        var (x, y) = (0, 0)
+        for move in moves {
+            switch move {
+            case "U": y += 1
+            case "D": y -= 1
+            case "L": x -= 1
+            case "R": x += 1
+            default: break
+            }
+        }
+        return x == 0 && y == 0
+    }
 }
