ac8a9957f416878a8c195996c7af19c0a9f36f0f
Nihad <43179082+N-ihad@users.noreply.github.com>
2026-04-08 18:56:09 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,20 @@
 
 import Foundation
 
+/*
+    https://leetcode.com/problems/xor-after-range-multiplication-queries-i/solutions/7830631/swift-solution-with-explanation-by-nihad-dk5b
+*/
+
 class Solution {
-    
+    func xorAfterQueries(_ nums: [Int], _ queries: [[Int]]) -> Int {
+        var nums = nums
+        for query in queries {
+            var idx = query[0]
+            while idx <= query[1] {
+                nums[idx] = (nums[idx] * query[3]) % (1_000_000_000 + 7)
+                idx += query[2]
+            }
+        }
+        return nums.reduce(0, ^)
+    }
 }
