7eac0e40f64145883e738a4f94a45ed140d7c563
Nihad <43179082+N-ihad@users.noreply.github.com>
2026-04-10 20:50:22 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,25 @@
 
 import Foundation
 
+/*
+    https://leetcode.com/problems/minimum-distance-between-three-equal-elements-i/solutions/7861663/swift-solution-with-explanation-by-nihad-c9sw
+*/
+
 class Solution {
-    
+    func minimumDistance(_ nums: [Int]) -> Int {
+        var freq: [Int: [Int]] = [:]
+        for i in 0..<nums.count {
+            freq[nums[i], default: []].append(i)
+        }
+
+        var result: Int?
+        for indices in freq.values where indices.count >= 3 {
+            for i in 1..<indices.count-1 {
+                let dist = (indices[i+1] - indices[i-1]) * 2
+                result = min(result ?? 200, dist)
+            }
+        }
+
+        return result ?? -1
+    }
 }
