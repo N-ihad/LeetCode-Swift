4ed53e8834b75becfb230ec0579193a6bcfcbc8b
Nihad <43179082+N-ihad@users.noreply.github.com>
2026-04-13 23:41:39 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,18 @@
 
 import Foundation
 
+/*
+    https://leetcode.com/problems/minimum-distance-to-the-target-element/solutions/7897422/swift-solution-with-explanation-by-nihad-qo42
+*/
+
 class Solution {
-    
+    func getMinDistance(_ nums: [Int], _ target: Int, _ start: Int) -> Int {
+        var result = 1000
+        for i in 0..<nums.count {
+            if nums[i] == target {
+                result = min(result, abs(i - start))
+            }
+        }
+        return result
+    }
 }
