fd42f5cc0198002650cf4e5d90fb9ef968bc5b9a
Nihad <43179082+N-ihad@users.noreply.github.com>
2026-04-15 13:13:33 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,21 @@
 
 import Foundation
 
+/*
+    https://leetcode.com/problems/shortest-distance-to-target-string-in-a-circular-array/solutions/7921062/swift-solution-with-explanation-by-nihad-0588
+*/
+
 class Solution {
-    
+    func closestTarget(_ words: [String], _ target: String, _ startIndex: Int) -> Int {
+        var (backwardIndex, forwardIndex, dist) = (startIndex, startIndex, 0)
+        repeat {
+            if words[backwardIndex] == target || words[forwardIndex] == target {
+                return dist
+            }
+            dist += 1
+            forwardIndex = (forwardIndex + 1) % words.count
+            backwardIndex = (backwardIndex - 1 + words.count) % words.count
+        } while backwardIndex != startIndex && forwardIndex != startIndex
+        return -1
+    }
 }
