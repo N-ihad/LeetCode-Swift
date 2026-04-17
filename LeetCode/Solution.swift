320bc37570045f609e2c4e39191fe5a65842a1dc
Nihad <43179082+N-ihad@users.noreply.github.com>
2026-04-17 23:31:07 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,25 @@
 
 import Foundation
 
+/*
+    https://leetcode.com/problems/minimum-absolute-distance-between-mirror-pairs/solutions/7970040/swift-solution-short-with-explanation-by-o9a2
+*/
+
 class Solution {
-    
+    func minMirrorPairDistance(_ nums: [Int]) -> Int {
+        var result = Int.max
+        var map: [Int: Int] = [:] // reversedNum -> lastSeenIndex
+
+        for j in 0..<nums.count {
+            let reversed = Int(String(String(nums[j]).reversed()))!
+
+            if let i = map[nums[j]] {
+                result = min(result, j - i)
+            }
+
+            map[reversed] = j
+        }
+
+        return result == Int.max ? -1 : result
+    }
 }
