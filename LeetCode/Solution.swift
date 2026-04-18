6b8d8b56120789f1e1dda2813e5a8212a8aa0478
Nihad <43179082+N-ihad@users.noreply.github.com>
2026-04-18 15:06:41 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,12 @@
 
 import Foundation
 
+/*
+    https://leetcode.com/problems/mirror-distance-of-an-integer/solutions/7980150/swift-solution-short-1-line-with-explana-bhel
+*/
+
 class Solution {
-    
+    func mirrorDistance(_ n: Int) -> Int {
+        abs(n - Int(String(String(n).reversed()))!)
+    }
 }
