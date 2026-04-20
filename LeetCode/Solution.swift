41e92e58a0f5648f013ab82f12a7287443403d72
Nihad <43179082+N-ihad@users.noreply.github.com>
2026-04-20 13:39:13 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,34 @@
 
 import Foundation
 
+/*
+    https://leetcode.com/problems/two-furthest-houses-with-different-colors/solutions/8007892/swift-solution-optimal-on-with-explanati-7tsb
+*/
+
 class Solution {
-    
+    func maxDistance(_ colors: [Int]) -> Int {
+        let n = colors.count
+
+        // If the first and last houses have different colors, the maximum distance is n-1
+        if colors[0] != colors[n - 1] {
+            return n - 1
+        }
+
+        var maxDist = 0
+        // Find the rightmost house that has a different color from the first house
+        for i in 1..<n {
+            if colors[i] != colors[0] {
+                maxDist = max(maxDist, i)
+            }
+        }
+
+        // Find the leftmost house (from the right side) that has a different color from the last house
+        for i in 0..<n-1 {
+            if colors[i] != colors[n - 1] {
+                maxDist = max(maxDist, n - 1 - i)
+            }
+        }
+
+        return maxDist
+    }
 }
