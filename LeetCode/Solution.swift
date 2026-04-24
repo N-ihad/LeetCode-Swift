3214c9c895918c53abf369c3c7bc3a28fc076ba1
Nihad <43179082+N-ihad@users.noreply.github.com>
2026-04-24 14:17:11 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,25 @@
 
 import Foundation
 
+/*
+    https://leetcode.com/problems/furthest-point-from-origin/solutions/8094380/swift-solution-with-explanation-by-nihad-eiej
+*/
+
 class Solution {
-    
+    func furthestDistanceFromOrigin(_ moves: String) -> Int {
+        var balance = 0
+        var wildcards = 0
+
+        for move in moves {
+            if move == "L" {
+                balance -= 1
+            } else if move == "R" {
+                balance += 1
+            } else {
+                wildcards += 1
+            }
+        }
+
+        return abs(balance) + wildcards
+    }
 }
