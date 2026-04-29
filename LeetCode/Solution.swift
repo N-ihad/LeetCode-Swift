4f66ab8dac1b632394b9390e5eb77dbe691ae888
Nihad <43179082+N-ihad@users.noreply.github.com>
2026-04-29 23:20:26 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,25 @@
 
 import Foundation
 
+/*
+    https://leetcode.com/problems/minimum-operations-to-make-a-uni-value-grid/solutions/8114799/swift-solution-with-explanation-by-nihad-5vce
+*/
+
 class Solution {
-    
+    func minOperations(_ grid: [[Int]], _ x: Int) -> Int {
+        let nums = grid.flatMap { $0 }.sorted()
+        let median = nums[nums.count / 2]
+        let commonRemainder = median % x
+
+        var result = 0
+
+        for num in nums {
+            if num % x != commonRemainder {
+                return -1
+            }
+            result += abs(num - median) / x
+        }
+
+        return result
+    }
 }
