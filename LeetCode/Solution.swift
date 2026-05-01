02290546ef302d50898b7da4afc392eb9ce1391b
Nihad <43179082+N-ihad@users.noreply.github.com>
2026-05-01 17:49:29 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,27 @@
 
 import Foundation
 
+/*
+    https://leetcode.com/problems/rotate-function/solutions/8124574/swift-solution-constant-space-with-expla-uyuy
+*/
+
 class Solution {
-    
+    func maxRotateFunction(_ nums: [Int]) -> Int {
+        let n = nums.count
+        let sum = nums.reduce(0, +)
+
+        var f = 0
+        for i in 0..<n {
+            f += i * nums[i]
+        }
+
+        var result = f
+
+        for k in 1..<n {
+            f += sum - n * nums[n - k]
+            result = max(result, f)
+        }
+
+        return result
+    }
 }
