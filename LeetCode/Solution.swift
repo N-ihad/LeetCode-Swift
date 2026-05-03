7a8bace413071f19daf6b05c6e880a1a250fe546
Nihad <43179082+N-ihad@users.noreply.github.com>
2026-05-03 04:05:01 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,12 @@
 
 import Foundation
 
+/*
+    https://leetcode.com/problems/rotate-string/solutions/8131753/swift-solution-1-line-with-explanation-b-8ohx
+*/
+
 class Solution {
-    
+    func rotateString(_ s: String, _ goal: String) -> Bool {
+        return s.count == goal.count && (s + s).contains(goal)
+    }
 }
