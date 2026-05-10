6cf038c18113d998ec34634d7f0eee6513df9045
Nihad <43179082+N-ihad@users.noreply.github.com>
2026-05-10 12:37:30 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,22 @@
 
 import Foundation
 
+/*
+    https://leetcode.com/problems/maximum-number-of-jumps-to-reach-the-last-index/solutions/8182878/swift-solution-with-explanation-by-nihad-tbur
+*/
+
 class Solution {
-    
+    func maximumJumps(_ nums: [Int], _ target: Int) -> Int {
+        let n = nums.count
+        var dp = Array(repeating: -1, count: n); dp.reserveCapacity(n)
+        dp[0] = 0
+
+        for i in 0..<n-1 {
+            for j in i+1..<n where dp[i] != -1 && abs(nums[j] - nums[i]) <= target {
+                dp[j] = max(dp[j], dp[i] + 1)
+            }
+        }
+
+        return dp[n-1]
+    }
 }
