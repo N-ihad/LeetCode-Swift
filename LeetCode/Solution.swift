21445ab605e34ac241a9eafac003f759988b6feb
Nihad <43179082+N-ihad@users.noreply.github.com>
2026-05-14 19:06:37 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,32 @@
 
 import Foundation
 
+/*
+    https://leetcode.com/problems/check-if-array-is-good/solutions/8229318/swift-solution-without-hash-table-and-so-vyt1
+*/
+
 class Solution {
-    
+    func isGood(_ nums: [Int]) -> Bool {
+        let n = nums.max()!
+        if n != nums.count - 1 {
+            return false
+        }
+
+        var freq = Array(repeating: 0, count: n + 1)
+        for num in nums {
+            freq[num] += 1
+        }
+
+        if freq[n] != 2 {
+            return false
+        }
+
+        for num in nums {
+            if num != n && freq[num] != 1 {
+                return false
+            }
+        }
+
+        return true
+    }
 }
