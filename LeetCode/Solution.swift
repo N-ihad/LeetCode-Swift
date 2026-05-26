d21dc6f9a270218de57ff334da9fd3789a3dba62
Nihad <43179082+N-ihad@users.noreply.github.com>
2026-05-26 15:38:08 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,25 @@
 
 import Foundation
 
+/*
+    https://leetcode.com/problems/count-the-number-of-special-characters-i/solutions/8294943/swift-solution-with-explanation-by-nihad-xclz
+*/
+
 class Solution {
-    
+    func numberOfSpecialChars(_ word: String) -> Int {
+        var seen: Set<Character> = []
+        for letter in word {
+            seen.insert(letter)
+        }
+
+        var result = 0
+        for letter in seen {
+            if letter.isLowercase,
+                seen.contains(Character(letter.uppercased())) {
+                result += 1
+            }
+        }
+
+        return result
+    }
 }
