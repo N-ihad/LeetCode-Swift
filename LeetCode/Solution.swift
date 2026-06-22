738f41e85565ea1a5f0faef70ad4eba320dc0ae1
Nihad <43179082+N-ihad@users.noreply.github.com>
2026-06-22 15:25:58 +0300

    Update Solution.swift

--- LeetCode/Solution.swift
+++ LeetCode/Solution.swift
@@ -7,6 +7,32 @@
 
 import Foundation
 
+/*
+    https://leetcode.com/problems/maximum-number-of-balloons/solutions/8351654/swift-solution-with-explanation-by-nihad-8tun
+*/
+
 class Solution {
-    
+    func maxNumberOfBalloons(_ text: String) -> Int {
+        var freq: [Character: Int] = [
+            "b": 0,
+            "a": 0,
+            "l": 0,
+            "o": 0,
+            "n": 0
+        ]
+
+        for c in text {
+            if freq[c] != nil {
+                freq[c]! += 1
+            }
+        }
+
+        return min(
+            freq["b"]!,
+            freq["a"]!,
+            freq["l"]! / 2,
+            freq["o"]! / 2,
+            freq["n"]!
+        )
+    }
 }
